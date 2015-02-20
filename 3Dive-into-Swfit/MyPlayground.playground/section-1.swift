7bd087fbb23527f0150f84ec1eaf0e54b3b43266
greatabel <myreceiver2for2github@gmail.com>
2015-02-20 23:17:03 +0800

    dive into swift

--- 3Dive-into-Swfit/MyPlayground.playground/section-1.swift
+++ 3Dive-into-Swfit/MyPlayground.playground/section-1.swift
@@ -14,3 +14,24 @@ var fastCar: Car = Car()
 //constants
 let name: String = "Constant name abel"
 let isMale: Bool = true
+
+var favoriteFood: String = "Pasta"
+favoriteFood = favoriteFood.uppercaseString
+
+var beach = "Beach"beach = beach + "Ball"
+
+beach
+
+let name1 = "Steve"
+var message = "Hello \(name1)"
+
+//variables in Strings
+let seatsPerRow = 25let numberOfRows = 15var seatsString = "In the theater, there are \(numberOfRows) rows and \(seatsPerRow) seats in each row."
+
+//Arrays
+var names: [String]=["steve","jeff","andy"]
+//because Swift can detet type of a given array,so I can write like:
+var names1 = ["haha","hehe"]
+
+
+
