c718bcf8d3b874c75b3fe92661c5b9718dbb630b
greatabel <myreceiver2for2github@gmail.com>
2015-02-20 22:53:14 +0800

    dive into swift

---
+++ 3Dive-into-Swfit/MyPlayground.playground/section-1.swift
@@ -0,0 +1,16 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var numberOfYears: Int = 30
+var accountBalance: Float = 1203.51
+var isStudent: Bool = true
+var firstName: String = "Steve"
+
+var fastCar: Car = Car()
+
+//constants
+let name: String = "Constant name abel"
+let isMale: Bool = true
