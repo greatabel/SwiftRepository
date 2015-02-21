523a0bc06bb82376fa39db6ffbd05bdcfefc7ded
greatabel <myreceiver2for2github@gmail.com>
2015-02-21 23:54:14 +0800

    background

--- 3Dive-into-Swfit/MyPlayground.playground/section-1.swift
+++ 3Dive-into-Swfit/MyPlayground.playground/section-1.swift
@@ -9,7 +9,7 @@ var accountBalance: Float = 1203.51
 var isStudent: Bool = true
 var firstName: String = "Steve"
 
-var fastCar: Car = Car()
+//var fastCar: Car = Car()
 
 //constants
 let name: String = "Constant name abel"
@@ -32,6 +32,28 @@ let seatsPerRow = 25let numberOfRows = 15var seatsString = "In the theater, th
 var names: [String]=["steve","jeff","andy"]
 //because Swift can detet type of a given array,so I can write like:
 var names1 = ["haha","hehe"]
+
+//Dictionary
+var homeruns : [String: Int] = ["Posey": 24,"Pagan":19,"Pence":15]
+var homeruns1 = ["Posey": 24,"Pagan":19,"Pence":15]
+
+var favStates = ["California", "New York", "Colorado", "Oregon"]
+
+for state in favStates {
+    var message = "\(state) is my favorite state"
+    println(message)
+}
+
+var translatedWord : String? = nil
+
+
+if translatedWord == nil {
+    var myMessage = "Good Morning"
+} else {
+    var myMessage = translatedWord!
+}
+
+
 
 
 
