383418806ac7b2213a4e0d8285ebcfc3da24b497
greatabel <myreceiver2for2github@gmail.com>
2015-04-08 17:23:57 +0800

    web page

--- ihanhai/ihanhai/ViewController.swift
+++ ihanhai/ihanhai/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    
+//    let url = "http://apple.com"
+   var try6 = NSURL(fileURLWithPath:NSBundle.mainBundle().pathForResource("index", ofType:"html")!)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        let requestURL = NSURL(string:url)
+        
+//        let request = NSURLRequest(URL: requestURL!)
+        let request = NSURLRequest(URL: try6!)
+        webView.loadRequest(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
