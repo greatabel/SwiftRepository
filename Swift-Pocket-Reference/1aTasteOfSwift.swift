ee0da079915935d48f3ddc8174470f1e46bdc20c
greatabel <myreceiver2for2github@gmail.com>
2015-07-27 23:33:31 +0800

    @*n

--- Swift-Pocket-Reference/1aTasteOfSwift.swift
+++ Swift-Pocket-Reference/1aTasteOfSwift.swift
@@ -8,12 +8,35 @@ extension  String {
 
     }
 
+    func trailingSpaces(width: Int) -> String{
+
+        var s = "\(self)"
+        for i in count(s)..<width
+        {
+            s = s + " "
+        }
+        return s
+    }
+
 
 }
 
 var strTest = "hello world"
 println(strTest)
 println(strTest.leadingSpaces(20))
+var trailStr = strTest.trailingSpaces(20) + "@"
+println(trailStr)
+
+// http://stackoverflow.com/questions/28469248/how-to-print-multiple-instances-of-a-variable-in-swift
+func * (left: String, right: Int) -> String {
+
+    return join("", Repeat(count:right, repeatedValue: left))
+
+}
+
+
+println(" ^-^ " * 10)
+
 
 extension Int {
     func times(task: () -> ()) {
@@ -23,5 +46,5 @@ extension Int {
     }
   }
 5.times{
-    println("Hello,World!")
+    println("#Hello,World!")
 }
