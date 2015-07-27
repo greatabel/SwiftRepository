0432c9d604ee3186a7e220f09cbb058c499922ec
greatabel <myreceiver2for2github@gmail.com>
2015-07-27 23:17:39 +0800

    add example

--- Swift-Pocket-Reference/1aTasteOfSwift.swift
+++ Swift-Pocket-Reference/1aTasteOfSwift.swift
@@ -14,3 +14,14 @@ extension  String {
 var strTest = "hello world"
 println(strTest)
 println(strTest.leadingSpaces(20))
+
+extension Int {
+    func times(task: () -> ()) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+  }
+5.times{
+    println("Hello,World!")
+}
