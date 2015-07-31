85a4f683d52a1e97140fc73bfda82af55c4148dc
greatabel <myreceiver2for2github@gmail.com>
2015-07-31 18:21:20 +0800

    find peripheral

--- Bluetooth/AbelHeadsBluetooth/AbelHeadsBluetooth/PeripheralsTableViewController.swift
+++ Bluetooth/AbelHeadsBluetooth/AbelHeadsBluetooth/PeripheralsTableViewController.swift
@@ -40,6 +40,18 @@ class PeripheralsTableViewController: UITableViewController,CBCentralManagerDele
     var connectedPeripheral: CBPeripheral?
     
     
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        manager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        manager = CBCentralManager(delegate: self, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,13 +65,49 @@ class PeripheralsTableViewController: UITableViewController,CBCentralManagerDele
         
     }
     
-    func startScanning(){
-        println("Started scanning");
-        
+    func startScanning() {
+        println("Started scanning.")
+        visiblePeripheralUUIDs.removeAllObjects()
+        visiblePeripherals.removeAll(keepCapacity: true)
+        tableView.reloadData()
+        manager.scanForPeripheralsWithServices(nil, options: nil)
+        scanTimer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: Selector("stopScanning"), userInfo: nil, repeats: false)
+    }
+    
+    func stopScanning(){
+        println("Stopped Scanning")
+        println("Found \(visiblePeripherals.count) peripherals.")
+        manager.stopScan()
+        refreshControl?.endRefreshing()
+        scanTimer?.invalidate()
     }
     
     func centralManagerDidUpdateState(central: CBCentralManager!) {
+        var printString: String
+        switch central.state {
+        case .PoweredOff:
+            printString = "Bluetooth hardware powered off."
+            isBluetoothEnabled = false
+        case .PoweredOn:
+            printString = "Bluetooth hardware powered on."
+            isBluetoothEnabled = true
+            startScanning()
+        case .Resetting:
+            printString = "Bluetooth hardware is resetting."
+            isBluetoothEnabled = false
+        case .Unauthorized:
+            printString = "Bluetooth hardware is unauthorized."
+            isBluetoothEnabled = false
+        case .Unsupported:
+            printString = "Bluetooth hardware is unsupported."
+            isBluetoothEnabled = false
+        case .Unknown:
+            printString = "Bluetooth hardware state is unknown."
+            isBluetoothEnabled = false
+        }
         
+        println("State updated to: \(printString)")
+
     }
     
     func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
@@ -68,12 +116,40 @@ class PeripheralsTableViewController: UITableViewController,CBCentralManagerDele
     
     func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
         
+         println("Peripheral found with name: \(peripheral.name)\nUUID: \(peripheral.identifier.UUIDString)\nRSSI: \(RSSI)\nAdvertisement Data: \(advertisementData)")
+        
+        visiblePeripheralUUIDs.addObject(peripheral.identifier.UUIDString)
+        visiblePeripherals[peripheral.identifier.UUIDString] = Peripheral(peripheral: peripheral, RSSI: RSSI.stringValue, advertisementDictionary: advertisementData)
+        
+        tableView.reloadData()
     }
     
     func centralManager(central: CBCentralManager!, didDisconnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
         
     }
     func centralManager(central: CBCentralManager!, didFailToConnectPeripheral peripheral: CBPeripheral!, error: NSError!) {
+        
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return visiblePeripherals.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("PeripheralCell", forIndexPath: indexPath) as! PeripheralTableViewCell
+        
+        if let visibleUUID = visiblePeripheralUUIDs[indexPath.row] as? String{
+            if let visiblePeripheral = visiblePeripherals[visibleUUID] {
+                if visiblePeripheral.connectable == "No"{
+                    cell.accessoryType = .None
+            }
+                cell.setupWithPeripheral(visiblePeripheral)
+        }
+        }
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
     }
 
