23aed054a0a90ff5dd3e67dcd826511ffdaa13fa
greatabel <myreceiver2for2github@gmail.com>
2015-08-05 16:17:51 +0800

    core data ch2

---
+++ Core Data by Tutorials iOS 8 and Swift/2Bow Ties/Bowtie.swift
@@ -0,0 +1,23 @@
+//
+//  Bowtie.swift
+//  Bow Ties
+//
+//  Created by 万畅 on 15/8/5.
+//  Copyright (c) 2015年 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Bowtie: NSManagedObject {
+
+    @NSManaged var isFororite: NSNumber
+    @NSManaged var lastWorn: NSDate
+    @NSManaged var name: String
+    @NSManaged var photoData: NSData
+    @NSManaged var rating: NSNumber
+    @NSManaged var searchKey: String
+    @NSManaged var timesWorn: NSNumber
+    @NSManaged var tintColor: AnyObject
+
+}
