f1bff0ed4c1df1c5dd8c4ce502195dad482b5f5c
greatabel <myreceiver2for2github@gmail.com>
2015-08-10 22:31:32 +0800

    type checke

---
+++ Swift-Pocket-Reference/31Checking and Casting Types.swift
@@ -0,0 +1,88 @@
+var a = [Any]()
+a.append(2)
+a.append(3.4)
+a.append("crunch")
+
+func someFunc(t: Int) {
+// do something with t
+    println("@ \(t)")
+}
+
+
+
+for i in 0..<a.count{
+    someFunc(i)
+
+}
+
+//  https://developer.apple.com/library/prerelease/mac/documentation/
+//  Swift/Conceptual/Swift_Programming_Language/TypeCasting.html#//apple_ref/doc/uid/TP40014097-CH22-XID_503
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+ 
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let someObjects: [AnyObject] = [
+    Movie(name: "2001: A Space Odyssey", director: "Stanley Kubrick"),
+    Movie(name: "Moon", director: "Duncan Jones"),
+    Movie(name: "Alien", director: "Ridley Scott")
+]
+
+for object in someObjects {
+    let movie = object as! Movie
+    println("Movie: '\(movie.name)', dir. \(movie.director)")
+}
+
+var things = [Any]()
+ 
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append(3.14159)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append(Movie(name: "Ghostbusters", director: "Ivan Reitman"))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+for thing in things {
+    switch thing {
+    case 0 as Int:
+        println("zero as an Int")
+    case 0 as Double:
+        println("zero as a Double")
+    case let someInt as Int:
+        println("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        println("a positive double value of \(someDouble)")
+    case is Double:
+        println("some other double value that I don't want to print")
+    case let someString as String:
+        println("a string value of \"\(someString)\"")
+    case let (x, y) as (Double, Double):
+        println("an (x, y) point at \(x), \(y)")
+    case let movie as Movie:
+        println("a movie called '\(movie.name)', dir. \(movie.director)")
+    case let stringConverter as String -> String:
+        println(stringConverter("Michael"))
+    default:
+        println("something else")
+    }
+}
