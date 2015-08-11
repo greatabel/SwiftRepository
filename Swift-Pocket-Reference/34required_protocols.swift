e04e40c7b918e208b72421b3b7369f31ac532478
greatabel <myreceiver2for2github@gmail.com>
2015-08-11 14:43:17 +0800

    protocol

---
+++ Swift-Pocket-Reference/34required_protocols.swift
@@ -0,0 +1,31 @@
+protocol SomeProtocol {
+    
+    var aWritableProperty: Double { get set}
+    var aReadOnlyProperty: Int {get}
+    static var aTypeProperty: String { get set }
+}
+
+class MyClass : SomeProtocol {
+     var aWritableProperty = 1.1
+    let  aReadOnlyProperty = 1
+
+    struct StoredTypeProperties {
+        static var aTypeProperty: String = "hello world"
+    }
+    class var aTypeProperty: String {
+        get {
+            return self.StoredTypeProperties.aTypeProperty 
+        }
+
+        set {
+            self.StoredTypeProperties.aTypeProperty = newValue
+        }
+    }
+}
+
+
+let st = MyClass()
+println("\(st.aWritableProperty) # \(st.aReadOnlyProperty) #\(MyClass.aTypeProperty)")
+st.aWritableProperty = 2.2
+MyClass.aTypeProperty = "Test"
+println("\(st.aWritableProperty) # \(st.aReadOnlyProperty) #\(MyClass.aTypeProperty)")
