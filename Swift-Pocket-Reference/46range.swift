145b9edae305b63cdd194f3175057d4e71facfce
greatabel <myreceiver2for2github@gmail.com>
2015-08-13 17:22:36 +0800

    range

--- Swift-Pocket-Reference/46range.swift
+++ Swift-Pocket-Reference/46range.swift
@@ -8,3 +8,20 @@ for x in r
 {
     println(x)
 }
+
+var i = 1.1...2.2
+println(i.start)
+print(i.end)
+
+println(i.contains(3.4))
+println(i.contains(1.6))
+
+var s = stride(from:2, to:18, by:2) 
+for x in s 
+{
+println (x) 
+}
+
+for x in stride(from:2.2, through:2.8, by:0.3) {
+println (x) 
+}
