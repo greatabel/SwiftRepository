c081515198d0bd8e3733c20bfed8eb3e6e02574f
greatabel <myreceiver2for2github@gmail.com>
2015-08-15 17:19:32 +0800

    add memoryViewController

--- Swift-By-Example/2Memory/2Memory/MemoryViewController.swift
+++ Swift-By-Example/2Memory/2Memory/MemoryViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MemoryViewController: UIViewController {
     
+    private var collectionView: UICollectionView!
     private let difficulty: Difficulty
     
     init(difficulty: Difficulty){
@@ -28,10 +29,51 @@ class MemoryViewController: UIViewController {
     
 }
 
+private extension MemoryViewController{
+    func sizeDifficulty(difficulty: Difficulty) -> (CGFloat, CGFloat){
+        switch difficulty{
+        case .Easy:
+            return (4,3)
+        case .Medium:
+            return (6,4)
+        case .Hard:
+            return (8,4)
+            
+        }
+    }
+}
 
+// MARK: setup
 private extension MemoryViewController{
     func setup(){
         view.backgroundColor = UIColor.greenSea()
+        
+        let ratio: CGFloat = 1.452
+        let space: CGFloat = 5
+        
+        let (columns, rows) = sizeDifficulty(difficulty)
+        let cardHeight: CGFloat = view.frame.height/rows - 2*space
+        let cardWidth: CGFloat = cardHeight/ratio
+        
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: space, left: space, bottom: space, right: space)
+        layout.itemSize = CGSize(width: cardWidth, height: cardHeight)
+        layout.minimumLineSpacing = space
+        
+        let covWidth = columns*(cardWidth + 2*space)
+        let covHeight = rows*(cardHeight + space)
+        
+        collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: covWidth, height: covHeight), collectionViewLayout: layout)
+        collectionView.center = view.center
+//        collectionView.dataSource = self
+//        collectionView.delegate = self
+        collectionView.scrollEnabled = false
+//        collectionView.registerClass(CardCell.self, forCellWithReuseIdentifier: "cardCell")
+        collectionView.backgroundColor = UIColor.clearColor()
+        
+        self.view.addSubview(collectionView)
+        
+        
     }
     
 }
