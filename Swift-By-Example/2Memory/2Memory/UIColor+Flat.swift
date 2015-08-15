353424458d1412b32442ea898894ad29bf410f6b
greatabel <myreceiver2for2github@gmail.com>
2015-08-15 16:55:04 +0800

    2 meory game

---
+++ Swift-By-Example/2Memory/2Memory/UIColor+Flat.swift
@@ -0,0 +1,37 @@
+//
+//  UIColor+Flat.swift
+//  2Memory
+//
+//  Created by 万畅 on 15/8/15.
+//  Copyright © 2015年 abelwan. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor{
+    
+    class func greenSea() -> UIColor {
+        return UIColor.colorComponents((22, 160, 133))
+    }
+    
+    class func emerald() -> UIColor {
+        return UIColor.colorComponents((46, 204, 113))
+    }
+    
+    class func sunflower() -> UIColor {
+        return UIColor.colorComponents((241, 196, 15))
+    }
+    
+    class func alizarin() -> UIColor {
+        return UIColor.colorComponents((231, 76, 60))
+    }
+    
+}
+
+private extension UIColor{
+    class func colorComponents(comoponents:(CGFloat, CGFloat, CGFloat) ) -> UIColor
+    {
+        return UIColor(red:comoponents.0/255, green: comoponents.1/255, blue: comoponents.2/255, alpha:1)
+    }
+    
+}
--- Swift-By-Example/2Memory/2Memory/ViewController.swift
+++ Swift-By-Example/2Memory/2Memory/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+enum Difficulty{
+    case Easy, Mediu, Hard
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setup()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +26,35 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+
+private extension ViewController{
+    func setup(){
+        
+        view.backgroundColor = UIColor.greenSea()
+        
+        buildButtonCenter(CGPoint(x: view.center.x, y: view.center.y/2.0),
+            title: "EASY", color: UIColor.emerald(), action: "onEasyTapped:")
+        buildButtonCenter(CGPoint(x: view.center.x, y: view.center.y),
+            title: "MEDIUM", color: UIColor.sunflower(), action: "onMediumTapped:")
+        buildButtonCenter(CGPoint(x: view.center.x, y: view.center.y*3.0/2.0),
+            title: "HARD", color: UIColor.alizarin(), action: "onHardTapped:")
+        
+        
+    }
+    func buildButtonCenter(center: CGPoint, title: String, color: UIColor, action: Selector) {
+        let button = UIButton()
+        button.setTitle(title, forState: .Normal)
+        
+        button.frame = CGRect(origin: CGPoint(x:0, y:0), size: CGSize(width: 200, height: 50))
+        button.center = center
+        button.backgroundColor = color
+        
+        button.addTarget(self, action: action, forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(button)
+        
+    }
 }
 
