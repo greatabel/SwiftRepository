3a9331f0541e9eea9a5658c0447fb3d687785f33
greatabel <myreceiver2for2github@gmail.com>
2015-08-15 14:52:02 +0800

    ch1 guess number

--- Swift-By-Example/1GuessTheNumber/1GuessTheNumber/ViewController.swift
+++ Swift-By-Example/1GuessTheNumber/1GuessTheNumber/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var rangeLbl: UILabel!
+    @IBOutlet weak var numberTxtField: UITextField!
+    @IBOutlet weak var messageLbl: UILabel!
+    @IBOutlet weak var numGuessesLbl: UILabel!
+    
+    @IBAction func onOKPressed(sender: AnyObject){
+        
+        print(numberTxtField.text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
