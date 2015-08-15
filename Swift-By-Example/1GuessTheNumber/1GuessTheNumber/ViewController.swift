ae9187ed889a52af9476a17878395f86b21ef270
greatabel <myreceiver2for2github@gmail.com>
2015-08-15 15:26:40 +0800

    ch1 guess number

--- Swift-By-Example/1GuessTheNumber/1GuessTheNumber/ViewController.swift
+++ Swift-By-Example/1GuessTheNumber/1GuessTheNumber/ViewController.swift
@@ -15,14 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLbl: UILabel!
     @IBOutlet weak var numGuessesLbl: UILabel!
     
-    @IBAction func onOKPressed(sender: AnyObject){
-        
+    
+    private var lowerBound = 0
+    private var upperBound = 100
+    private var numGuesses = 0
+    private var secretNumber = 0
+    
+    
+    @IBAction func onOkPressed(sender: AnyObject) {
         print(numberTxtField.text)
+        let number = Int(numberTxtField.text!)
+        print("number= \(number)")
+        
+        if let number = number {
+            selectedNumber(number)
+        } else {
+            var alert = UIAlertController(title: nil, message: "Enter a number", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numberTxtField.becomeFirstResponder()
+        reset()
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,3 +51,90 @@ class ViewController: UIViewController {
 
 }
 
+
+private extension ViewController{
+    
+    enum Comparison{
+        case Smaller
+        case Greater
+        case Equals
+    }
+    
+    func selectedNumber(number: Int){
+        switch compareNumber(number, otherNumber: secretNumber){
+        case .Equals:
+            var alert = UIAlertController(title: nil, message: "You won in \(numGuesses) guesses!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { cmd in
+                self.reset()
+                self.numberTxtField.text = ""
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+
+        case .Smaller:
+            lowerBound = max(lowerBound, number)
+            messageLbl.text = "Your last guess was too low"
+            numberTxtField.text = ""
+            numGuesses++
+            renderRange()
+            renderNumGuesses()
+            
+        case .Greater:
+            upperBound = min(upperBound, number)
+            messageLbl.text = "Your last guess was too high"
+            numberTxtField.text = ""
+            numGuesses++
+            renderRange()
+            renderNumGuesses()
+            
+        }
+        
+    }
+    
+    func compareNumber(number: Int,otherNumber: Int) -> Comparison{
+        if number < otherNumber{
+            return .Smaller
+        } else if number > otherNumber{
+            return .Greater
+        }
+        return .Equals
+    }
+    
+    
+}
+
+private extension ViewController{
+    
+    func extractSecretNumber(){
+        
+        let diff = upperBound - lowerBound
+        let randomNumber = Int(arc4random_uniform(UInt32(diff)))
+        secretNumber = randomNumber + Int(lowerBound)
+        print("## hehe \(secretNumber)")
+        
+    }
+    func renderRange(){
+        rangeLbl.text = "\(lowerBound) and \(upperBound)"
+    }
+    func renderNumGuesses(){
+        numGuessesLbl.text = "Number of Guesses: \(numGuesses)"
+    }
+    func resetData(){
+        lowerBound = 0
+        upperBound = 100
+        numGuesses = 0
+        
+    }
+    func resetMsg(){
+        messageLbl.text = ""
+    }
+    
+    func reset(){
+        resetData()
+        renderRange()
+        renderNumGuesses()
+        extractSecretNumber()
+        resetMsg()
+    }
+    
+    
+}
