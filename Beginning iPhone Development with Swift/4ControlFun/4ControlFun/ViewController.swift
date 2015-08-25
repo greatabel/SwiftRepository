af20834f22adf4588fef7599373fc61c5ab9a8ba
greatabel <myreceiver2for2github@gmail.com>
2015-08-25 23:22:33 +0800

    ch4 keyboard

--- Beginning iPhone Development with Swift/4ControlFun/4ControlFun/ViewController.swift
+++ Beginning iPhone Development with Swift/4ControlFun/4ControlFun/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameField: UITextField!
 
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textFieldDoneEditing(sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func backgroundTap(sender:UIControl){
+     nameField.resignFirstResponder()
+     numberField.resignFirstResponder()
+    }
+    
 
 }
 
