1ee88258c2e2242bff2abb89c2ff1ce080adc9f4
greatabel <myreceiver2for2github@gmail.com>
2015-08-25 23:41:35 +0800

    ch4 switches

--- Beginning iPhone Development with Swift/4ControlFun/4ControlFun/ViewController.swift
+++ Beginning iPhone Development with Swift/4ControlFun/4ControlFun/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
 
     override func didReceiveMemoryWarning() {
