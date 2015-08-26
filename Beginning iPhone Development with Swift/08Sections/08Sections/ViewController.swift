21543d3e98b349ef4b26c1142d99d53ae734793f
greatabel <myreceiver2for2github@gmail.com>
2015-08-26 17:24:25 +0800

    table 08

---
+++ Beginning iPhone Development with Swift/08Sections/08Sections/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  08Sections
+//
+//  Created by 万畅 on 15/8/26.
+//  Copyright © 2015年 abelwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private let dwarves = [
+        "Sleepy", "Sneezy", "Bashful", "Happy",
+        "Doc", "Grumpy", "Dopey",
+        "Thorin", "Dorin", "Nori", "Ori",
+        "Balin", "Dwalin", "Fili", "Kili",
+        "Oin", "Gloin", "Bifur", "Bofur",
+        "Bombur"
+    ]
+    
+    let simpleTableIdentifier = "SimpleTableIdentifier"
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return dwarves.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            var cell = tableView.dequeueReusableCellWithIdentifier(simpleTableIdentifier)
+            if(cell == nil){
+        cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: simpleTableIdentifier)
+            }
+            cell!.textLabel?.text = dwarves[indexPath.row]
+            return cell!
+    }
+
+
+}
+
