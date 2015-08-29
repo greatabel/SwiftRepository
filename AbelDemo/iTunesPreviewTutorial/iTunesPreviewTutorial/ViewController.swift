5024955d96b56121f9eb33fa7d3b843efc631d3a
greatabel <myreceiver2for2github@gmail.com>
2015-08-29 18:32:00 +0800

    load images

---
+++ AbelDemo/iTunesPreviewTutorial/iTunesPreviewTutorial/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  iTunesPreviewTutorial
+//
+//  Created by Jameson Quave on 4/16/15.
+//  Copyright (c) 2015 JQ Software LLC. All rights reserved.
+//  http://jamesonquave.com/blog/developing-ios-apps-using-swift-tutorial-part-2/
+//  http://stackoverflow.com/questions/31254725/transport-security-has-blocked-a-cleartext-http
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    var tableData = []
+    @IBOutlet weak var appsTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchItunesFor("JQ Software")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        if let rowData: NSDictionary = self.tableData[indexPath.row] as? NSDictionary,
+            // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
+            urlString = rowData["artworkUrl60"] as? String,
+            imgURL = NSURL(string: urlString),
+            // Get the formatted price string for display in the subtitle
+            formattedPrice = rowData["formattedPrice"] as? String,
+            // Download an NSData representation of the image at the URL
+            imgData = NSData(contentsOfURL: imgURL),
+            // Get the track name
+            trackName = rowData["trackName"] as? String {
+                // Get the formatted price string for display in the subtitle
+                cell.detailTextLabel?.text = formattedPrice
+                // Update the imageView cell to use the downloaded image data
+                cell.imageView?.image = UIImage(data: imgData)
+                // Update the textLabel text to use the trackName from the API
+                cell.textLabel?.text = trackName
+        }
+        return cell
+    }
+    
+    func searchItunesFor(searchTerm: String) {
+        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
+        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
+        
+        // Now escape anything else that isn't URL-friendly
+        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task =  session.dataTaskWithURL(url!, completionHandler:
+                {data, response, error -> Void in
+                    
+                    print("Task completed")
+                    if(error != nil) {
+                        // If there is an error in the web request, print it to the console
+                        print(error!.localizedDescription)
+                    }
+                    do{
+                    if let jsonResult = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as? NSDictionary {
+                        
+                            // If there is an error parsing JSON, print it to the console
+//                            print("JSON Error \(err!.localizedDescription)")
+                        
+                        if let results: NSArray = jsonResult["results"] as? NSArray {
+                            print("@@@\(results.count)")
+                            dispatch_async(dispatch_get_main_queue(), {
+                                print("### \(results.count)")
+                                self.tableData = results
+                                self.appsTableView!.reloadData()
+                            })
+                        }
+                    }
+                    }catch{
+                        
+                    }
+
+                            })
+            
+            // The task is just an object with all these properties set
+            // In order to actually make the web request, we need to "resume"
+            task.resume()
+        }
+    }
+
+}
+
