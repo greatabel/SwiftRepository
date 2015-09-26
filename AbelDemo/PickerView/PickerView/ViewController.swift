0243ea8c1a586d6b751b77ffe8537bdf7ded097a
greatabel <myreceiver2for2github@gmail.com>
2015-09-26 17:23:16 +0800

    pickerview add pickerdata

--- AbelDemo/PickerView/PickerView/ViewController.swift
+++ AbelDemo/PickerView/PickerView/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
     }
 
     let pickerData = [
-        ["10","20","30","40"],
+        ["10\"","20\"","30\"","40\""],
         ["Cheese", "Pepperoni", "Sausage","Veggie","BBQ Chicken"]
     ]
 
@@ -37,6 +37,20 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
 
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerData[component].count
+    }
+
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        updateLabel()
+    }
+
+    func updateLabel(){
+        let size = pickerData[0][myPicker.selectedRowInComponent(0)]
+        let topping = pickerData[1][myPicker.selectedRowInComponent(1)]
+        myLabel.text = "Pizza: " + size + " " + topping
     }
 
 
