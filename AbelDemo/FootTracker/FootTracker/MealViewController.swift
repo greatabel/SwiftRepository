1ea5cc80b6f2d7f77148222b3abd0af9f0d79c59
greatabel <myreceiver2for2github@gmail.com>
2015-11-05 16:11:12 +0800

    cancel

--- AbelDemo/FootTracker/FootTracker/MealViewController.swift
+++ AbelDemo/FootTracker/FootTracker/MealViewController.swift
@@ -10,5 +10,16 @@ import UIKit
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBAction func cancel(sender: UIBarButtonItem) {
+
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+
+        if isPresentingInAddMealMode {
+            dismissViewControllerAnimated(true, completion: nil)
+        } else {
+            navigationController!.popViewControllerAnimated(true)
+        }
+        
+    }
 
 }
