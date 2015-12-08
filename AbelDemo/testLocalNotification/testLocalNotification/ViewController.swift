44cc8cc5b941ebe2413e6250730cfd3ef12d5ac4
greatabel <myreceiver2for2github@gmail.com>
2015-12-08 17:50:55 +0800

    send

--- AbelDemo/testLocalNotification/testLocalNotification/ViewController.swift
+++ AbelDemo/testLocalNotification/testLocalNotification/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func sendNotification(sender: UIButton) {
+        print("press send")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
