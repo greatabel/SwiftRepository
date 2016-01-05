038e3ba461c024c6638cbc14d867e51659f3c3e7
greatabel <myreceiver2for2github@gmail.com>
2016-01-05 22:33:41 +0800

    use code to draw

--- BigNerdRanch/3Views and View hierarchy/3WorldTrotter/3WorldTrotter/ViewController.swift
+++ BigNerdRanch/3Views and View hierarchy/3WorldTrotter/3WorldTrotter/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let firstFrame = CGRect(x:160, y:240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blueColor()
+        view.addSubview(firstView)
     }
 
     override func didReceiveMemoryWarning() {
