3fd9c6a113f969f3d695555b71eaa809b6716d7a
greatabel <myreceiver2for2github@gmail.com>
2016-02-13 23:53:50 +0800

    change

--- AbelDemo/MyFoodTracker/MyFoodTracker/Meal.swift
+++ AbelDemo/MyFoodTracker/MyFoodTracker/Meal.swift
@@ -3,10 +3,12 @@ import UIKit
 class Meal {
     var name: String
     var photo: UIImage?
+    var birthday: String
 
-    init?(name: String, photo: UIImage?) {
+    init?(name: String,birthday: String, photo: UIImage?) {
         self.name = name
         self.photo = photo
+        self.birthday = birthday
 
         if name.isEmpty {
             return nil
