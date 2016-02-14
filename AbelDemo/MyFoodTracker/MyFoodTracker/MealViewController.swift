35af45930e70292fb96ccc48664f01decd74cda3
greatabel <myreceiver2for2github@gmail.com>
2016-02-15 01:04:40 +0800

    meal

--- AbelDemo/MyFoodTracker/MyFoodTracker/MealViewController.swift
+++ AbelDemo/MyFoodTracker/MyFoodTracker/MealViewController.swift
@@ -1,9 +1,29 @@
 import UIKit
 
-class MealViewController: UIViewController, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UINavigationControllerDelegate {
 
+
+    @IBOutlet weak var nameTextField: UITextField!
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    @IBOutlet weak var birthdayTextField: UITextField!
+
+    var meal: Meal?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.delegate = self
+
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+
+            photoImageView.image = meal.photo
+            birthdayTextField.text = meal.birthday
+        }
+    }
 
     @IBAction func cancel(sender: UIBarButtonItem) {
         let isPresentingInAddMealMode = presentingViewController is UINavigationController
