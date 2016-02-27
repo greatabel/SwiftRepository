73201bb2921fc8570537fcf2ff5b52ee0f94c13a
greatabel <myreceiver2for2github@gmail.com>
2016-02-27 22:11:00 +0800

    rm todolist

--- Swift-By-Example/3TodoList/Pods/Target Support Files/LatoFont/LatoFont-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "UIFont+Lato.h"
-
-FOUNDATION_EXPORT double LatoFontVersionNumber;
-FOUNDATION_EXPORT const unsigned char LatoFontVersionString[];
-
--- Swift-By-Example/3TodoList/Pods/Target Support Files/Pods-3TodoList/Pods-3TodoList-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_3TodoListVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_3TodoListVersionString[];
-
