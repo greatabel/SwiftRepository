efb4b10e55dcc28eb6fd75a4ef4212a6ad39e25b
greatabel <myreceiver2for2github@gmail.com>
2016-03-04 23:16:26 +0800

    ios8 sdk development

---
+++ iOS8 SDK development/1Play Around with Xcode/MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("#", str)
+
+var myLabel = UILabel(frame: CGRectMake(0.0, 0.0, 200.0, 100.0))
+myLabel.text = str
+myLabel.backgroundColor = UIColor.redColor()
+myLabel.textColor = UIColor.whiteColor()
