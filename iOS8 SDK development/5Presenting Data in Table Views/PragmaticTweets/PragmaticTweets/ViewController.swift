66554683a3de743d0e0b6ec490a977736cef1a28
greatabel <myreceiver2for2github@gmail.com>
2016-03-17 16:21:54 +0800

    begin table

---
+++ iOS8 SDK development/5Presenting Data in Table Views/PragmaticTweets/PragmaticTweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PragmaticTweets
+//
+//  Created by 万畅 on 16/3/17.
+//  Copyright © 2016年 abelwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
