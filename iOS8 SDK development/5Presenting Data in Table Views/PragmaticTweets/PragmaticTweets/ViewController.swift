25a7f2df2275e2a26a7556e6d7531d4b0c53f00d
greatabel <myreceiver2for2github@gmail.com>
2016-03-17 17:24:36 +0800

    use array to store data, show at row

--- iOS8 SDK development/5Presenting Data in Table Views/PragmaticTweets/PragmaticTweets/ViewController.swift
+++ iOS8 SDK development/5Presenting Data in Table Views/PragmaticTweets/PragmaticTweets/ViewController.swift
@@ -1,14 +1,31 @@
-//
-//  ViewController.swift
-//  PragmaticTweets
-//
-//  Created by 万畅 on 16/3/17.
-//  Copyright © 2016年 abelwan. All rights reserved.
-//
-
 import UIKit
 
+let defaultAvatarURL = NSURL(string:
+    "http://tp2.sinaimg.cn/5329434101/50/40068050394/0"
+    )
+
+let defaultAvatarURL_I = NSURL(string:
+    "http://tp3.sinaimg.cn/1398613362/50/5606554466/1"
+)
 public class ViewController: UITableViewController {
+
+    var parsedTweets: Array<ParsedTweet> = [
+        ParsedTweet(tweetText:"1)iOS 8 SDK Development now in print. " +
+            "Swift programming FTW!",
+            userName:"@pragprog",
+            createdAt:"2014-08-20 16:44:30 EDT",
+            userAvatarURL: defaultAvatarURL),
+        ParsedTweet(tweetText:"2)iOS 8 SDK Development now in print. " +
+            "Swift programming FTW!",
+            userName:"@pragprog",
+            createdAt:"2014-08-20 16:44:30 EDT",
+            userAvatarURL: defaultAvatarURL_I),
+        ParsedTweet(tweetText:"3)iOS 8 SDK Development now in print. " +
+            "Swift programming FTW!",
+            userName:"@pragprog",
+            createdAt:"2014-08-20 16:44:30 EDT",
+            userAvatarURL: defaultAvatarURL)
+    ]
 
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +37,7 @@ public class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 5
+        return 3
     }
 
     override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -33,7 +50,10 @@ public class ViewController: UITableViewController {
 
     override public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
-        cell.textLabel!.text = "Row \(indexPath.row)"
+        let parsedTweet = parsedTweets[indexPath.row]
+        cell.textLabel?.text = parsedTweet.tweetText
+        
+//        cell.textLabel!.text = "Row haha \(indexPath.row)"
         return cell
     }
 
