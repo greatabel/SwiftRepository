1c4d49ba4e154e0e76b8cd2fdebedffef4652dd3
greatabel <myreceiver2for2github@gmail.com>
2016-03-19 11:44:43 +0800

    set datastring to datepicker

--- AbelDemo/DatePicker/DatePicker/ViewController.swift
+++ AbelDemo/DatePicker/DatePicker/ViewController.swift
@@ -1,5 +1,7 @@
-// http://www.ioscreator.com/tutorials/display-date-date-picker-ios8-swift
+/* http://www.ioscreator.com/tutorials/display-date-date-picker-ios8-swift
+http://blog.apoorvmote.com/change-textfield-input-to-datepicker/
 
+*/
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,6 +16,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         datePicker.addTarget(self, action: Selector("datePickerChanged:"),
             forControlEvents: UIControlEvents.ValueChanged)
+        dateLabel.text = "1989-12-12"
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        guard let converteddate = dateFormatter.dateFromString(dateLabel.text!) else {
+            return
+        }
+        datePicker.date = converteddate
+
     }
 
     func datePickerChanged(datePicker: UIDatePicker) {
