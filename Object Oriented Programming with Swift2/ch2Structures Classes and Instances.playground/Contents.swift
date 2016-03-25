7d410563bae2b8c31f80555ea110222f7a5206b5
greatabel <myreceiver2for2github@gmail.com>
2016-03-25 18:02:45 +0800

    reference

--- Object Oriented Programming with Swift2/ch2Structures Classes and Instances.playground/Contents.swift
+++ Object Oriented Programming with Swift2/ch2Structures Classes and Instances.playground/Contents.swift
@@ -23,3 +23,15 @@ var circleToDelete1 = Circle(radius: 25)
 var circleToDelete2 = Circle(radius: 50)
 circleToDelete1 = Circle(radius: 32)
 circleToDelete2 = Circle(radius: 47)
+
+var circle3 = Circle(radius: 42)
+var referenceToCircle3 = circle3
+circle3 = Circle(radius: 84)
+
+print("---")
+
+func getGeneratedCircleRadius() -> Double {
+    let circle = Circle(radius: 40)
+    return circle.radius
+}
+print(getGeneratedCircleRadius())
