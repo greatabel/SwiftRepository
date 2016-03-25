f628a22ab527b9d53deb24a50d581b2f4713c3e6
greatabel <myreceiver2for2github@gmail.com>
2016-03-25 17:50:16 +0800

    deinit

--- Object Oriented Programming with Swift2/ch2Structures Classes and Instances.playground/Contents.swift
+++ Object Oriented Programming with Swift2/ch2Structures Classes and Instances.playground/Contents.swift
@@ -8,9 +8,18 @@ class Circle {
         print("I am initializing with radius: \(radius)")
         self.radius = radius
     }
+
+    deinit {
+        print("I am deinit")
+    }
 }
 
 var circle1 = Circle(radius: 10)
 var circle2 = Circle(radius: 20)
 print(circle1.dynamicType)
 print(circle2.dynamicType)
+
+var circleToDelete1 = Circle(radius: 25)
+var circleToDelete2 = Circle(radius: 50)
+circleToDelete1 = Circle(radius: 32)
+circleToDelete2 = Circle(radius: 47)
