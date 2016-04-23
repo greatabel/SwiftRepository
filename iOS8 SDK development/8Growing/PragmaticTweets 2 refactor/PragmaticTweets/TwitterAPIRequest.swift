8475b500477dd1adb602ed705f21e8f849d7398c
greatabel <myreceiver2for2github@gmail.com>
2016-04-23 15:52:48 +0800

    ch8 refactor

--- iOS8 SDK development/8Growing/PragmaticTweets 2 refactor/PragmaticTweets/TwitterAPIRequest.swift
+++ iOS8 SDK development/8Growing/PragmaticTweets 2 refactor/PragmaticTweets/TwitterAPIRequest.swift
@@ -4,4 +4,42 @@ import Accounts
 
 class TwitterAPIRequest: NSObject {
 
+    func sendTwitterRequest (requestURL : NSURL!,
+                             params : [String:String],
+                             delegate : TwitterAPIRequestDelegate?) {
+        let accountStore = ACAccountStore()
+        let twitterAccountType =
+            accountStore.accountTypeWithAccountTypeIdentifier(
+                ACAccountTypeIdentifierTwitter)
+        accountStore.requestAccessToAccountsWithType(twitterAccountType,
+                                                     options: nil,
+                                                     completion: {
+                                                        (granted: Bool, error: NSError!) -> Void in
+                                                        if (!granted) {
+                                                            print ("account access not granted")
+                                                        } else {
+                                                            let twitterAccounts =
+                                                                accountStore.accountsWithAccountType(twitterAccountType)
+                                                            if twitterAccounts.count == 0 {
+                                                                print ("no twitter accounts configured")
+                                                                return
+                                                            } else {
+                                                                let request = SLRequest(forServiceType: SLServiceTypeTwitter,
+                                                                    requestMethod:SLRequestMethod.GET,
+                                                                    URL:requestURL,
+                                                                    parameters:params)
+                                                                request.account = twitterAccounts.first as! ACAccount
+                                                                request.performRequestWithHandler ( {
+                                                                    (data: NSData!, urlResponse: NSHTTPURLResponse!,
+                                                                    error: NSError!) -> Void in
+                                                                    delegate!.handleTwitterData(data,
+                                                                        urlResponse: urlResponse,
+                                                                        error: error,
+                                                                        fromRequest: self)
+                                                                })
+                                                            }
+                                                        }
+        })
+    }
+    
 }
