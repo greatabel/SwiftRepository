854d8e96cdeb03b1fb7e40fd062fad1c8f851422
greatabel <myreceiver2for2github@gmail.com>
2016-04-27 13:38:55 +0800

    delegate

--- iOS8 SDK development/8Growing/PragmaticTweets 2 refactor/PragmaticTweets/AppDelegate.swift
+++ iOS8 SDK development/8Growing/PragmaticTweets 2 refactor/PragmaticTweets/AppDelegate.swift
@@ -29,6 +29,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TwitterAPIRequestDelegate
                                    delegate: self)
     }
 
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
     func handleTwitterData(data: NSData!,
                            urlResponse: NSHTTPURLResponse!,
                            error: NSError!,
@@ -37,14 +45,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TwitterAPIRequestDelegate
             try? NSJSONSerialization.JSONObjectWithData(data,
                                                         options: NSJSONReadingOptions(rawValue: 0))
         print("suggestions JSON: \(jsonObject)")
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
 
