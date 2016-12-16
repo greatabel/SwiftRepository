43c429cc36055869f5b38ae25206c8c7136cc619
greatabel <myreceiver2for2github@gmail.com>
2016-12-16 15:17:58 +0800

    add xctest

--- Swift Algorithms and Data Structures/Swift Algorithms and Data Structures/ViewController.swift
+++ Swift Algorithms and Data Structures/Swift Algorithms and Data Structures/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Swift Algorithms and Data Structures
-//
-//  Created by 万畅 on 15/11/20.
-//  Copyright © 2015年 abelwan. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
