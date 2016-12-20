88449c871067352927cdd4eb6e8315cf24a22c39
greatabel <myreceiver2for2github@gmail.com>
2016-12-20 22:03:32 +0800

    printAllKey

--- Swift Algorithms and Data Structures/3LinkedList.swift
+++ Swift Algorithms and Data Structures/3LinkedList.swift
@@ -4,4 +4,16 @@ public class LinkedList<T: Equatable> {
 
     private var head: LLNode<T> = LLNode<T>()
 
+
+    func printAllKeys() {
+
+        var current: LLNode! = head
+        print("--")
+        while(current != nil) {
+            print("link item is: \(current.key)")
+            current = current.next
+        }
+    }
+
+
 }
