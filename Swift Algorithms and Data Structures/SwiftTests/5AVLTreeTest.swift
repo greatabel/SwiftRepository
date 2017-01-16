aac61d622d6f04aacd5d28bf56119f82e00065d7
greatabel <myreceiver2for2github@gmail.com>
2017-01-17 01:10:22 +0800

    test count AVLTree

--- Swift Algorithms and Data Structures/SwiftTests/5AVLTreeTest.swift
+++ Swift Algorithms and Data Structures/SwiftTests/5AVLTreeTest.swift
@@ -22,9 +22,32 @@ class AVLTreeTest: XCTestCase {
         }
     }
 
+    func testCount() {
+        let numberList = [1, 2, 3, 5, 234, -20]
+        let tree = AVLTree<Int>()
+        for number in numberList {
+            tree.addNode(key: number)
+        }
+        XCTAssert(tree.count == numberList.count, "Expected tree's size to be \(numberList.count), got \(tree.count) instead.")
+    }
+
     func buildAVLTree(numberList: Array<Int>) {
         let avlTest: AVLTree<Int> = AVLTree<Int>()
         XCTAssertNotNil(avlTest, "avl instance not created..")
+
+        //build the tree list
+        for number in numberList {
+            print("adding \(number) to avl tree...")
+            avlTest.addNode(key: number)
+        }
+
+        //traverse the completed tree
+//        avlTest.traverse()
+
+
+        //tree balance check
+        XCTAssertTrue(avlTest.isTreeBalanced(), "tree is unbalanced..")
+
     }
 
 }
