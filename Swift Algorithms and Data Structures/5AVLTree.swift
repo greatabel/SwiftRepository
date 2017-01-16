b02461c8c6c4a9d7cfc7a79c6430beb78aa5b3e0
greatabel <myreceiver2for2github@gmail.com>
2017-01-16 20:51:14 +0800

    count AVLTree

--- Swift Algorithms and Data Structures/5AVLTree.swift
+++ Swift Algorithms and Data Structures/5AVLTree.swift
@@ -12,4 +12,12 @@ public class AVLTree<T> {
         self.height = -1
     }
 
+    var count: Int {
+        let left = self.left?.count ?? 0
+        let right = self.right?.count ?? 0
+        return left + 1 + right
+    }
+    
+
+
 }
