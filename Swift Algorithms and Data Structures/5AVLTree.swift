0e341e651b4dcdc31381f1ab5eced8da81f1045a
greatabel <myreceiver2for2github@gmail.com>
2017-01-16 21:00:09 +0800

    addNode(key: T)

--- Swift Algorithms and Data Structures/5AVLTree.swift
+++ Swift Algorithms and Data Structures/5AVLTree.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 
-public class AVLTree<T> {
+public class AVLTree<T: Comparable> {
 
     var key: T?
     var left: AVLTree?
@@ -17,7 +17,19 @@ public class AVLTree<T> {
         let right = self.right?.count ?? 0
         return left + 1 + right
     }
-    
+
+    func addNode(key: T){
+
+        if(self.key == nil) {
+            self.key = key
+            self.height = 0
+            return
+        }
+
+        if(key < self.key!) {
+
+        }
+    }
 
 
 }
