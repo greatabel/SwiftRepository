6518ec448af78b01b0f5a611ee7ed3f199238dc0
greatabel <myreceiver2for2github@gmail.com>
2017-01-17 12:00:04 +0800

    rotation test

--- Swift Algorithms and Data Structures/SwiftTests/5AVLTreeTest.swift
+++ Swift Algorithms and Data Structures/SwiftTests/5AVLTreeTest.swift
@@ -100,4 +100,48 @@ class AVLTreeTest: XCTestCase {
 
     }
 
+    //input for a balanced avl tree - O(log n)
+    func testAVLBalancedTree() {
+
+        let numberList : Array<Int> = [8, 5, 10, 3, 12, 9, 6, 16]
+
+        //build and balance model
+        self.buildAVLTree(numberList: numberList)
+
+    }
+
+
+    //input for multiple right rotations - O(n)
+    func testAVLRotationRight() {
+
+        let numberList: Array<Int> = [29, 26, 23, 20, 19]
+
+        //build and balance model
+        self.buildAVLTree(numberList: numberList)
+    }
+
+
+
+    //input for multiple left rotations - O(n)
+    func testAVLRotationLeft() {
+
+        let numberList: Array<Int> = [19, 20, 23, 26, 29]
+
+        //build and balance model
+        self.buildAVLTree(numberList: numberList)
+
+    }
+
+
+
+    //input for left and right rotations - 0(n)
+    func testAVLRotationLeftAndRight() {
+
+        let numberList: Array<Int> = [19, 20, 21, 26, 16, 12]
+
+        //build and balance model
+        self.buildAVLTree(numberList: numberList)
+
+    }
+
 }
