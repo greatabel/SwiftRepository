3a3dc7caa60906e9949ba07db03b1cde1714608c
greatabel <myreceiver2for2github@gmail.com>
2017-01-28 22:08:47 +0800

    Queue isEmpty func

--- Swift Algorithms and Data Structures/7Queue.swift
+++ Swift Algorithms and Data Structures/7Queue.swift
@@ -3,11 +3,21 @@ import Foundation
 public class Queue<T> {
 
     private var top: QNode<T>! = QNode<T>()
-    
+
     var count: Int {
 
 
 
         return 0
+    }
+
+    func isEmpty() -> Bool {
+
+        if let _: T = self.top?.key {
+            return false
+        }
+        else {
+            return true
+        }
     }
 }
