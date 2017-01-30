a8ceb15a0f551ac5781bfbca5e8dcd2fbda17ce1
greatabel <myreceiver2for2github@gmail.com>
2017-01-31 00:54:43 +0800

    roll back count of stack

--- Swift Algorithms and Data Structures/8Stack.swift
+++ Swift Algorithms and Data Structures/8Stack.swift
@@ -6,28 +6,7 @@ class Stack<T> {
     private var top: LLNode<T>! = LLNode<T>()
     
     var count: Int {
-
-
-        //determine if the key or instance exist
-        let topitem: T? = self.top?.key
-
-        if (topitem == nil) {
-            return 0
-        }
-
-        var current: LLNode = top
-        var x: Int = 1
-
-
-        //cycle through the list of items to get to the end.
-        while ((current.next) != nil) {
-            current = current.next!
-            x += 1
-        }
-
-        return x
-
-
+        return 0
     }
 
 
