452bea008852503ae4844c26cba9e3fe78d20d01
greatabel <myreceiver2for2github@gmail.com>
2017-01-30 21:39:00 +0800

    buildQueue

--- Swift Algorithms and Data Structures/SwiftTests/7QueueTest.swift
+++ Swift Algorithms and Data Structures/SwiftTests/7QueueTest.swift
@@ -1,1 +1,38 @@
-import Foundation
+import XCTest
+@testable import Swift_Algorithms_and_Data_Structures
+
+
+class QueueTest: XCTestCase {
+
+    var numberList: Array<Int>!
+
+    override func setUp() {
+        super.setUp()
+        numberList = [8,7,10,9]
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+    func buildQueue() -> Queue<Int>! {
+
+        let newQ: Queue<Int>! = Queue<Int>()
+
+        XCTAssert(newQ.count == 0, "new queue instance count is 0")
+
+        for s in numberList {
+
+        }
+        return newQ
+    }
+    
+}
