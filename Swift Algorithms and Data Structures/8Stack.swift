4efa3b0e149f45d7556ef71aa0c71c8f13ebb22d
greatabel <myreceiver2for2github@gmail.com>
2017-01-31 18:28:15 +0800

    isEmpty and pop

--- Swift Algorithms and Data Structures/8Stack.swift
+++ Swift Algorithms and Data Structures/8Stack.swift
@@ -20,5 +20,44 @@ class Stack<T> {
         return x
     }
 
+    func isEmpty() -> Bool {
+
+        if let _: T = self.top?.key {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+
+    func peek() -> T? {
+        if let topitem: T = self.top?.key {
+            return topitem
+        }
+        else {
+            return nil
+        }
+    }
+
+    func push(key: T) {
+        
+    }
+
+    func pop() -> T? {
+        let topitem: T? = self.top?.key
+        if( topitem == nil ) {
+            return nil
+        }
+        let queueitem: T? = top.key!
+
+        if let nextitem = top.next {
+            top = nextitem
+        }
+        else {
+            top = nil
+        }
+        return queueitem
+    }
+
 
 }
