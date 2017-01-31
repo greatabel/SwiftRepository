d111f21987a49b81a916b89f17b5ea2fcb3c82d8
greatabel <myreceiver2for2github@gmail.com>
2017-01-31 18:38:23 +0800

    Vertext

--- Swift Algorithms and Data Structures/SwiftTests/8StackTest.swift
+++ Swift Algorithms and Data Structures/SwiftTests/8StackTest.swift
@@ -12,7 +12,8 @@ class StackTest: XCTestCase {
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // Put teardown code here. This method is called after the invocation 
+        // of each test method in the class.
         super.tearDown()
     }
 
