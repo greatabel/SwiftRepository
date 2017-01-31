6e5a2d38712a15a45801e7080c4868ff99687e0b
greatabel <myreceiver2for2github@gmail.com>
2017-01-31 18:40:54 +0800

    Vertex class

--- Swift Algorithms and Data Structures/9Vertex.swift
+++ Swift Algorithms and Data Structures/9Vertex.swift
@@ -3,4 +3,11 @@ import Foundation
 
 public class Vertex{
 
+    var key: String?
+    var neighbors: Array<Edge>
+    var visited: Bool = false
+
+    init() {
+        self.neighbors = Array<Edge>()
+    }
 }
