8be6eab9ef6e72646ce8b2a82f805ccaea4e6e59
greatabel <myreceiver2for2github@gmail.com>
2017-02-03 17:42:03 +0800

    11HashTable.swift

---
+++ Swift Algorithms and Data Structures/SwiftTests/11HashTest.swift
@@ -0,0 +1,25 @@
+import UIKit
+import XCTest
+
+
+@testable import Swift_Algorithms_and_Data_Structures
+
+class HashTableTest: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
