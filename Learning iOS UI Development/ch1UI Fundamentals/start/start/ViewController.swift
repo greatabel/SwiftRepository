14d07b374460e87f0d1a3cf41b1e6cf0d2a9c19d
greatabel <myreceiver2for2github@gmail.com>
2017-02-03 22:59:04 +0800

    change outlet

--- Learning iOS UI Development/ch1UI Fundamentals/start/start/ViewController.swift
+++ Learning iOS UI Development/ch1UI Fundamentals/start/start/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet  var viewContainer: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
