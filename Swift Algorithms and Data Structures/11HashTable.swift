4ecfa407580e062eea86a0f146f41dd0c1161a06
greatabel <myreceiver2for2github@gmail.com>
2017-02-03 17:35:30 +0800

    addWord

--- Swift Algorithms and Data Structures/11HashTable.swift
+++ Swift Algorithms and Data Structures/11HashTable.swift
@@ -7,5 +7,43 @@ public class HashTable {
     init(capacity: Int) {
         self.buckets = Array<HashNode!>(repeating:nil, count: capacity)
     }
-    
+
+    func addWord(firstname: String, lastname: String) {
+        var hashindex: Int!
+        var fullname: String!
+
+        fullname = firstname + lastname
+        hashindex = self.createHash(fullname)
+        let childToUse: HashNode = HashNode()
+        var head: HashNode!
+
+        childToUse.firstname = firstname
+        childToUse.lastname = lastname
+
+        if  buckets[hashindex] == nil {
+            buckets[hashindex] = childToUse
+        }
+        else {
+            print("a collision occured. implementing chaining..")
+            head = buckets[hashindex]
+
+            //append new item to the head of the list
+            childToUse.next = head
+            head = childToUse
+
+            //update the chained list
+            buckets[hashindex] = head
+        }
+
+    }
+
+    func createHash(_ fullname: String) -> Int! {
+        var remainder: Int = 0
+        var divisor: Int = 0
+        for key in fullname.unicodeScalars {
+            divisor += Int(key.value)
+        }
+        remainder = divisor % buckets.count
+        return remainder - 1
+    }
 }
