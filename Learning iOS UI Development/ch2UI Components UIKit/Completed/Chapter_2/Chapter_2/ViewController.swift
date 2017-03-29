ee124cef7b7ba626e3fdd6fd7c629fa6975202e5
greatabel <myreceiver2for2github@gmail.com>
2017-03-29 11:38:58 +0800

    ch2UI Components UIKit

---
+++ Learning iOS UI Development/ch2UI Components UIKit/Completed/Chapter_2/Chapter_2/ViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        customizeSlider()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    func customizeSlider(){
+        
+        // YOUR CODE HERE -------------------------------
+        // Initialize the images
+        
+        // YOUR CODE HERE -------------------------------
+    }
+
+}
+
