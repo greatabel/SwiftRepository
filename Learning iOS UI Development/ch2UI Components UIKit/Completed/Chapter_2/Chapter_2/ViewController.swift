85593888d46232eab6395be85cc047d2f7286586
greatabel <myreceiver2for2github@gmail.com>
2017-03-29 23:19:38 +0800

    ch2

--- Learning iOS UI Development/ch2UI Components UIKit/Completed/Chapter_2/Chapter_2/ViewController.swift
+++ Learning iOS UI Development/ch2UI Components UIKit/Completed/Chapter_2/Chapter_2/ViewController.swift
@@ -15,7 +15,20 @@ class ViewController: UIViewController {
         
         // YOUR CODE HERE -------------------------------
         // Initialize the images
-        
+        let minTrack = UIImage(named: "minTrack")
+        let maxTrack = UIImage(named: "maxTrack")
+        let thumb = UIImage(named: "thumb")  
+        // Create resizable images
+        let resizableMinTrack = minTrack?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5))
+        let resizableMaxTrack = maxTrack?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5))
+
+        // Configure the styles!
+        UISlider.appearance().setMaximumTrackImage(resizableMaxTrack, for: UIControlState())
+        UISlider.appearance().setMinimumTrackImage(resizableMinTrack, for: UIControlState())
+        UISlider.appearance().setThumbImage(thumb, for: UIControlState())
+
+
+
         // YOUR CODE HERE -------------------------------
     }
 
