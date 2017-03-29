849f721be0eec6f4889558845dfc3e6802378a1c
greatabel <myreceiver2for2github@gmail.com>
2017-03-30 01:58:35 +0800

    ui

--- BigNerdRanch/3Views and View hierarchy/3WorldTrotter/3WorldTrotter/ViewController.swift
+++ BigNerdRanch/3Views and View hierarchy/3WorldTrotter/3WorldTrotter/ViewController.swift
@@ -5,16 +5,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let firstFrame = CGRect(x:160, y:240, width: 100, height: 150)
-        let firstView = UIView(frame: firstFrame)
-        firstView.backgroundColor = UIColor.blue
-        view.addSubview(firstView)
+        
+//        let firstFrame = CGRect(x:160, y:240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x:20,y:30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+////        view.addSubview(secondView)
+//        firstView.addSubview(secondView)
 
-        let secondFrame = CGRect(x:20,y:30, width: 50, height: 50)
-        let secondView = UIView(frame: secondFrame)
-        secondView.backgroundColor = UIColor.green
-//        view.addSubview(secondView)
-        firstView.addSubview(secondView)
     }
 
     override func didReceiveMemoryWarning() {
