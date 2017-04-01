aae3e6bd821b40bbac8511690f244ca48826028a
greatabel <myreceiver2for2github@gmail.com>
2017-04-01 15:01:10 +0800

    convert

--- BigNerdRanch/4Views and View hierarchy/3WorldTrotter/3WorldTrotter/ConversionViewController.swift
+++ BigNerdRanch/4Views and View hierarchy/3WorldTrotter/3WorldTrotter/ConversionViewController.swift
@@ -1,23 +1,51 @@
 import UIKit
 
+@available(iOS 10.0, *)
 class ConversationViewController: UIViewController {
 
     @IBOutlet var celsiusLabel: UILabel!
 
     @IBOutlet var textField: UITextField!
 
-    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
-        if let text = textField.text, !text.isEmpty {
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
 
-            celsiusLabel.text = text
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else{
+            return nil
+        }
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
+        if let text = textField.text, let value = Double(text) {
+
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
         }
         else {
-            celsiusLabel.text = "???"
+            fahrenheitValue = nil
         }
     }
 
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
         textField.resignFirstResponder()
+    }
+
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
     }
 
 }
