a541435444710022d96b22d0c17b124a6db6d69c
greatabel <myreceiver2for2github@gmail.com>
2017-04-01 14:26:26 +0800

    connect storyboard and viewcontroller

--- BigNerdRanch/4Views and View hierarchy/3WorldTrotter/3WorldTrotter/ConversionViewController.swift
+++ BigNerdRanch/4Views and View hierarchy/3WorldTrotter/3WorldTrotter/ConversionViewController.swift
@@ -1,5 +1,16 @@
 import UIKit
 
 class ConversationViewController: UIViewController {
-    
+
+    @IBOutlet var celsiusLabel: UILabel!
+
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
+        if let text = textField.text, !text.isEmpty {
+
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+//        celsiusLabel.text = TextField.text
+    }
 }
