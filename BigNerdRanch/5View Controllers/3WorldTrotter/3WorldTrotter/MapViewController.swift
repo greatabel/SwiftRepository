3e6c382f7d6951d9a1616bc5fed5c801455e7ba5
greatabel <myreceiver2for2github@gmail.com>
2017-04-05 17:07:37 +0800

    mapview

---
+++ BigNerdRanch/5View Controllers/3WorldTrotter/3WorldTrotter/MapViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("MapViewController loaded its view")
+    }
+
+}
