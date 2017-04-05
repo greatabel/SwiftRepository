524bb01830bfe805282ef71c58de90592fb0715e
greatabel <myreceiver2for2github@gmail.com>
2017-04-06 00:21:34 +0800

    segmentedcontrol

--- BigNerdRanch/6Programmatic Views/6WorldTrotter/3WorldTrotter/MapViewController.swift
+++ BigNerdRanch/6Programmatic Views/6WorldTrotter/3WorldTrotter/MapViewController.swift
@@ -8,6 +8,16 @@ class MapViewController: UIViewController {
     override func loadView() {
         mapView = MKMapView()
         view = mapView
+
+        let segmentedControl =
+            UISegmentedControl(items: ["Standard", "Hybrid",
+                                       "Satellite"])
+        segmentedControl.backgroundColor =
+            UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
     }
 
     override func viewDidLoad() {
