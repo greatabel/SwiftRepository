ef89498034a0e46f24e39a6c64b53c0c9770a930
greatabel <myreceiver2for2github@gmail.com>
2017-04-05 17:23:14 +0800

    6Programmmatic view init

---
+++ BigNerdRanch/6Programmatic Views/3WorldTrotter/3WorldTrotter/ViewController.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        let firstFrame = CGRect(x:160, y:240, width: 100, height: 150)
+//        let firstView = UIView(frame: firstFrame)
+//        firstView.backgroundColor = UIColor.blue
+//        view.addSubview(firstView)
+//
+//        let secondFrame = CGRect(x:20,y:30, width: 50, height: 50)
+//        let secondView = UIView(frame: secondFrame)
+//        secondView.backgroundColor = UIColor.green
+////        view.addSubview(secondView)
+//        firstView.addSubview(secondView)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
