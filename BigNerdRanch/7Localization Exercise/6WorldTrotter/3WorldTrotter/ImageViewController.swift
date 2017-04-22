b5361fc04e081df0c09c269d260614d5573b3141
greatabel <myreceiver2for2github@gmail.com>
2017-04-22 23:51:19 +0800

    image view

---
+++ BigNerdRanch/7Localization Exercise/6WorldTrotter/3WorldTrotter/ImageViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import MapKit
+
+
+class ImageViewController: UIViewController {
+
+    // 课后习题7.19
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let path = Bundle.main.url(forResource:"index", withExtension: "jpg")
+        do{
+            let imageData = try Data(contentsOf: path!) as Data
+            let advTimeGif = UIImage(data:imageData)
+            let imageView2 = UIImageView(image: advTimeGif)
+            imageView2.frame = CGRect(x: 20.0, y: 220.0, width: self.view.frame.size.width - 40
+                                        , height: 350.0)
+            view.addSubview(imageView2)
+        }
+        catch{
+            print("error")
+            }
+    }
+    
+}
