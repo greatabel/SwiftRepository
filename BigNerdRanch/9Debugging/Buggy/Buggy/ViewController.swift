4476ee59b50af3f3959a58aaf38f016519692f75
greatabel <myreceiver2for2github@gmail.com>
2017-04-25 01:25:32 +0800

    init

--- BigNerdRanch/9Debugging/Buggy/Buggy/ViewController.swift
+++ BigNerdRanch/9Debugging/Buggy/Buggy/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonTapped(_ sender: UIButton){
+        print("buttonTapped")
+    }
+
 
 }
 
