849202165edad47540afefabded5df85a0697f11
greatabel <myreceiver2for2github@gmail.com>
2017-05-09 20:20:23 +0800

    show tableview rightDetail style

--- Big Nerd Ranch/10TableViewControllers/Homepwner_Abel/Homepwner_Abel/ItemsViewController.swift
+++ Big Nerd Ranch/10TableViewControllers/Homepwner_Abel/Homepwner_Abel/ItemsViewController.swift
@@ -15,6 +15,11 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
 
+        let item = itemStore.allItems[indexPath.row]
+
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "&\(item.valueInDollars)"
+        
         return cell
     }
 
