037ca5c4f1d2db5c1011fae543a104e89f5b1faa
greatabel <myreceiver2for2github@gmail.com>
2017-05-09 22:26:16 +0800

    show 2 sections

--- Big Nerd Ranch/10TableViewControllers Exercise/Homepwner_Abel/Homepwner_Abel/ItemsViewController.swift
+++ Big Nerd Ranch/10TableViewControllers Exercise/Homepwner_Abel/Homepwner_Abel/ItemsViewController.swift
@@ -17,8 +17,8 @@ class ItemsViewController: UITableViewController {
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
 //        print(itemStore.allItems.count)
-        var a = itemStore.allItems.filter({$0.valueInDollars > 50})
-        var b = itemStore.allItems.filter({$0.valueInDollars <= 50})
+        let a = itemStore.allItems.filter({$0.valueInDollars > 50})
+        let b = itemStore.allItems.filter({$0.valueInDollars <= 50})
         sectionData.append(a)
         sectionData.append(b)
 //        print(sectionData[0],sectionData[1])
@@ -26,21 +26,39 @@ class ItemsViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemStore.allItems.count
+//        return itemStore.allItems.count
+        return sectionData[section].count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
 
-        let item = itemStore.allItems[indexPath.row]
+//        let item = itemStore.allItems[indexPath.row]
+         let item = sectionData[indexPath.section][indexPath.row]
 
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "&\(item.valueInDollars)"
         
         return cell
     }
-    
+
     // http://stackoverflow.com/questions/29578965/how-do-i-populate-two-sections-in-a-tableview-with-two-different-arrays-using-sw
+    // http://stackoverflow.com/questions/40415223/numberofsectionsintableview-not-working
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return sectionData.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "大于50的区间 -->"
+        }
+        else{
+            return "剩余的 -->"
+        }
+    }
+
+
 
     
 }
