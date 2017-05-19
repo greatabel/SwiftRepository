a6fdb041a9540fb47069d9a14dac3c3921af5f92
greatabel <myreceiver2for2github@gmail.com>
2017-05-19 15:02:25 +0800

    ch14 NavigationController

---
+++ Big Nerd Ranch/14UINavigationController/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var nameField: UITextField!
+
+    @IBOutlet var serialNumberField: UITextField!
+
+    @IBOutlet var valueField: UITextField!
+
+    @IBOutlet var dateLabel: UILabel!
+
+    var item: Item!
+
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+//        valueField.text = "\(item.valueInDollars)"
+//        dateLabel.text = "\(item.dateCreated)"
+        valueField.text =
+            numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
+
+
+}
