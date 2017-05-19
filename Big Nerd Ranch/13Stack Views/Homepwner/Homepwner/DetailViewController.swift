c1a225188dcceef7ac26b73ec169ceb75ad2e319
greatabel <myreceiver2for2github@gmail.com>
2017-05-19 14:49:58 +0800

    connect 4 @IBOutlets

---
+++ Big Nerd Ranch/13Stack Views/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var nameField: UITextField!
+
+    @IBOutlet var serialNumberField: UITextField!
+
+    @IBOutlet var valueField: UITextField!
+
+    @IBOutlet var dateLabel: UILabel!
+
+
+}
