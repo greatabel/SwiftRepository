707ead23a855beb759410c1a969a5bf1de490e9c
greatabel <myreceiver2for2github@gmail.com>
2017-05-19 14:58:10 +0800

    numberFormatter

--- Big Nerd Ranch/13Stack Views/Homepwner/Homepwner/DetailViewController.swift
+++ Big Nerd Ranch/13Stack Views/Homepwner/Homepwner/DetailViewController.swift
@@ -12,13 +12,30 @@ class DetailViewController: UIViewController {
 
     var item: Item!
 
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         nameField.text = item.name
         serialNumberField.text = item.serialNumber
-        valueField.text = "\(item.valueInDollars)"
-        dateLabel.text = "\(item.dateCreated)"
+//        valueField.text = "\(item.valueInDollars)"
+//        dateLabel.text = "\(item.dateCreated)"
+        valueField.text =
+            numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
     }
 
 
