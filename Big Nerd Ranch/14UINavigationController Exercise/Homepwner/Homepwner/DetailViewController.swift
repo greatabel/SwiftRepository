92d3f763605cccde7af4c21dabb4f06083aab462
greatabel <myreceiver2for2github@gmail.com>
2017-05-26 18:14:38 +0800

    14 exercise init

---
+++ Big Nerd Ranch/14UINavigationController Exercise/Homepwner/Homepwner/DetailViewController.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var nameField: UITextField!
+
+    @IBOutlet var serialNumberField: UITextField!
+
+    @IBOutlet var valueField: UITextField!
+
+    @IBOutlet var dateLabel: UILabel!
+
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+
+
+    var item: Item! {
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
+
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+//        valueField.text = "\(item.valueInDollars)"
+//        dateLabel.text = "\(item.dateCreated)"
+        valueField.text =
+            numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // clear first responder
+        view.endEditing(true)
+        
+        //save changes to item
+        item.name = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text
+
+        if let valueText = valueField.text,
+            let value = numberFormatter.number(from: valueText) {
+            item.valueInDollars = value.intValue
+        } else {
+            item.valueInDollars = 0
+        }
+    }
+
+
+}
