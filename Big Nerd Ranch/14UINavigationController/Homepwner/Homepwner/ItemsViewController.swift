cce4069224d503c4b9ab22843816df1786faf7d4
greatabel <myreceiver2for2github@gmail.com>
2017-05-26 18:12:20 +0800

    rm head view

--- Big Nerd Ranch/14UINavigationController/Homepwner/Homepwner/ItemsViewController.swift
+++ Big Nerd Ranch/14UINavigationController/Homepwner/Homepwner/ItemsViewController.swift
@@ -34,16 +34,20 @@ class ItemsViewController: UITableViewController {
             setEditing(true, animated: true)
         }
     }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        tableView.contentInset = insets
-        tableView.scrollIndicatorInsets = insets
+//        let statusBarHeight = UIApplication.shared.statusBarFrame.height
+//        
+//        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+//        tableView.contentInset = insets
+//        tableView.scrollIndicatorInsets = insets
 
 //        tableView.rowHeight = 65
         tableView.rowHeight = UITableViewAutomaticDimension
