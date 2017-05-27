2650ed26251ffd90b054985c6cd32e466346d762
greatabel <myreceiver2for2github@gmail.com>
2017-05-27 11:44:06 +0800

    show date as change picker

--- Big Nerd Ranch/14UINavigationController Exercise/Homepwner/Homepwner/DatePickerViewController.swift
+++ Big Nerd Ranch/14UINavigationController Exercise/Homepwner/Homepwner/DatePickerViewController.swift
@@ -4,13 +4,20 @@ class DatePickerViewController: UIViewController{
 
     @IBOutlet var datePicker: UIDatePicker!
 
+    func formatDate(originalDate: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        let strDate = dateFormatter.string(from: originalDate)
+        return strDate
+
+    }
     var originalDate: Date! {
         didSet {
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "YYYY-MM-dd"
-            let strDate = dateFormatter.string(from: originalDate)
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.dateFormat = "YYYY-MM-dd"
+//            let strDate = dateFormatter.string(from: originalDate)
 
-            navigationItem.title = "original date:\(strDate)"
+            navigationItem.title = "original date:\(formatDate(originalDate: originalDate))"
         }
     }
 
@@ -18,6 +25,13 @@ class DatePickerViewController: UIViewController{
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         datePicker.date = originalDate
+    }
+
+    @IBAction func dateValueChanged(sender:UIDatePicker)
+    {
+        print("--> dateValueChanged")
+        navigationItem.title = "original date:\(formatDate(originalDate:sender.date))"
+//        dateTextField.text = sender.date.description
     }
 
 }
