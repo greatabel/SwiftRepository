77f0c58fef1b250c410573788f27c882ea0654a5
greatabel <myreceiver2for2github@gmail.com>
2017-06-07 10:58:53 +0800

    HeaderCell

--- Beginning iPhone Development with Swift 3/10Collection Views/DialogViewer/DialogViewer/ContentCell.swift
+++ Beginning iPhone Development with Swift 3/10Collection Views/DialogViewer/DialogViewer/ContentCell.swift
@@ -2,12 +2,43 @@ import UIKit
 
 class ContentCell: UICollectionViewCell {
     var label: UILabel!
-    var text: String!
+
+    var text: String! {
+        get {
+            return label.text
+        }
+        set(newText) {
+            label.text = newText
+            var newLabelFrame = label.frame
+            var newContentFrame = contentView.frame
+            let textSize = sizeForContentString(s: newText,
+                                                forMaxWidth: maxWidth)
+            newLabelFrame.size = textSize
+            newContentFrame.size = textSize
+            label.frame = newLabelFrame
+            contentView.frame = newContentFrame
+        }
+    }
+
+    
     var maxWidth: CGFont!
 
     class func sizeForContentString(s: String,
                                     forMaxWidth maxWidth: CGFont) -> CGSize {
-        return CGSize.zero
+//        return CGSize.zero
+        let maxSize = CGSize(width: maxWidth as! Double, height: 1000.0)
+        let opts = NSStringDrawingOptions.usesLineFragmentOrigin
+
+        let style = NSMutableParagraphStyle()
+        style.lineBreakMode = NSLineBreakMode.byCharWrapping
+        let attributes = [ NSFontAttributeName: defaultFont(),
+                           NSParagraphStyleAttributeName: style]
+
+        let string = s as NSString
+        let rect = string.boundingRect(with: maxSize, options: opts,
+                                       attributes: attributes, context: nil)
+
+        return rect.size
     }
 
     override init(frame: CGRect) {
@@ -30,4 +61,6 @@ class ContentCell: UICollectionViewCell {
     class func defaultFont() -> UIFont {
         return UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
     }
+
+
 }
