7c78a08ffae4665cffc9019e90e39a84aefef3ca
greatabel <myreceiver2for2github@gmail.com>
2017-06-18 22:50:59 +0800

    show black cells

--- Big Nerd Ranch/21Collection Views/Photorama/Photorama/PhotoDataSource.swift
+++ Big Nerd Ranch/21Collection Views/Photorama/Photorama/PhotoDataSource.swift
@@ -1,1 +1,19 @@
-import Foundation
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+
+    var photos = [Photo]()
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: identifier,
+                                               for: indexPath)
+        return cell }
+}
--- Big Nerd Ranch/21Collection Views/Photorama/Photorama/PhotosViewController.swift
+++ Big Nerd Ranch/21Collection Views/Photorama/Photorama/PhotosViewController.swift
@@ -2,43 +2,50 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
-    @IBOutlet var imageView: UIImageView!
+//    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var collectionView: UICollectionView!
 
     var store: PhotoStore!
+
+    let photoDataSource = PhotoDataSource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         print(#function)
+        collectionView.dataSource = photoDataSource
         store.fetchInterestingPhotos {
             (photoResult) -> Void in
             switch photoResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
 
-                if let firstPhoto = photos.first {
-                    self.updateImageView(for: firstPhoto)
-                }
+                self.photoDataSource.photos = photos
+//                if let firstPhoto = photos.first {
+////                    self.updateImageView(for: firstPhoto)
+//                }
 
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
+                self.photoDataSource.photos.removeAll()
             }
+            self.collectionView.reloadSections(IndexSet(integer: 0))
         }
         
     }
 
-    func updateImageView(for photo: Photo) {
-        store.fetchImage(for: photo) {
-            (imageResult) -> Void in
-            switch imageResult {
-            case let .success(image):
-//                self.imageView.image = image
-                print("success")
-            case let .failure(error):
-                print("Error downloading image: \(error)")
-            }
-        } }
+//    func updateImageView(for photo: Photo) {
+//        store.fetchImage(for: photo) {
+//            (imageResult) -> Void in
+//            switch imageResult {
+//            case let .success(image):
+////                self.imageView.image = image
+//                print("success")
+//            case let .failure(error):
+//                print("Error downloading image: \(error)")
+//            }
+//        } }
 
  
     
