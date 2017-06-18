0b0e913f8154f9b94aece7e12cdf9b19a08103af
greatabel <myreceiver2for2github@gmail.com>
2017-06-18 22:32:50 +0800

    ch21

---
+++ Big Nerd Ranch/21Collection Views/Photorama/Photorama/Photo.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class Photo {
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, photoID: String, remoteURL: URL, dateTaken: Date) {
+        self.title = title
+        self.photoID = photoID
+        self.remoteURL = remoteURL
+        self.dateTaken = dateTaken
+    }
+    
+
+}
---
+++ Big Nerd Ranch/21Collection Views/Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+
+    var store: PhotoStore!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print(#function)
+        store.fetchInterestingPhotos {
+            (photoResult) -> Void in
+            switch photoResult {
+            case let .success(photos):
+                print("Successfully found \(photos.count) photos.")
+
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
+
+            case let .failure(error):
+                print("Error fetching interesting photos: \(error)")
+            }
+        }
+        
+    }
+
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        } }
+
+ 
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
