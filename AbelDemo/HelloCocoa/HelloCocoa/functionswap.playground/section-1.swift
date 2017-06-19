5407ff6e0c5726584a4337e5f584183487160a35
greatabel <myreceiver2for2github@gmail.com>
2017-06-19 11:59:59 +0800

    rm some obsolete proj

--- AbelDemo/HelloCocoa/HelloCocoa/functionswap.playground/section-1.swift
+++
@@ -1,49 +0,0 @@
-// Playground - noun: a place where people can play
-
-import Cocoa
-
-func multiplyNumbers2 (firstNumber: Int, multiplier: Int = 2) -> Int { return firstNumber * multiplier;}// Parameters with default values can be omitted multiplyNumbers2(2) // = 4
-
-func swapValue(inout firstValue: Int, inout secondValue: Int) {
-    
-    let tempValue = firstValue
-    firstValue = secondValue
-    secondValue = tempValue
-}
-
-var swap1 = 1
-var swap2 = 2
-println((swap1,swap2))
-swapValue(&swap1, &swap2)
-
-println((swap1,swap2))
-
-
-var Func: ( Int, Int) -> Int;
-
-Func = multiplyNumbers2
-Func(25, 4)
-
-func timesThree( number: Int) -> Int{
-    return number * 3
-}
-
-func doSomethingToNumber(aNumber: Int ,doSomething: (Int)->Int) ->Int{
-    return doSomething(aNumber)
-}
-
-doSomethingToNumber(400, timesThree)
-
-
-func createAdder(numberToAdd: Int) -> (Int) -> Int {
-    func adder(number: Int) -> Int {    return number + numberToAdd
-    }    return adder
-}
-    var addTwo = createAdder(20)
-
-println(addTwo(80))
-println(addTwo(180))
-
-// = 4
-
-
--- AbelDemo/testSwift2version/testSwift2version/testSwift2version-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <UIKit/UIKit.h>
-#import "JBChartView.h"
-#import "JBBarChartView.h"
-#import "JBLineChartView.h"
