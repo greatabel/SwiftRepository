49365d2da345b4757d553257cc70914e8c04d538
greatabel <myreceiver2for2github@gmail.com>
2017-07-05 16:12:26 +0800

    beginning ch22

---
+++ Big Nerd Ranch/22Core Data/Photorama/Photorama/FlickrAPI.swift
@@ -0,0 +1,104 @@
+import Foundation
+
+enum FlickrError: Error {
+    case invalidJSONData
+}
+
+enum Method: String {
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let apiKey = "a6d819499131071f158fd740860a5a88"
+
+    private static let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter
+    }()
+
+    static var interestingPhotosURL: URL {
+        return flickrURL(method: .interestingPhotos,
+                         parameters: ["extras": "url_h,date_taken"])
+    }
+
+    private static func flickrURL(method: Method,
+                                  parameters: [String:String]?) -> URL {
+        var components = URLComponents(string: baseURLString)!
+
+        var queryItems = [URLQueryItem]()
+
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": apiKey
+        ]
+
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+
+        return components.url!
+    }
+
+    static func photos(fromJSON data: Data) -> PhotosResult {
+
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+
+            guard
+                let jsonDictionary = jsonObject as? [AnyHashable:Any],
+                let photos = jsonDictionary["photos"] as? [String:Any],
+                let photosArray = photos["photo"] as? [[String:Any]] else {
+
+                    // The JSON structure doesn't match our expectations
+                    return .failure(FlickrError.invalidJSONData)
+            }
+
+            var finalPhotos = [Photo]()
+            for photoJSON in photosArray {
+                if let photo = photo(fromJSON: photoJSON) {
+                    finalPhotos.append(photo)
+                }
+            }
+
+            if finalPhotos.isEmpty && !photosArray.isEmpty {
+                // We weren't able to parse any of the photos.
+                // Maybe the JSON format for photos has changed.
+                return .failure(FlickrError.invalidJSONData)
+            }
+            return .success(finalPhotos)
+        } catch let error {
+            return .failure(error)
+        }
+    }
+
+    private static func photo(fromJSON json: [String : Any]) -> Photo? {
+        guard
+            let photoID = json["id"] as? String,
+            let title = json["title"] as? String,
+            let dateString = json["datetaken"] as? String,
+            let photoURLString = json["url_h"] as? String,
+            let url = URL(string: photoURLString),
+            let dateTaken = dateFormatter.date(from: dateString) else {
+
+                // Don't have enough information to construct a Photo
+                return nil
+        }
+
+        return Photo(title: title, photoID: photoID, remoteURL: url, dateTaken: dateTaken)
+    }
+
+}
---
+++ Big Nerd Ranch/22Core Data/Photorama/Photorama/PhotoStore.swift
@@ -0,0 +1,120 @@
+import Foundation
+import UIKit
+
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
+
+enum PhotosResult {
+    case success([Photo])
+    case failure(Error)
+}
+
+class PhotoStore {
+
+    let imageStore = ImageStore()
+
+    private let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        return URLSession(configuration: config)
+    }()
+
+    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+        guard let jsonData = data else {
+            return .failure(error!)
+        }
+        return FlickrAPI.photos(fromJSON: jsonData)
+    }
+
+    func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+
+                // Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+
+        return .success(image)
+    }
+
+    func fetchInterestingPhotos(completion: @escaping (PhotosResult) -> Void) {
+        let url = FlickrAPI.interestingPhotosURL
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+//            if let jsonData = data {
+////                if let jsonString = String(data: jsonData, encoding: .utf8) {
+////                                           print(jsonString)
+////                }
+//                do {
+//                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+//                    print(jsonObject)
+//                } catch let error {
+//                    print("Error creating JSON object: \(error)")
+//                }
+//
+//            } else if let requestError = error {
+//                print("Error fetching interesting photos: \(requestError)")
+//            } else {
+//                print("Unexpected error with the request")
+//            }
+
+
+
+            // excercise a:
+//            let httpResponse = response as! HTTPURLResponse
+//            print("response ->", httpResponse.statusCode, httpResponse.allHeaderFields)
+
+            let result = self.processPhotosRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+
+        }
+        
+        task.resume()
+    }
+
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+
+        let photoKey = photo.photoID
+        print("photoKey = \(photoKey)")
+        if let image = imageStore.image(forKey: photoKey){
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+
+        let photoURL = photo.remoteURL
+        print("photoURL->", photoURL)
+        let request = URLRequest(url: photoURL)
+
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+
+            let result = self.processImageRequest(data: data, error: error)
+
+            if case let .success(image) = result {
+                self.imageStore.setImage(image, forKey: photoKey)
+            }
+
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+
+}
