c83534ce2244a7bf5d5b9c1501b27babe6397ddb
greatabel <myreceiver2for2github@gmail.com>
2017-07-05 17:36:11 +0800

    init ch12

---
+++ Beginning iPhone Development with Swift 3/12Bridge Controll/12Bridge Controll/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  12Bridge Controll
+//
+//  Created by 万畅 on 2017/7/5.
+//  Copyright © 2017年 luminagic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
