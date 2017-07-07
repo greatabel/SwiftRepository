aa6487d7ff6e0cb11b0d05db124aa490b0eaff0b
greatabel <myreceiver2for2github@gmail.com>
2017-07-07 14:35:05 +0800

    2nd tab setting

--- Beginning iPhone Development with Swift 3/12Bridge Controll/12Bridge Controll/SecondViewController.swift
+++ Beginning iPhone Development with Swift 3/12Bridge Controll/12Bridge Controll/SecondViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet var engineSwitch:UISwitch!
+    
+    @IBOutlet var warpFactorSlider:UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -10,6 +14,26 @@ class SecondViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+    }
+
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        engineSwitch.isOn = defaults.bool(forKey: warpDriveKey)
+        warpFactorSlider.value = defaults.float(forKey: warpFactorKey)
+    }
+
+    @IBAction func onEngineSwitchTapped(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(engineSwitch.isOn, forKey: warpDriveKey)
+    }
+    @IBAction func onWarpSliderDragged(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(warpFactorSlider.value, forKey: warpFactorKey)
     }
 
 
