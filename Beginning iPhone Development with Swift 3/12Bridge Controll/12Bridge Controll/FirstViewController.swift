955b05dcbbab0d431119223acb71ce8c65f3b74b
greatabel <myreceiver2for2github@gmail.com>
2017-07-07 14:19:05 +0800

    show settings at 1st tab

--- Beginning iPhone Development with Swift 3/12Bridge Controll/12Bridge Controll/FirstViewController.swift
+++ Beginning iPhone Development with Swift 3/12Bridge Controll/12Bridge Controll/FirstViewController.swift
@@ -23,6 +23,25 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        officerLabel.text = defaults.string(forKey: officerKey)
+        authorizationCodeLabel.text = defaults.string(forKey: authorizationCodeKey)
+        rankLabel.text = defaults.string(forKey: rankKey)
+        warpDriveLabel.text = defaults.bool(forKey: warpDriveKey)
+            ? "Engaged" : "Disabled"
+        warpFactorLabel.text = (defaults.object(forKey: warpFactorKey) as AnyObject).stringValue
+        favoriteTeaLabel.text = defaults.string(forKey: favoriteTeaKey)
+        favoriteCaptainLabel.text = defaults.string(forKey: favoriteCaptainKey)
+        favoriteGadgetLabel.text = defaults.string(forKey: favoriteGadgetKey)
+        favoriteAlienLabel.text = defaults.string(forKey: favoriteAlienKey)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        refreshFields()
+    }
+
 
 }
 
