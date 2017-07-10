906b4b5c8633fde22dc4e69b68cf963dae3064da
greatabel <myreceiver2for2github@gmail.com>
2017-07-10 23:03:27 +0800

    13Persistence

---
+++ Beginning iPhone Development with Swift 3/13Persistence/13Persistence/ViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lineFields: [UITextField]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
