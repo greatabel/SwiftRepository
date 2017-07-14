84949d7f4bbadccd56dd225f21204dd3ce823e57
greatabel <myreceiver2for2github@gmail.com>
2017-07-14 15:31:30 +0800

    fix swift3 UnsafePointer

--- Beginning iPhone Development with Swift 3/13Persistence SQLite/13Persistence/ViewController.swift
+++ Beginning iPhone Development with Swift 3/13Persistence SQLite/13Persistence/ViewController.swift
@@ -1,5 +1,15 @@
 import UIKit
 
+extension String {
+    public init?(validatingUTF8 cString: UnsafePointer<UInt8>) {
+        guard let (s, _) = String.decodeCString(cString, as: UTF8.self,
+                                                repairingInvalidCodeUnits: false) else {
+                                                    return nil
+        }
+        self = s
+    }
+}
+
 class ViewController: UIViewController {
 
     fileprivate static let rootKey = "rootKey"
@@ -25,28 +35,42 @@ class ViewController: UIViewController {
             print("Failed to create table")
             return
         }
-
-        let fileURL = self.dataFileURL()
-        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
-            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
-                for i in 0..<array.count {
-                    lineFields[i].text = array[i]
-                }
+        let query = "SELECT ROW, FIELD_DATA FROM FIELDS ORDER BY ROW"
+        var statement:OpaquePointer? = nil
+        if sqlite3_prepare_v2(database, query, -1, &statement, nil) == SQLITE_OK {
+            while sqlite3_step(statement) == SQLITE_ROW {
+                let row = Int(sqlite3_column_int(statement, 0))
+                let rowData = sqlite3_column_text(statement, 1)
+//                let fieldValue = String.init(cString: UnsafePointer<CChar>(rowData!))
+//                lineFields[row].text = fieldValue
+// https://stackoverflow.com/questions/25168030/issue-with-unsafepointeruint8-in-sqlite-project-in-swift
+                let value = String(cString: rowData!)
+                lineFields[row].text = value
             }
-
-            let data = NSMutableData(contentsOf: fileURL as URL)
-            let unarchiver = NSKeyedUnarchiver(forReadingWith: data! as Data)
-            let fourLines = unarchiver.decodeObject(forKey: ViewController.rootKey) as! FourLines
-            unarchiver.finishDecoding()
-
-            if let newLines = fourLines.lines {
-                for i in 0..<newLines.count {
-                    lineFields[i].text = newLines[i]
-                }
-            }
-
-
+            sqlite3_finalize(statement)
         }
+
+//        let fileURL = self.dataFileURL()
+//        if (FileManager.default.fileExists(atPath: fileURL.path!)) {
+//            if let array = NSArray(contentsOf: fileURL as URL) as? [String] {
+//                for i in 0..<array.count {
+//                    lineFields[i].text = array[i]
+//                }
+//            }
+//
+//            let data = NSMutableData(contentsOf: fileURL as URL)
+//            let unarchiver = NSKeyedUnarchiver(forReadingWith: data! as Data)
+//            let fourLines = unarchiver.decodeObject(forKey: ViewController.rootKey) as! FourLines
+//            unarchiver.finishDecoding()
+//
+//            if let newLines = fourLines.lines {
+//                for i in 0..<newLines.count {
+//                    lineFields[i].text = newLines[i]
+//                }
+//            }
+//
+//
+//        }
 
         let app = UIApplication.shared
         NotificationCenter.default.addObserver(self,
@@ -67,17 +91,44 @@ class ViewController: UIViewController {
         return url!
     }
 
+//    func applicationWillResignActive(notification:NSNotification) {
+//        let fileURL = self.dataFileURL()
+//        let fourLines = FourLines()
+//        let array = (self.lineFields as NSArray).value(forKey: "text")
+//            as! [String]
+//        fourLines.lines = array
+//        let data = NSMutableData()
+//        let archiver = NSKeyedArchiver(forWritingWith: data)
+//        archiver.encode(fourLines, forKey: ViewController.rootKey)
+//        archiver.finishEncoding()
+//        data.write(to: fileURL as URL, atomically: true)
+//    }
     func applicationWillResignActive(notification:NSNotification) {
-        let fileURL = self.dataFileURL()
-        let fourLines = FourLines()
-        let array = (self.lineFields as NSArray).value(forKey: "text")
-            as! [String]
-        fourLines.lines = array
-        let data = NSMutableData()
-        let archiver = NSKeyedArchiver(forWritingWith: data)
-        archiver.encode(fourLines, forKey: ViewController.rootKey)
-        archiver.finishEncoding()
-        data.write(to: fileURL as URL, atomically: true)
+        var database:OpaquePointer? = nil
+        let result = sqlite3_open(dataFilePath(), &database)
+        if result != SQLITE_OK {
+            sqlite3_close(database)
+            print("Failed to open database")
+            return
+        }
+        for i in 0..<lineFields.count  {
+            let field = lineFields[i]
+            let update = "INSERT OR REPLACE INTO FIELDS (ROW, FIELD_DATA) " +
+            "VALUES (?, ?);"
+            var statement:OpaquePointer? = nil
+            if sqlite3_prepare_v2(database, update, -1, &statement, nil) == SQLITE_OK {
+                let text = field.text
+                sqlite3_bind_int(statement, 1, Int32(i))
+                sqlite3_bind_text(statement, 2, text!, -1, nil)
+            }
+            if sqlite3_step(statement) != SQLITE_DONE {
+                print("Error updating table")
+                sqlite3_close(database)
+                return
+            }
+            sqlite3_finalize(statement)
+        }
+        sqlite3_close(database)
     }
 
 
