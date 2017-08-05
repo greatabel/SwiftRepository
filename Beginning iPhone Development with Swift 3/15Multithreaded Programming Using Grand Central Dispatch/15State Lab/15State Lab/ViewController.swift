40a82753cbb78688f84a3956254b429a28b05883
greatabel <myreceiver2for2github@gmail.com>
2017-08-05 21:53:02 +0800

    rotateLabelDown()

--- Beginning iPhone Development with Swift 3/15Multithreaded Programming Using Grand Central Dispatch/15State Lab/15State Lab/ViewController.swift
+++ Beginning iPhone Development with Swift 3/15Multithreaded Programming Using Grand Central Dispatch/15State Lab/15State Lab/ViewController.swift
@@ -2,15 +2,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let bounds = view.bounds
+        let labelFrame = CGRect(origin: CGPoint(x: bounds.origin.x, y:bounds.midY - 50),
+                                size: CGSize(width: bounds.size.width, height: 100))
+        label = UILabel(frame: labelFrame)
+        label.font = UIFont(name: "Helvetica", size:70)
+        label.text = "嘿嘿!"
+        label.textAlignment = NSTextAlignment.center
+        label.backgroundColor = UIColor.clear
+        view.addSubview(label)
+        rotateLabelDown();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    func rotateLabelDown() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.label.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi))
+        },
+                       completion: {(Bool) -> Void in
+                        self.rotateLabelUp()
+        } )
+    }
+
+    func rotateLabelUp() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.label.transform = CGAffineTransform(rotationAngle: 0)
+        },
+                       completion: {(Bool) -> Void in
+                        self.rotateLabelDown()
+        } )
+    }
+
 
 
 }
