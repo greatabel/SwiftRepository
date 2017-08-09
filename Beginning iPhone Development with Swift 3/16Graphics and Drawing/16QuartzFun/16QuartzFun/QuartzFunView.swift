4c168f6c6897fda8af08440aedf2c9902f10b75e
greatabel <myreceiver2for2github@gmail.com>
2017-08-09 08:02:27 +0800

    randomColor()

--- Beginning iPhone Development with Swift 3/16Graphics and Drawing/16QuartzFun/16QuartzFun/QuartzFunView.swift
+++ Beginning iPhone Development with Swift 3/16Graphics and Drawing/16QuartzFun/16QuartzFun/QuartzFunView.swift
@@ -1,5 +1,14 @@
 import UIKit
 
+extension UIColor {
+    class func randomColor() -> UIColor
+    {
+        let red = CGFloat(Double(arc4random_uniform(255))/255)
+        let green = CGFloat(Double(arc4random_uniform(255))/255)
+        let blue = CGFloat(Double(arc4random_uniform(255))/255)
+        return UIColor(red: red, green: green, blue: blue, alpha:1.0)
+    }
+}
 class QuartzFunView: UIView {
 
     /*
