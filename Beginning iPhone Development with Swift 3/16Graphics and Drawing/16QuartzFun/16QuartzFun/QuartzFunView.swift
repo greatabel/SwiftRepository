2061f0665f46449051052dde699aa68d60961cfa
greatabel <myreceiver2for2github@gmail.com>
2017-08-10 17:37:24 +0800

    Optimizing

--- Beginning iPhone Development with Swift 3/16Graphics and Drawing/16QuartzFun/16QuartzFun/QuartzFunView.swift
+++ Beginning iPhone Development with Swift 3/16Graphics and Drawing/16QuartzFun/16QuartzFun/QuartzFunView.swift
@@ -28,6 +28,8 @@ class QuartzFunView: UIView {
     }
     */
 
+
+
     // Application-settable properties
     var shape = Shape.line
     var currentColor = UIColor.red
@@ -54,14 +56,36 @@ class QuartzFunView: UIView {
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             lastTouchLocation = touch.location(in: self)
-            setNeedsDisplay()
+//            setNeedsDisplay()
+            if shape == .image {
+                let horizontalOffset = image!.size.width / 2
+                let verticalOffset = image!.size.height / 2
+                redrawRect = redrawRect.union(CGRect(x: lastTouchLocation.x - horizontalOffset,
+                                                     y: lastTouchLocation.y - verticalOffset,
+                                                     width: image!.size.width,
+                                                     height: image!.size.height))
+            } else {
+                redrawRect = redrawRect.union(currentRect())
+            }
+            setNeedsDisplay(redrawRect)
         }
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             lastTouchLocation = touch.location(in: self)
-            setNeedsDisplay()
+
+            if shape == .image {
+                let horizontalOffset = image!.size.width / 2
+                let verticalOffset = image!.size.height / 2
+                redrawRect = redrawRect.union(CGRect(x: lastTouchLocation.x - horizontalOffset,
+                                                     y: lastTouchLocation.y - verticalOffset,
+                                                     width: image!.size.width,
+                                                     height: image!.size.height))
+            } else {
+                redrawRect = redrawRect.union(currentRect())
+            }
+            setNeedsDisplay(redrawRect)
         }
     }
 
@@ -70,10 +94,10 @@ class QuartzFunView: UIView {
         context!.setLineWidth(2.0)
         context!.setStrokeColor(currentColor.cgColor)
 
-        let currentRect = CGRect(x: firstTouchLocation.x,
-                                 y: firstTouchLocation.y,
-                                 width: lastTouchLocation.x - firstTouchLocation.x,
-                                 height: lastTouchLocation.y - firstTouchLocation.y)
+//        let currentRect = CGRect(x: firstTouchLocation.x,
+//                                 y: firstTouchLocation.y,
+//                                 width: lastTouchLocation.x - firstTouchLocation.x,
+//                                 height: lastTouchLocation.y - firstTouchLocation.y)
 
 
         switch shape {
@@ -82,15 +106,15 @@ class QuartzFunView: UIView {
             context?.addLine(to: CGPoint(x: lastTouchLocation.x, y: lastTouchLocation.y))
             context?.strokePath()
         case .rect:
-            context?.addRect(currentRect)
+            context?.addRect(currentRect())
             // 设置下方形背景颜色
             context?.setFillColor(UIColor.blue.cgColor)
             context?.drawPath(using: .fillStroke)
-            
+
             context?.strokePath()
             break
         case .ellipse:
-            context?.addEllipse(in: currentRect)
+            context?.addEllipse(in: currentRect())
             context?.drawPath(using: .fillStroke)
             break
         case .image:
@@ -102,6 +126,11 @@ class QuartzFunView: UIView {
             break
         }
 
+    }
+
+    func currentRect() -> CGRect {
+        return CGRect(x: firstTouchLocation.x, y: firstTouchLocation.y,
+                      width: lastTouchLocation.x, height: lastTouchLocation.y)
     }
 
 }
