9177fe0d1806f45ca37a352ccb48fe15f8fac60e
greatabel <myreceiver2for2github@gmail.com>
2017-08-10 17:42:31 +0800

    17

---
+++ Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooterTests/_7TextShooterTests.swift
@@ -0,0 +1,36 @@
+//
+//  _7TextShooterTests.swift
+//  17TextShooterTests
+//
+//  Created by 万畅 on 2017/8/10.
+//  Copyright © 2017年 luminagic. All rights reserved.
+//
+
+import XCTest
+@testable import _7TextShooter
+
+class _7TextShooterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
