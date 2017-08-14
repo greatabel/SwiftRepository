bd744fecef504e372f910ec6bde8240d6da25d15
greatabel <myreceiver2for2github@gmail.com>
2017-08-14 16:07:16 +0800

    tags <--> photo

--- Big Nerd Ranch/23Core Data Relationships/Photorama/Photo+CoreDataProperties.swift
+++ Big Nerd Ranch/23Core Data Relationships/Photorama/Photo+CoreDataProperties.swift
@@ -8,9 +8,27 @@ extension Photo {
         return NSFetchRequest<Photo>(entityName: "Photo")
     }
 
-    @NSManaged public var photoID: String?
-    @NSManaged public var title: String?
-    @NSManaged public var remoteURL: NSObject?
     @NSManaged public var dateToken: NSDate?
+    @NSManaged public var photoID: String?
+    @NSManaged public var remoteURL: NSObject?
+    @NSManaged public var title: String?
+    @NSManaged public var tags: NSSet?
+
+}
+
+// MARK: Generated accessors for tags
+extension Photo {
+
+    @objc(addTagsObject:)
+    @NSManaged public func addToTags(_ value: Tag)
+
+    @objc(removeTagsObject:)
+    @NSManaged public func removeFromTags(_ value: Tag)
+
+    @objc(addTags:)
+    @NSManaged public func addToTags(_ values: NSSet)
+
+    @objc(removeTags:)
+    @NSManaged public func removeFromTags(_ values: NSSet)
 
 }
