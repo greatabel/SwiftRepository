71d9565f4e66e6530f0472dfee5b3f1d9a11e4d3
greatabel <myreceiver2for2github@gmail.com>
2017-08-15 14:08:28 +0800

    backgournd task

--- Big Nerd Ranch/23Core Data Relationships/Photorama/Photorama/PhotoStore.swift
+++ Big Nerd Ranch/23Core Data Relationships/Photorama/Photorama/PhotoStore.swift
@@ -40,11 +40,37 @@ class PhotoStore {
         return URLSession(configuration: config)
     }()
 
-    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+    private func processPhotosRequest(data: Data?,
+                                      error: Error?,
+                                      completion: @escaping (PhotosResult) -> Void) {
         guard let jsonData = data else {
-            return .failure(error!)
+            completion(.failure(error!))
+            return
         }
-        return FlickrAPI.photos(fromJSON: jsonData, into: persistentContainer.viewContext)
+//        return FlickrAPI.photos(fromJSON: jsonData, into: persistentContainer.viewContext)
+        persistentContainer.performBackgroundTask{
+            (context) in
+
+            let result = FlickrAPI.photos(fromJSON: jsonData, into: context)
+            do {
+                try context.save()
+            } catch {
+                print("Error saving to Core Data: \(error).")
+                completion(.failure(error))
+                return 
+            }
+            switch result {
+            case let .success(photos):
+                let photoIDs = photos.map { return $0.objectID }
+                let viewContext = self.persistentContainer.viewContext
+                let viewContextPhotos =
+                    photoIDs.map { return viewContext.object(with: $0) } as! [Photo]
+                completion(.success(viewContextPhotos))
+            case .failure:
+                completion(result)
+            }
+
+        }
     }
 
     func processImageRequest(data: Data?, error: Error?) -> ImageResult {
@@ -94,20 +120,25 @@ class PhotoStore {
 
 
 //            let result = self.processPhotosRequest(data: data, error: error)
-            var result = self.processPhotosRequest(data: data, error: error)
-
-            if case .success = result {
-                do {
-                    try self.persistentContainer.viewContext.save()
-                } catch let error {
-                    result = .failure(error)
+//            var result = self.processPhotosRequest(data: data, error: error)
+//
+//            if case .success = result {
+//                do {
+//                    try self.persistentContainer.viewContext.save()
+//                } catch let error {
+//                    result = .failure(error)
+//                }
+//            }
+//
+//            OperationQueue.main.addOperation {
+//                completion(result)
+//            }
+            self.processPhotosRequest(data: data, error: error) {
+                (result) in
+                OperationQueue.main.addOperation {
+                    completion(result)
                 }
             }
-
-            OperationQueue.main.addOperation {
-                completion(result)
-            }
-
         }
         
         task.resume()
