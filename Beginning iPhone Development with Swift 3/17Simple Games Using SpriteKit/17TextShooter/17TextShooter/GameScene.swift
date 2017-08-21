cdc96cfb4948ff6cda2aac62857fd0f1dc307bbf
greatabel <myreceiver2for2github@gmail.com>
2017-08-21 15:16:58 +0800

    scene by code

--- Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/GameScene.swift
+++ Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/GameScene.swift
@@ -2,80 +2,64 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
+    private var levelNumber: Int
+    private var playerLives: Int
+    private var finished = false
+
+    class func scene(size:CGSize, levelNumber:Int) -> GameScene {
+        return GameScene(size: size, levelNumber: levelNumber)
+    }
+
+    override convenience init(size:CGSize) {
+        self.init(size: size, levelNumber: 1)
+    }
+
+    init(size:CGSize, levelNumber:Int) {
+        self.levelNumber = levelNumber
+        self.playerLives = 5
+        super.init(size: size)
+        backgroundColor = SKColor.lightGray
+        let lives = SKLabelNode(fontNamed: "Courier")
+        lives.fontSize = 16
+        lives.fontColor = SKColor.black
+        lives.name = "LivesLabel"
+        lives.text = "Lives: \(playerLives)"
+        lives.verticalAlignmentMode = .top
+        lives.horizontalAlignmentMode = .right
+        lives.position = CGPoint(x: frame.size.width,
+                                 y: frame.size.height)
+        addChild(lives)
+        let level = SKLabelNode(fontNamed: "Courier")
+        level.fontSize = 16
+        level.fontColor = SKColor.black
+        level.name = "LevelLabel"
+        level.text = "Level \(levelNumber)"
+        level.verticalAlignmentMode = .top
+        level.horizontalAlignmentMode = .left
+        level.position = CGPoint(x: 0, y: frame.height)
+        addChild(level)
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        levelNumber = aDecoder.decodeInteger(forKey: "level")
+        playerLives = aDecoder.decodeInteger(forKey: "playerLives")
+        super.init(coder: aDecoder)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    override func encode(with aCoder: NSCoder) {
+        aCoder.encode(Int(levelNumber), forKey: "level")
+        aCoder.encode(playerLives, forKey: "playerLives")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+
+        for touch in touches {
+            let location = touch.location(in: self)
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
+
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
 }
--- Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/GameViewController.swift
+++ Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/GameViewController.swift
@@ -7,21 +7,17 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let scene = GameScene(size: view.frame.size, levelNumber: 1)
+
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+
+        skView.ignoresSiblingOrder = true
+
+        scene.scaleMode = .aspectFill
+
+        skView.presentScene(scene)
     }
 
     override var shouldAutorotate: Bool {
