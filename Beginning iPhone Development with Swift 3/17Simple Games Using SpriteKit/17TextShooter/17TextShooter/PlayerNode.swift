eb6c87d2d25bc5b5012b14734c015d326d123822
greatabel <myreceiver2for2github@gmail.com>
2017-08-21 15:40:37 +0800

    add wobble

--- Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/PlayerNode.swift
+++ Beginning iPhone Development with Swift 3/17Simple Games Using SpriteKit/17TextShooter/17TextShooter/PlayerNode.swift
@@ -30,6 +30,14 @@ class PlayerNode: SKNode {
         let duration = TimeInterval(2 * distance / screenWidth)
 
         run(SKAction.move(to: location, duration: duration), withKey: "movement")
+
+        let wobbleTime = 0.3
+        let halfWobbleTime = wobbleTime / 2
+        let wobbling = SKAction.sequence([
+            SKAction.scaleX(to: 0.2, duration: halfWobbleTime),
+            SKAction.scaleX(to: 1.0, duration: halfWobbleTime)])
+        let wobbleCount = Int(duration/wobbleTime)
+        run(SKAction.repeat(wobbling, count: wobbleCount), withKey: "wobbling")
     }
 
 }
