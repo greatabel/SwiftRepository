a3543f69a8eda10824fdda5a6fca017c1595c5b9
greatabel <myreceiver2for2github@gmail.com>
2017-08-24 16:03:26 +0800

    format

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/AppDelegate.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  18TouchExplorer
-//
-//  Created by 万畅 on 2017/8/24.
-//  Copyright © 2017年 luminagic. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/ViewController.swift
@@ -1,14 +1,11 @@
-//
-//  ViewController.swift
-//  18TouchExplorer
-//
-//  Created by 万畅 on 2017/8/24.
-//  Copyright © 2017年 luminagic. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var tapsLabel: UILabel!
+    @IBOutlet var touchesLabel: UILabel!
+    @IBOutlet var forceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
