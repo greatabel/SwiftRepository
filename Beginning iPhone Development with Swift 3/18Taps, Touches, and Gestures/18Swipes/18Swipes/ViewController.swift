f54d2b0a988abd9e7e941fe59c5c62a070e3ffa6
greatabel <myreceiver2for2github@gmail.com>
2017-08-25 17:51:04 +0800

    UI and connects

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Swipes/18Swipes/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Swipes/18Swipes/ViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    
+    private var gestureStartPoint: CGPoint!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
