0431b272c68cb2030d62804c51bbb95b773fa6bb
greatabel <myreceiver2for2github@gmail.com>
2017-08-25 17:37:24 +0800

    start begin

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18TouchExplorer/18TouchExplorer/ViewController.swift
@@ -17,6 +17,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func updateLabelsFromTouches(_ touch: UITouch?, allTouches: Set<UITouch>?) {
+        let numTaps = touch?.tapCount ?? 0
+        let tapsMessage = "\(numTaps) taps detected"
+        tapsLabel.text = tapsMessage
+
+        let numTouches = allTouches?.count ?? 0
+        let touchMsg = "\(numTouches) touches detected"
+        touchesLabel.text = touchMsg
+
+        if traitCollection.forceTouchCapability == .available {
+            forceLabel.text = "Force: \(touch?.force ?? 0)\nMax force: \(touch?.maximumPossibleForce ?? 0)"
+        } else {
+            forceLabel.text = "3D Touch not available"
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        messageLabel.text = "Touches Began"
+        updateLabelsFromTouches(touches.first, allTouches: event?.allTouches)
+    }
 
 }
 
