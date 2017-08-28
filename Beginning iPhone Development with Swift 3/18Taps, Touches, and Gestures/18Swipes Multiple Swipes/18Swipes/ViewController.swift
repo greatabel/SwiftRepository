260d7ed812b5d23b56170b09bcaf020c8a386118
greatabel <myreceiver2for2github@gmail.com>
2017-08-28 13:32:43 +0800

    multiple touches

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Swipes Multiple Swipes/18Swipes/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Swipes Multiple Swipes/18Swipes/ViewController.swift
@@ -12,15 +12,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let vertical = UISwipeGestureRecognizer(target: self,
-                action: #selector(ViewController.reportVerticalSwipe(_:)))
-        vertical.direction = [.up, .down]
-        view.addGestureRecognizer(vertical)
+        for touchCount in 0..<5 {
+            let vertical = UISwipeGestureRecognizer(target: self,
+                                    action: #selector(ViewController.reportVerticalSwipe(_:)))
+            vertical.direction = [.up, .down]
+            vertical.numberOfTouchesRequired = touchCount
 
-        let horizontal = UISwipeGestureRecognizer(target: self,
-               action: #selector(ViewController.reportHorizontalSwipe(_:)))
-        horizontal.direction = [.left, .right]
-        view.addGestureRecognizer(horizontal)
+            view.addGestureRecognizer(vertical)
+
+            let horizontal = UISwipeGestureRecognizer(target: self,
+                                   action: #selector(ViewController.reportHorizontalSwipe(_:)))
+
+            horizontal.direction = [.left, .right]
+            horizontal.numberOfTouchesRequired = touchCount
+            view.addGestureRecognizer(horizontal)
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,11 +35,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func descriptionForTouchCount(_ touchCount: Int) -> String {
+        switch touchCount {
+        case 1:
+            return "Single"
+        case 2:
+            return "Double"
+        case 3:
+            return "Tripple"
+        case 4:
+            return "Quadruple"
+        case 5:
+            return "Quintuple"
+        default:
+            return ""
+        }
+    }
 
 
 
     func reportHorizontalSwipe(_ recognizer:UIGestureRecognizer) {
         label.text = "Horizontal swipe detected"
+        let count = descriptionForTouchCount(recognizer.numberOfTouches)
+        label.text = "\(count)-finger horizontal swipe detected"
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
             self.label.text = ""
         })
@@ -40,6 +65,8 @@ class ViewController: UIViewController {
 
     func reportVerticalSwipe(_ recognizer:UIGestureRecognizer) {
         label.text = "Vertical swipe detected"
+        let count = descriptionForTouchCount(recognizer.numberOfTouches)
+        label.text = "\(count)-finger vertical swipe detected"
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
             self.label.text = ""
         })
