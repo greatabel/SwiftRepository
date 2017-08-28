2cd37a1e810dff5d20c446f32bbbd656d2032ae2
greatabel <myreceiver2for2github@gmail.com>
2017-08-28 13:43:34 +0800

    3 labels

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Taps/18Taps/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Taps/18Taps/ViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var singleLabel:UILabel!
+    @IBOutlet var doubleLabel:UILabel!
+    @IBOutlet var tripleLabel:UILabel!
+    @IBOutlet var quadrupleLabel:UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
