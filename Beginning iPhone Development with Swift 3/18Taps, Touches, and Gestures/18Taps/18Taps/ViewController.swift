7330023f8ce06e4d905287ccfde5480ce9035e23
greatabel <myreceiver2for2github@gmail.com>
2017-08-28 13:45:57 +0800

    showText

--- Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Taps/18Taps/ViewController.swift
+++ Beginning iPhone Development with Swift 3/18Taps, Touches, and Gestures/18Taps/18Taps/ViewController.swift
@@ -17,6 +17,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func singleTap() {
+        showText("Single Tap Detected", inLabel: singleLabel)
+    }
+
+    func doubleTap() {
+        showText("Double Tap Detected", inLabel: doubleLabel)
+    }
+
+    func tripleTap() {
+        showText("Triple Tap Detected", inLabel: tripleLabel)
+    }
+
+    func quadrupleTap() {
+        showText("Quadruple Tap Detected", inLabel: quadrupleLabel)
+    }
+    
+    private func showText(_ text: String, inLabel label: UILabel) {
+        label.text = text
+        DispatchQueue.main.asyncAfter(deadline:
+            DispatchTime.now() + Double(Int64(2 * NSEC_PER_SEC)) / Double(NSEC_PER_SEC), execute: {
+            label.text = ""
+        })
+    }
+
 
 }
 
