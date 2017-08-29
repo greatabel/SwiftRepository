9e7031b16bca621464751b71afca40807ab0f1d0
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 11:34:44 +0800

    locationManager

--- Beginning iPhone Development with Swift 3/19Determing Location/19WhereAmI/19WhereAmI/ViewController.swift
+++ Beginning iPhone Development with Swift 3/19Determing Location/19WhereAmI/19WhereAmI/ViewController.swift
@@ -46,6 +46,75 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
     }
 
+    func locationManager(_ manager: CLLocationManager,
+                         didFailWithError error: Error) {
+        let errorType = error._code == CLError.denied.rawValue
+            ? "Access Denied": "Error \(error._code)"
+        let alertController = UIAlertController(title: "Location Manager Error",
+                                                message: errorType, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel,
+                                     handler: { action in })
+        alertController.addAction(okAction)
+        present(alertController, animated: true,
+                completion: nil)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations
+        locations: [CLLocation]) {
+        if let newLocation = locations.last {
+            let latitudeString = String(format: "%g\u{00B0}",
+                                        newLocation.coordinate.latitude)
+            latitudeLabel.text = latitudeString
+
+            let longitudeString = String(format: "%g\u{00B0}",
+                                         newLocation.coordinate.longitude)
+            longitudeLabel.text = longitudeString
+
+            let horizontalAccuracyString = String(format:"%gm",
+                                                  newLocation.horizontalAccuracy)
+            horizontalAccuracyLabel.text = horizontalAccuracyString
+
+            let altitudeString = String(format:"%gm", newLocation.altitude)
+            altitudeLabel.text = altitudeString
+
+            let verticalAccuracyString = String(format:"%gm",
+                                                newLocation.verticalAccuracy)
+            verticalAccuracyLabel.text = verticalAccuracyString
+
+            if newLocation.horizontalAccuracy < 0 {
+                // invalid accuracy
+                return
+            }
+
+
+            if newLocation.horizontalAccuracy > 100 ||
+                newLocation.verticalAccuracy > 50 {
+                // accuracy radius is so large, we don't want to use it
+                return
+            }
+
+            if previousPoint == nil {
+                totalMovementDistance = 0
+//                let start = Place(title:"Start Point",
+//                                  subtitle:"This is where we started",
+//                                  coordinate:newLocation.coordinate)
+//                mapView.addAnnotation(start)
+                let region = MKCoordinateRegionMakeWithDistance(newLocation.coordinate,
+                                                                100, 100)
+                mapView.setRegion(region, animated: true)
+            } else {
+                print("movement distance: " +
+                    "\(newLocation.distance(from: previousPoint!))")
+                totalMovementDistance +=
+                    newLocation.distance(from: previousPoint!)
+            }
+            previousPoint = newLocation
+
+            let distanceString = String(format:"%gm", totalMovementDistance)
+            distanceTraveledLabel.text = distanceString
+        }
+    }
+
 
 }
 
