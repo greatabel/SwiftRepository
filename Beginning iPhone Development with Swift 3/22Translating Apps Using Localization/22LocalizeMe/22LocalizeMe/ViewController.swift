f526ec5699386b1fa3aa2622cbbedcc8df56983e
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 23:04:50 +0800

    finish original proj

--- Beginning iPhone Development with Swift 3/22Translating Apps Using Localization/22LocalizeMe/22LocalizeMe/ViewController.swift
+++ Beginning iPhone Development with Swift 3/22Translating Apps Using Localization/22LocalizeMe/22LocalizeMe/ViewController.swift
@@ -9,6 +9,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let locale = Locale.current
+        _ = Locale.preferredLanguages[0]
+        let displayLang = locale.languageCode
+        let capitalized = displayLang?.capitalized(with: locale)
+        localeLabel.text = capitalized
+
+        labels[0].text = NSLocalizedString("LABEL_ONE", comment: "The number 1")
+        labels[1].text = NSLocalizedString("LABEL_TWO", comment: "The number 2")
+        labels[2].text = NSLocalizedString("LABEL_THREE", comment: "The number 3")
+        labels[3].text = NSLocalizedString("LABEL_FOUR", comment: "The number 4")
+        labels[4].text = NSLocalizedString("LABEL_FIVE", comment: "The number 5")
+
+        let flagFile = NSLocalizedString("FLAG_FILE", comment: "Name of the flag")
+        flagImageView.image = UIImage(named: flagFile)
     }
 
     override func didReceiveMemoryWarning() {
