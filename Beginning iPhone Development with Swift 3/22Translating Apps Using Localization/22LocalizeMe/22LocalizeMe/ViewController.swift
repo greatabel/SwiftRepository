8d15aee1708e65aee0ea1f42b462c4d6a9b7d4bf
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 22:49:18 +0800

    labels binds

--- Beginning iPhone Development with Swift 3/22Translating Apps Using Localization/22LocalizeMe/22LocalizeMe/ViewController.swift
+++ Beginning iPhone Development with Swift 3/22Translating Apps Using Localization/22LocalizeMe/22LocalizeMe/ViewController.swift
@@ -2,6 +2,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var localeLabel : UILabel!
+    @IBOutlet var flagImageView : UIImageView!
+    @IBOutlet var labels : [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
