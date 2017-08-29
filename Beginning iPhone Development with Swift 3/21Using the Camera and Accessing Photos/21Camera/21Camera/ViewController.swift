d221718132dbac90a3860f24d424eda58c87db7d
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 16:32:03 +0800

    UI

--- Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
+++ Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
@@ -2,6 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var takePictureButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
