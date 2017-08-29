9bf790049cd959640611ec8a99a029d77f09fc33
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 15:38:22 +0800

    UI

--- Beginning iPhone Development with Swift 3/20Device Orientation and Motion/20MotionMonitor/20MotionMonitor/ViewController.swift
+++ Beginning iPhone Development with Swift 3/20Device Orientation and Motion/20MotionMonitor/20MotionMonitor/ViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
+import CoreMotion
+
 
 class ViewController: UIViewController {
+
+    @IBOutlet var gyroscopeLabel: UILabel!
+    @IBOutlet var accelerometerLabel: UILabel!
+    @IBOutlet var attitudeLabel: UILabel!
+    private let motionManager = CMMotionManager()
+    private let queue = OperationQueue()
 
     override func viewDidLoad() {
         super.viewDidLoad()
