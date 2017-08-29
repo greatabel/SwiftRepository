171d60bb9ca340f2c7e80cc3dcbdbb2d6ff46003
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 15:32:10 +0800

    20Orientation

---
+++ Beginning iPhone Development with Swift 3/20Device Orientation and Motion/20MotionMonitor/20MotionMonitorTests/_0MotionMonitorTests.swift
@@ -0,0 +1,36 @@
+//
+//  _0MotionMonitorTests.swift
+//  20MotionMonitorTests
+//
+//  Created by 万畅 on 2017/8/29.
+//  Copyright © 2017年 luminagic. All rights reserved.
+//
+
+import XCTest
+@testable import _0MotionMonitor
+
+class _0MotionMonitorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
