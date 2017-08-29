4aab5490dd486919c356f91a7067691a8756e29e
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 16:36:58 +0800

    UIImagePickerControllerDelegate

--- Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
+++ Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
@@ -1,20 +1,41 @@
 import UIKit
+import AVKit
+import AVFoundation
+import MobileCoreServices
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+        UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var takePictureButton: UIButton!
+
+    var avPlayerViewController: AVPlayerViewController!
+    var image: UIImage?
+    var movieURL: URL?
+    var lastChosenMediaType: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func imagePickerController(_ picker: UIImagePickerController,
+                                       didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        lastChosenMediaType = info[UIImagePickerControllerMediaType] as? String
+        if let mediaType = lastChosenMediaType {
+            if mediaType == (kUTTypeImage as NSString) as String {
+                image = info[UIImagePickerControllerEditedImage] as? UIImage
+            } else if mediaType == (kUTTypeMovie as NSString) as String {
+                movieURL = info[UIImagePickerControllerMediaURL] as? URL
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
+    }
 
 }
 
