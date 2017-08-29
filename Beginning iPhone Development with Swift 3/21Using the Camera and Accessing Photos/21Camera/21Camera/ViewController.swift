ed94d7620a403691c5f906dd1543b9a60bfaed47
greatabel <myreceiver2for2github@gmail.com>
2017-08-29 16:40:12 +0800

    take pic or choose

--- Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
+++ Beginning iPhone Development with Swift 3/21Using the Camera and Accessing Photos/21Camera/21Camera/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     private func imagePickerController(_ picker: UIImagePickerController,
                                        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         lastChosenMediaType = info[UIImagePickerControllerMediaType] as? String
@@ -35,6 +36,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate,
 
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion:nil)
+    }
+
+
+    @IBAction func shootPictureOrVideo(_ sender: UIButton) {
+        pickMediaFromSource(UIImagePickerControllerSourceType.camera)
+    }
+
+    @IBAction func selectExistingPictureOrVideo(_ sender: UIButton) {
+        pickMediaFromSource(UIImagePickerControllerSourceType.photoLibrary)
+    }
+
+    func pickMediaFromSource(_ sourceType:UIImagePickerControllerSourceType) {
+        let mediaTypes =
+            UIImagePickerController.availableMediaTypes(for: sourceType)!
+        if UIImagePickerController.isSourceTypeAvailable(sourceType)
+            && mediaTypes.count > 0 {
+            let picker = UIImagePickerController()
+            picker.mediaTypes = mediaTypes
+            picker.delegate = self
+            picker.allowsEditing = true
+            picker.sourceType = sourceType
+            present(picker, animated: true, completion: nil)
+        } else {
+            let alertController = UIAlertController(title:"Error accessing media",
+                                                    message: "Unsupported media source.",
+                                                    preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK",
+                                         style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            present(alertController, animated: true, completion: nil)
+        }
     }
 
 }
