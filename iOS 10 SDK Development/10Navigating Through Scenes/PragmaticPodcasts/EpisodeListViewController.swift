097f3444c62b6c2730f86cd9f7d431ebfbaa1052
greatabel <myreceiver2for2github@gmail.com>
2017-09-01 16:35:37 +0800

    change podcast

--- iOS 10 SDK Development/10Navigating Through Scenes/PragmaticPodcasts/EpisodeListViewController.swift
+++ iOS 10 SDK Development/10Navigating Through Scenes/PragmaticPodcasts/EpisodeListViewController.swift
@@ -22,7 +22,18 @@ UITableViewDataSource, UITableViewDelegate {
     }
   }
 
+  // http://www.coreint.org/podcast.xml 测试podcast
   @IBAction func unwindToEpisodeList(_ segue: UIStoryboardSegue) {
+    if let addPodcastVC = segue.source as? AddPodcastViewController,
+      let urlText = addPodcastVC.urlField.text,
+      let url = URL(string: urlText) {
+      let parser = PodcastFeedParser(contentsOf: url)
+      parser.onParserFinished = { [weak self] in
+        if let feed = parser.currentFeed {
+          self?.feeds.append(feed)
+        }
+      }
+    }
 
   }
 
