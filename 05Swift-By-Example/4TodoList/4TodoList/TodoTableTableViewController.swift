a87d816516ea57d713bb82bcc26d0975aa1dcfad
greatabel <myreceiver2for2github@gmail.com>
2017-09-11 08:43:00 +0800

    use cocoapods font

--- 05Swift-By-Example/4TodoList/4TodoList/TodoTableTableViewController.swift
+++ 05Swift-By-Example/4TodoList/4TodoList/TodoTableTableViewController.swift
@@ -10,6 +10,7 @@ class TodoTableTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        title = "Todos"
     }
 
     override func didReceiveMemoryWarning() {
