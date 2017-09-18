805d59e1c513f3e4a281cf5e326df7c92fd8df01
greatabel <myreceiver2for2github@gmail.com>
2017-09-18 23:19:25 +0800

    1Your First Unit Tests/

---
+++ Test-Driven iOS Development with Swift 3/1Your First Unit Tests/1FirstDemo/1FirstDemo/ViewController.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfVowels(in string: String) -> Int {
+        let vowels: [Character] = ["a", "e", "i", "o", "u",
+                                   "A", "E", "I", "O", "U"]
+        var numberOfVowels = 0
+        for character in string.characters {
+            if vowels.contains(character) {
+                numberOfVowels += 1
+            }
+        }
+        return numberOfVowels
+    }
+
+}
+
---
+++ Test-Driven iOS Development with Swift 3/1Your First Unit Tests/1FirstDemo/1FirstDemoTests/_FirstDemoTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import _FirstDemo
+
+class _FirstDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_NumberOfVowels_WhenPassedDominik_ReturnsThree() {
+        let viewController = ViewController()
+        let string = "Dominik"
+        let numberOfVowels = viewController.numberOfVowels(in: string)
+        XCTAssertEqual(numberOfVowels, 3,
+                       "should find 3 vowels in Dominik")
+    }
+    
+}
