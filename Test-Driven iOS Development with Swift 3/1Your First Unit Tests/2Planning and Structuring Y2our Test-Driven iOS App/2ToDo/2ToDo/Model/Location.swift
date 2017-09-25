f2406ce0a3747fafc64f4e4285f9f24115dab525
greatabel <myreceiver2for2github@gmail.com>
2017-09-25 14:55:57 +0800

    location coordinate test

--- Test-Driven iOS Development with Swift 3/1Your First Unit Tests/2Planning and Structuring Y2our Test-Driven iOS App/2ToDo/2ToDo/Model/Location.swift
+++ Test-Driven iOS Development with Swift 3/1Your First Unit Tests/2Planning and Structuring Y2our Test-Driven iOS App/2ToDo/2ToDo/Model/Location.swift
@@ -1,3 +1,13 @@
+import Foundation
+import CoreLocation
+
 struct Location {
+
     let name: String
+    let coordinate: CLLocationCoordinate2D?
+
+    init(name: String, coordinate: CLLocationCoordinate2D? = nil) {
+        self.name = name
+        self.coordinate = coordinate
+    }
 }
---
+++ Test-Driven iOS Development with Swift 3/1Your First Unit Tests/2Planning and Structuring Y2our Test-Driven iOS App/2ToDo/2ToDoTests/LocationTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import _ToDo
+import CoreLocation
+
+class LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_Init_WhenGivenCoordinate_SetsCoordinate() {
+        let coordinate = CLLocationCoordinate2D(latitude: 1, longitude: 2)
+        let location = Location(name: "", coordinate: coordinate)
+        XCTAssertEqual(location.coordinate?.latitude, coordinate.latitude)
+        XCTAssertEqual(location.coordinate?.longitude,
+                       coordinate.longitude)
+    }
+    
+}
