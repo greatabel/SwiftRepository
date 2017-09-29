58027dd237760f1d33ce41b51092e5886f56cbc5
greatabel <myreceiver2for2github@gmail.com>
2017-09-29 15:56:23 +0800

    refactor

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/ItemListViewControllerTest.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/ItemListViewControllerTest.swift
@@ -8,7 +8,12 @@ class ItemListViewControllerTest: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        sut = ItemListViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(
+            withIdentifier: "ItemListViewController")
+        sut = viewController as! ItemListViewController
+
+        _ = sut.view
     }
 
     override func tearDown() {
@@ -23,19 +28,11 @@ class ItemListViewControllerTest: XCTestCase {
 //        XCTAssertNotNil(sut.tableView)
 //    }
     func test_TableView_AfterViewDidLoad_IsNotNil() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(
-            withIdentifier: "ItemListViewController")
-        let sut = viewController as! ItemListViewController
-        _ = sut.view
+
         XCTAssertNotNil(sut.tableView)
     }
 
     func test_LoadingView_SetsTableViewDataSource() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier:
-            "ItemListViewController")
-        let sut = viewController as! ItemListViewController
 
         XCTAssertTrue(sut.tableView.dataSource is ItemListDataProvider)
 
