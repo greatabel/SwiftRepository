fe9c1b3072dcb8109e90025008def71a6298a6a9
greatabel <myreceiver2for2github@gmail.com>
2017-10-10 11:13:54 +0800

    refactor case

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/ItemListDataProvider.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/ItemListDataProvider.swift
@@ -3,17 +3,24 @@ import UIKit
 class ItemListDataProvider: NSObject, UITableViewDataSource {
 
     var itemManager: ItemManager?
+
+    enum Section: Int {
+        case toDo
+        case done
+    }
     
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
+        guard let itemManager = itemManager else { return 0 }
+        guard let itemSection = Section(rawValue: section) else {
+            fatalError()
+        }
         let numberOfRows: Int
-        switch section {
-        case 0:
-            numberOfRows = itemManager?.toDoCount ?? 0
-        case 1:
-            numberOfRows = itemManager?.doneCount ?? 0
-        default:
-            numberOfRows = 0
+        switch itemSection {
+        case .toDo:
+            numberOfRows = itemManager.toDoCount
+        case .done:
+            numberOfRows = itemManager.doneCount
         }
         return numberOfRows
 
