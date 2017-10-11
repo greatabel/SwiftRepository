62753f6b3a67eda65f83a5b1c742544ecd748622
greatabel <myreceiver2for2github@gmail.com>
2017-10-11 23:01:41 +0800

    pass test_CellForRow_InSectionTwo_CallsConfigCellWithDoneItem

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/ItemListDataProvider.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/ItemListDataProvider.swift
@@ -32,10 +32,23 @@ class ItemListDataProvider: NSObject, UITableViewDataSource {
 
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
-        if let item = itemManager?.item(at: indexPath.row) {
-            cell.configCell(with: item)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell",
+                                                 for: indexPath) as! ItemCell
+
+        guard let itemManager = itemManager else {
+            fatalError() }
+        guard let section = Section(rawValue: indexPath.section) else {
+            fatalError()
         }
+        let item : ToDoItem
+        switch section {
+        case .toDo:
+            item = itemManager.item(at: indexPath.row)
+        case .done:
+            item = itemManager.doneItem(at: indexPath.row)
+        }
+
+        cell.configCell(with: item)
 
         return cell
     }
