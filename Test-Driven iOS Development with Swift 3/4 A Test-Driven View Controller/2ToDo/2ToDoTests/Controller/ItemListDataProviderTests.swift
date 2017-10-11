aa62f606993fb8d14aeec13ac3028db89fb932e6
greatabel <myreceiver2for2github@gmail.com>
2017-10-11 20:52:50 +0800

    test_CellForRow_InSectionTwo_CallsConfigCellWithDoneItem

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/ItemListDataProviderTests.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/ItemListDataProviderTests.swift
@@ -109,5 +109,23 @@ class ItemListDataProviderTests: XCTestCase {
 
     }
 
+    func test_CellForRow_InSectionTwo_CallsConfigCellWithDoneItem() {
+        let mockTableView = MockTableView()
+        mockTableView.dataSource = sut
+        mockTableView.register(MockItemCell.self, forCellReuseIdentifier: "ItemCell")
+
+        sut.itemManager?.add(ToDoItem(title: "Foo"))
+
+        let second = ToDoItem(title: "Bar")
+        sut.itemManager?.add(second)
+        sut.itemManager?.checkItem(at: 1)
+        mockTableView.reloadData()
+
+        let cell = mockTableView
+            .cellForRow(at: IndexPath(row: 0, section: 1)) as! MockItemCell
+        XCTAssertEqual(cell.catchedItem, second)
+
+    }
+
     
 }
