9160d0f07776f48dcf2df406fb0510d3074c4d71
greatabel <myreceiver2for2github@gmail.com>
2017-10-13 16:51:26 +0800

    ItemCellTests

---
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/ItemCellTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+@testable import _ToDo
+
+extension ItemCellTests {
+    class FackDataSource: NSObject, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+    }
+}
+class ItemCellTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+
+    
+}
