343084d7a1344ac970863ed8741ec9a35ad41ad2
greatabel <myreceiver2for2github@gmail.com>
2017-10-14 23:31:07 +0800

    lazy var dateFormatter

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/ItemCell.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/ItemCell.swift
@@ -7,15 +7,19 @@ class ItemCell: UITableViewCell {
     @IBOutlet var locationLabel: UILabel!
 
     @IBOutlet var dateLabel: UILabel!
-    
+
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+
     func configCell(with item: ToDoItem) {
         titleLabel.text = item.title
         locationLabel.text = item.location?.name
 
         if let timestamp = item.timestamp {
             let date = Date(timeIntervalSince1970: timestamp)
-            let dateFormatter = DateFormatter()
-            dateFormatter.dateFormat = "MM/dd/yyyy"
             dateLabel.text = dateFormatter.string(from: date)
         }
     }
