7577562b05976770db28321dbb3d28bed56ebcc9
greatabel <myreceiver2for2github@gmail.com>
2017-10-16 14:40:59 +0800

    test_Save_UsesGeocoderToGetCoordinateFromAddress

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/InputViewControllerTests.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDoTests/Controller/InputViewControllerTests.swift
@@ -1,9 +1,12 @@
 import XCTest
 @testable import _ToDo
+import CoreLocation
 
 class InputViewControllerTests: XCTestCase {
 
     var sut: InputViewController!
+
+    var placemark: MockPlacemark!
 
     override func setUp() {
         super.setUp()
@@ -48,6 +51,67 @@ class InputViewControllerTests: XCTestCase {
     func test_HasCancelButton() {
         XCTAssertNotNil(sut.cancelButton)
     }
+    func test_Save_UsesGeocoderToGetCoordinateFromAddress() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+
+        let date = dateFormatter.date(from: "02/22/2016")!
+        let timestamp = date.timeIntervalSince1970
+
+        sut.titleTextField.text = "Foo"
+        sut.dateTextField.text = dateFormatter.string(from: date)
+        sut.locationTextField.text = "Bar"
+        sut.addressTextField.text = "Infinite Loop 1, Cupertino"
+        sut.descriptionTextField.text = "Baz"
+        let mockGeocoder = MockGeocoder()
+        sut.geocoder = mockGeocoder
+
+        sut.itemManager = ItemManager()
+
+        sut.save()
+        
+        placemark = MockPlacemark()
+        let coordinate = CLLocationCoordinate2DMake(37.3316851,
+                                                    -122.0300674)
+        placemark.mockCoordinate = coordinate
+        mockGeocoder.completionHandler?([placemark], nil)
+
+        let item = sut.itemManager?.item(at: 0)
+
+        let testItem = ToDoItem(title: "Foo",
+                                itemDescription: "Baz",
+                                timestamp: timestamp,
+                                location: Location(name: "Bar",
+                                                   coordinate: coordinate))
+
+        XCTAssertEqual(item, testItem)
+    }
 
     
+}
+extension InputViewControllerTests {
+    class MockGeocoder: CLGeocoder {
+
+        var completionHandler: CLGeocodeCompletionHandler?
+
+        override func geocodeAddressString(
+            _ addressString: String,
+            completionHandler: @escaping CLGeocodeCompletionHandler) {
+
+            self.completionHandler = completionHandler
+        }
+    }
+
+    class MockPlacemark : CLPlacemark {
+
+        var mockCoordinate: CLLocationCoordinate2D?
+
+        override var location: CLLocation? {
+            guard let coordinate = mockCoordinate else
+            { return CLLocation() }
+
+            return CLLocation(latitude: coordinate.latitude,
+                              longitude: coordinate.longitude)
+        }
+    }
 }
