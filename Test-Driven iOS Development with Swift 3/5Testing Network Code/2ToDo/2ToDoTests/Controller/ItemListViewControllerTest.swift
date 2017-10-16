a9ce5ebe91129a89a0eaa3d5ddf79f1839f5293d
greatabel <myreceiver2for2github@gmail.com>
2017-10-16 14:51:12 +0800

    5Testing Network Code/

---
+++ Test-Driven iOS Development with Swift 3/5Testing Network Code/2ToDo/2ToDoTests/Controller/ItemListViewControllerTest.swift
@@ -0,0 +1,45 @@
+import XCTest
+@testable import _ToDo
+
+class ItemListViewControllerTest: XCTestCase {
+
+    var sut: ItemListViewController!
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(
+            withIdentifier: "ItemListViewController")
+        sut = viewController as! ItemListViewController
+
+        _ = sut.view
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+ 
+
+//    func test_TableView_AfterViewDidLoad_IsNotNil() {
+//        _ = sut.view
+//        XCTAssertNotNil(sut.tableView)
+//    }
+    func test_TableView_AfterViewDidLoad_IsNotNil() {
+
+        XCTAssertNotNil(sut.tableView)
+    }
+
+    func test_LoadingView_SetsTableViewDataSource() {
+
+        XCTAssertTrue(sut.tableView.dataSource is ItemListDataProvider)
+
+    }
+
+    func test_LoadingView_SetsTableViewDelegate() {
+        XCTAssertTrue(sut.tableView.delegate is ItemListDataProvider)
+    }
+    
+}
