de0c880e53eebac2f09e283028b6009bb8a01726
greatabel <myreceiver2for2github@gmail.com>
2017-10-16 14:48:20 +0800

    connect save to button

--- Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/InputViewController.swift
+++ Test-Driven iOS Development with Swift 3/4 A Test-Driven View Controller/2ToDo/2ToDo/Controller/InputViewController.swift
@@ -33,7 +33,7 @@ class InputViewController: UIViewController {
         return dateFormatter
     }()
 
-    func save() {
+    @IBAction func save() {
         guard let titleString = titleTextField.text,
             titleString.characters.count > 0 else { return }
         let date: Date?
