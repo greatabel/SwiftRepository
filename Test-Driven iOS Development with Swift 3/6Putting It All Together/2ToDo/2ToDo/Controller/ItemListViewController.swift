698b610974af033cdcfb13861f12895e3c30631c
greatabel <myreceiver2for2github@gmail.com>
2017-10-18 13:55:08 +0800

    pass test_ItemListViewController_HasAddBarButtonWithSelfAsTarget

--- Test-Driven iOS Development with Swift 3/6Putting It All Together/2ToDo/2ToDo/Controller/ItemListViewController.swift
+++ Test-Driven iOS Development with Swift 3/6Putting It All Together/2ToDo/2ToDo/Controller/ItemListViewController.swift
@@ -16,6 +16,10 @@ class ItemListViewController: UIViewController {
         tableView.delegate = dataProvider
     }
 
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
