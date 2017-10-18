c033285f8f776c44314c9113a37d44f28c59a43c
greatabel <myreceiver2for2github@gmail.com>
2017-10-18 12:00:46 +0800

    6Putting It All Together/

---
+++ Test-Driven iOS Development with Swift 3/6Putting It All Together/2ToDo/2ToDo/Controller/ItemListViewController.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+
+
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var dataProvider: (UITableViewDataSource & UITableViewDelegate)!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+//        tableView = UITableView()
+        tableView.dataSource = dataProvider
+        tableView.delegate = dataProvider
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
