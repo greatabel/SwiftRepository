a497ba515546f67afaf85b81a0a029faeea69bc6
greatabel <myreceiver2for2github@gmail.com>
2017-10-21 14:17:59 +0800

    test_SelectingACell_SendsNotification

--- Test-Driven iOS Development with Swift 3/6Putting It All Together/2ToDo/2ToDoTests/Controller/ItemListDataProviderTests.swift
+++ Test-Driven iOS Development with Swift 3/6Putting It All Together/2ToDo/2ToDoTests/Controller/ItemListDataProviderTests.swift
@@ -173,5 +173,22 @@ class ItemListDataProviderTests: XCTestCase {
         XCTAssertEqual(tableView.numberOfRows(inSection: 1), 0)
     }
 
+    func test_SelectingACell_SendsNotification() {
+        let item = ToDoItem(title: "First")
+        sut.itemManager?.add(item)
+        expectation(
+            forNotification: NSNotification.Name(rawValue: "ItemSelectedNotification"),
+            object: nil) { (notification) -> Bool in
+                guard let index =
+                    notification.userInfo?["index"] as? Int else
+                { return false }
+                return index == 0
+        }
+        tableView.delegate?.tableView!(
+            tableView,
+            didSelectRowAt: IndexPath(row: 0, section: 0))
+        waitForExpectations(timeout: 3, handler: nil)
+    }
+
     
 }
