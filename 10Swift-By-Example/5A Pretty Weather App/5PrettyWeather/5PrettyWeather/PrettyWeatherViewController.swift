8808aec1059c1f742b87a8196bcef49e25084265
greatabel <myreceiver2for2github@gmail.com>
2017-10-27 16:36:31 +0800

    setup layoutview

--- 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/PrettyWeatherViewController.swift
+++ 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/PrettyWeatherViewController.swift
@@ -1,10 +1,16 @@
 import UIKit
+import Cartography
 
 class PrettyWeatherViewController: UIViewController {
 
+    private let backgroundView = UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        setup()
+        layoutView()
+        style()
+        render(image: UIImage(named: "DefaultImage"))
 
         // Do any additional setup after loading the view.
     }
@@ -13,16 +19,24 @@ class PrettyWeatherViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+private extension PrettyWeatherViewController {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setup() {
+
     }
-    */
+
+    func layoutView() {
+
+    }
+
+    func render(image: UIImage?){
+
+    }
+
+    func style(){
+
+    }
 
 }
