6f43912e8255a7544621999dda49b225a0618595
greatabel <myreceiver2for2github@gmail.com>
2017-10-29 15:56:04 +0800

    temple copy to WeatherHourForecastView.swift

--- 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/ViewControllers/WeatherHourForecastView.swift
+++ 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/ViewControllers/WeatherHourForecastView.swift
@@ -1,13 +1,48 @@
 import UIKit
+import Cartography
 
 class WeatherHourForecastView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+
+    private var didSetupConstraints = false
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        style()
+
     }
-    */
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func updateConstraints() {
+        if didSetupConstraints {
+            super.updateConstraints()
+            return
+        }
+        layoutView()
+        super.updateConstraints()
+        didSetupConstraints = true
+    }
 
 }
+
+private extension WeatherHourForecastView {
+    func setup() {
+
+    }
+}
+private extension WeatherHourForecastView{
+    func layoutView(){
+  
+    }
+}
+private extension WeatherHourForecastView {
+    func style() {
+        backgroundColor = UIColor.blue
+    }
+}
+
+
