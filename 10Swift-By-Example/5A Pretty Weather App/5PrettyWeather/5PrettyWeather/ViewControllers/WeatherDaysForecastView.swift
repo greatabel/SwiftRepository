71b1408f017eac37881b362a904d3a7b8616ff0f
greatabel <myreceiver2for2github@gmail.com>
2017-10-31 17:13:25 +0800

    finish of WeatherDaysForecastView

--- 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/ViewControllers/WeatherDaysForecastView.swift
+++ 10Swift-By-Example/5A Pretty Weather App/5PrettyWeather/5PrettyWeather/ViewControllers/WeatherDaysForecastView.swift
@@ -3,9 +3,7 @@ import Cartography
 
 class WeatherDaysForecastView: UIView {
 
-    static var HEIGHT: CGFloat {
-        get { return 300 }
-    }
+
     private var didSetupConstraints = false
     private var forecastCells = [WeatherDayForecastView]()
 
@@ -34,28 +32,50 @@ class WeatherDaysForecastView: UIView {
 
 private extension WeatherDaysForecastView {
     func setup() {
-
+        (0..<7).forEach {
+            _ in
+            let cell = WeatherDayForecastView(frame: CGRect.zero)
+            forecastCells.append(cell)
+            addSubview(cell)
+        }
     }
 }
 private extension WeatherDaysForecastView{
     func layoutView(){
-        constrain(self) { view in
-            view.height == WeatherDaysForecastView.HEIGHT
-            return
+        constrain(forecastCells.first!) {
+            $0.top == $0.superview!.top
+        }
+        for idx in 0..<(forecastCells.count - 1) {
+            let cell = forecastCells[idx]
+            let nextCell = forecastCells[idx + 1]
+            constrain(cell, nextCell){
+                $0.bottom == $1.top
+            }
+        }
+        forecastCells.forEach {
+            cell in
+            constrain(cell){
+                $0.left == $0.superview!.left
+                $0.right == $0.superview!.right
+            }
+        }
+        constrain(forecastCells.last!){
+            $0.bottom == $0.superview!.bottom
         }
     }
 }
+
 private extension WeatherDaysForecastView {
     func style() {
-        backgroundColor = UIColor.blue
+//        backgroundColor = UIColor.blue
     }
 }
 
 extension WeatherDaysForecastView{
     func render(){
-//        forecastCells.forEach {
-//            $0.render()
-//        }
+        forecastCells.forEach {
+            $0.render()
+        }
     }
 }
 
