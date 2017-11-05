0d21a986a5a6bef13ce198e6fb1f8c85c5234022
greatabel <myreceiver2for2github@gmail.com>
2017-11-05 23:28:34 +0800

    make MenuViewController work

--- 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/MenuViewController.swift
+++ 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/MenuViewController.swift
@@ -12,6 +12,7 @@ class MenuViewController: UIViewController {
                                                buttonStyle: .rect)
     private let gameCenterButton = HTPressableButton(frame: CGRectMake(0, 0, 260, 50),
                                                      buttonStyle: .rect)
+    let testButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,24 +26,62 @@ class MenuViewController: UIViewController {
 
 private extension MenuViewController{
     func setup(){
+        playButton?.addTarget(self, action: #selector(self.onPlayPressed(sender:)),
+                              for: .touchUpInside)
+        view.addSubview(playButton!)
+        gameCenterButton?.addTarget(self, action: #selector(self.onGameCenterPressed(sender:)),
+                                    for: .touchUpInside)
+        view.addSubview(gameCenterButton!)
 
     }
+    @objc func onPlayPressed(sender: UIButton) {
+        print(#function)
+        let vc = GameViewController()
+        vc.modalTransitionStyle = .crossDissolve
+        present(vc, animated: true, completion: nil)
+    }
+
+    @objc func onGameCenterPressed(sender: UIButton) {
+        print(#function)
+
+    }
+
+
 }
 
 private extension MenuViewController{
     func layoutView(){
 
+        constrain(playButton!) { view in
+            view.bottom == view.superview!.centerY - 60
+            view.centerX == view.superview!.centerX
+            view.height == 80
+            view.width == view.superview!.width - 40
+        }
+        constrain(gameCenterButton!) { view in
+            view.bottom == view.superview!.centerY + 60
+            view.centerX == view.superview!.centerX
+            view.height == 80
+            view.width == view.superview!.width - 40
+        }
+
     }
 }
 private extension MenuViewController{
     func style(){
+        playButton?.buttonColor = UIColor.ht_grapeFruit()
+        playButton?.shadowColor = UIColor.ht_grapeFruitDark()
 
+        gameCenterButton?.buttonColor = UIColor.ht_aqua()
+        gameCenterButton?.shadowColor = UIColor.ht_aquaDark()
     }
 }
 
 private extension MenuViewController{
     func render(){
-
+        playButton?.setTitle("Play", for: [])
+        gameCenterButton?.setTitle("Game Center", for: [])
     }
+
 }
 
