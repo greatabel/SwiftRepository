2aa4ce7dfcdd5548de6c27d5f4a9f1de75062b3a
greatabel <myreceiver2for2github@gmail.com>
2017-11-05 23:44:27 +0800

    new GameViewController

--- 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/GameViewController.swift
+++ 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/GameViewController.swift
@@ -1,11 +1,42 @@
 import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        if let path = Bundle.main.path(forResource: file as String, ofType: "sks") {
+            let sceneData = try! NSData(contentsOfFile: path, options: .mappedIfSafe)
+            let archiver = NSKeyedUnarchiver(forReadingWith: sceneData as Data)
+
+            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+            let scene = archiver.decodeObject(forKey: NSKeyedArchiveRootObjectKey) as! GameScene
+            archiver.finishDecoding()
+            return scene
+        } else {
+            return nil
+        }
+    }
+}
 
 class GameViewController: UIViewController {
 
+    private let skView = SKView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        skView.frame = view.bounds
+        view.addSubview(skView)
+        if let scene = GameScene.unarchiveFromFile(file: "GameScene") as? GameScene {
+            scene.size = skView.frame.size
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+
+            skView.ignoresSiblingOrder = true
+
+            scene.scaleMode = .aspectFill
+
+            skView.presentScene(scene)
+        }
     }
 
 
