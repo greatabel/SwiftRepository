d398cd8367df5919c35661e842840d30cb06ab36
greatabel <myreceiver2for2github@gmail.com>
2017-11-05 22:27:49 +0800

    MenuViewController.swift

--- 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/MenuViewController.swift
+++ 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/MenuViewController.swift
@@ -1,27 +1,48 @@
 import UIKit
+import HTPressableButton
+import Cartography
+
+func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+    return CGRect(x: x, y: y, width: width, height: height)
+}
 
 class MenuViewController: UIViewController {
 
+    private let playButton = HTPressableButton(frame: CGRectMake(0, 0, 260, 50),
+                                               buttonStyle: .rect)
+    private let gameCenterButton = HTPressableButton(frame: CGRectMake(0, 0, 260, 50),
+                                                     buttonStyle: .rect)
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setup()
+        layoutView()
+        style()
+        render()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+private extension MenuViewController{
+    func setup(){
+
+    }
+}
+
+private extension MenuViewController{
+    func layoutView(){
+
+    }
+}
+private extension MenuViewController{
+    func style(){
+
+    }
+}
+
+private extension MenuViewController{
+    func render(){
+
+    }
+}
+
