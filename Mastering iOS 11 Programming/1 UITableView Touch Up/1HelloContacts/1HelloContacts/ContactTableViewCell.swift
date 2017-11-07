890c2d0184273a3fdb116fe6eda01e8490075817
greatabel <myreceiver2for2github@gmail.com>
2017-11-07 16:14:53 +0800

    HCContact

---
+++ Mastering iOS 11 Programming/1 UITableView Touch Up/1HelloContacts/1HelloContacts/ContactTableViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var contactImage: UIImageView!
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        contactImage.image = nil
+    }
+}
