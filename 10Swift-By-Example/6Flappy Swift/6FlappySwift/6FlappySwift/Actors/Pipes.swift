401d4af40a9a3a0e6942dc7f49a4164f7d729f54
greatabel <myreceiver2for2github@gmail.com>
2017-11-07 10:58:32 +0800

    Pipes init

--- 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/Actors/Pipes.swift
+++ 10Swift-By-Example/6Flappy Swift/6FlappySwift/6FlappySwift/Actors/Pipes.swift
@@ -2,6 +2,29 @@ import SpriteKit
 
 class Pipes {
 
+    private class var createActionKey : String { get {return "createActionKey"} }
+    private var parentNode: SKSpriteNode!
+    private let topPipeTexture: String
+    private let bottomPipeTexture: String
+
+    init(topPipeTexture: String, bottomPipeTexture: String) {
+        self.topPipeTexture = topPipeTexture
+        self.bottomPipeTexture = bottomPipeTexture
+    }
+    
+    func addTo(parentNode: SKSpriteNode) -> Pipes {
+        self.parentNode = parentNode
+        return self
+    }
+
 }
 
+private extension Pipes {
+    func createNewPipesNode() {
+        PipesNode(topPipeTexture: topPipeTexture, bottomPipeTexture:bottomPipeTexture, centerY: centerPipes()).addTo(parentNode: parentNode).start()
+    }
 
+    func centerPipes() -> CGFloat {
+        return parentNode.size.height/2 - 100 + 20 * CGFloat(arc4random_uniform(10))
+    }
+}
