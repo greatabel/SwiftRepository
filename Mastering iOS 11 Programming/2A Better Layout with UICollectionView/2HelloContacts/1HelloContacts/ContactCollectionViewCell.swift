a2df02d1d69c351feb60faa5aa262b1e006d4d00
greatabel <myreceiver2for2github@gmail.com>
2017-11-08 10:50:28 +0800

    collectionview

--- Mastering iOS 11 Programming/2A Better Layout with UICollectionView/2HelloContacts/1HelloContacts/ContactCollectionViewCell.swift
+++ Mastering iOS 11 Programming/2A Better Layout with UICollectionView/2HelloContacts/1HelloContacts/ContactCollectionViewCell.swift
@@ -1,10 +1,16 @@
 import UIKit
 
-class ContactTableViewCell: UITableViewCell {
+class ContactCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var contactImage: UIImageView!
 
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        contactImage.layer.cornerRadius = 25
+    }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         contactImage.image = nil
--- Mastering iOS 11 Programming/2A Better Layout with UICollectionView/2HelloContacts/1HelloContacts/ViewController.swift
+++ Mastering iOS 11 Programming/2A Better Layout with UICollectionView/2HelloContacts/1HelloContacts/ViewController.swift
@@ -1,16 +1,20 @@
 import UIKit
 import Contacts
 
-class ViewController: UIViewController , UITableViewDataSource,
-    UITableViewDelegate, UITableViewDataSourcePrefetching {
+class ViewController: UIViewController , UICollectionViewDataSource,
+    UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
-    @IBOutlet var tableView: UITableView!
+   @IBOutlet var collectionView: UICollectionView!
 
     var contacts = [HCContact]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.collectionViewLayout = ContactsCollectionViewLayout()
+        
         let store = CNContactStore()
         let authorizationStatus = CNContactStore.authorizationStatus(for: .contacts)
         print("## authorizationStatus = \(authorizationStatus.rawValue)")
@@ -25,42 +29,40 @@ class ViewController: UIViewController , UITableViewDataSource,
             retrieveContacts(fromStore: store)
         }
 
-        tableView.dataSource = self
-        tableView.delegate = self
-        tableView.prefetchDataSource = self
+
 
         navigationItem.rightBarButtonItem = editButtonItem
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     func retrieveContacts(fromStore store: CNContactStore) {
 
         let containerId = store.defaultContainerIdentifier()
-
         let predicate = CNContact.predicateForContactsInContainer(withIdentifier: containerId)
-
         let keysToFetch =
             [CNContactGivenNameKey as CNKeyDescriptor,
              CNContactFamilyNameKey as CNKeyDescriptor,
              CNContactImageDataKey as CNKeyDescriptor,
              CNContactImageDataAvailableKey as CNKeyDescriptor]
-
-        contacts = try! store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch).map { contact in
+        contacts = try! store.unifiedContacts(matching: predicate, keysToFetch: keysToFetch).map
+            { contact in
             return HCContact(contact: contact)
         }
-
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
         return contacts.count
     }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell") as! ContactTableViewCell
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier:
+            "contactCell", for: indexPath) as! ContactCollectionViewCell
         let contact = contacts[indexPath.row]
 
         cell.nameLabel.text = "\(contact.givenName) \(contact.familyName)"
@@ -73,54 +75,8 @@ class ViewController: UIViewController , UITableViewDataSource,
         return cell
     }
 
-    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        for indexPath in indexPaths {
-            let contact = contacts[indexPath.row]
-            contact.fetchImageIfNeeded()
-        }
-    }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let contact = contacts[indexPath.row]
-
-        let alertController = UIAlertController(title: "通讯录被点击", message: "你按了 \(contact.givenName)", preferredStyle: .alert)
-
-        let dismissAction = UIAlertAction(title: "Ok", style: .default, handler: {action in
-            tableView.deselectRow(at: indexPath, animated: true)
-        })
-
-        alertController.addAction(dismissAction)
-        present(alertController, animated: true, completion: nil)
-    }
 
 
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        print("####")
-        let deleteHandler: UIContextualActionHandler = { [weak self] action, view, callback in
-            self?.contacts.remove(at: indexPath.row)
-            self?.tableView.beginUpdates()
-            self?.tableView.deleteRows(at: [indexPath], with: .fade)
-            self?.tableView.endUpdates()
-            callback(true)
-        }
-
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete", handler: deleteHandler)
-        let actions = [deleteAction]
-        let config = UISwipeActionsConfiguration(actions: actions)
-        return config
-    }
-
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        print("\(#function) sourceIndexPath= \(sourceIndexPath)")
-        let contact = contacts.remove(at: sourceIndexPath.row)
-        contacts.insert(contact, at: destinationIndexPath.row)
-    }
-
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-
-        tableView.setEditing(editing, animated: animated)
-    }
 
 
 }
