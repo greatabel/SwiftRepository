dcf13b31db7e4d31c5534a87ad0bdfc029bed492
greatabel <myreceiver2for2github@gmail.com>
2017-11-11 22:13:53 +0800

    cameraNode

--- 10Swift-By-Example/8Cube Runner/SceneKitPlayground.playground/Contents.swift
+++ 10Swift-By-Example/8Cube Runner/SceneKitPlayground.playground/Contents.swift
@@ -5,20 +5,17 @@ import SpriteKit
 import SceneKit
 import UIKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode!
-    private var spinnyNode : SKShapeNode!
-    
-    override func didMove(to view: SKView) {
-
-    }
-
-}
 
 // Load the SKScene from 'GameScene.sks'
 let sceneView = SCNView(frame: CGRect(x:0 , y:0, width: 400, height: 480))
 var scene = SCNScene()
+
+var cameraNode = SCNNode()
+cameraNode.camera = SCNCamera()
+cameraNode.position = SCNVector3(x: 0, y: 0, z: 4)
+scene.rootNode.addChildNode(cameraNode)
+
+
 sceneView.scene = scene
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
