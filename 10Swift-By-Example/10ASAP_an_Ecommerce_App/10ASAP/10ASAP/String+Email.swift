f89a52dab9e2551f6ae13bae705d9504c191a019
greatabel <myreceiver2for2github@gmail.com>
2017-11-29 15:55:38 +0800

    string email

---
+++ 10Swift-By-Example/10ASAP_an_Ecommerce_App/10ASAP/10ASAP/String+Email.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+extension String {
+    func isValidEmail() -> Bool {
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: self)
+    }
+}
