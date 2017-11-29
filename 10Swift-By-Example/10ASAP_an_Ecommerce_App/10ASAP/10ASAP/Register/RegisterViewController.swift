5c5bdc19b8e2050d2c2d970463cf8eba22d5c241
greatabel <myreceiver2for2github@gmail.com>
2017-11-29 15:54:34 +0800

    connect textfield

--- 10Swift-By-Example/10ASAP_an_Ecommerce_App/10ASAP/10ASAP/Register/RegisterViewController.swift
+++ 10Swift-By-Example/10ASAP_an_Ecommerce_App/10ASAP/10ASAP/Register/RegisterViewController.swift
@@ -3,6 +3,20 @@ import LatoFont
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet var okButton: UIButton! {
+        didSet {
+            okButton.isEnabled = false
+            okButton.titleLabel?.font = UIFont.latoFont(ofSize: 18)
+        }
+    }
+
+    @IBOutlet var emailTextField: UITextField! {
+        didSet {
+            emailTextField.becomeFirstResponder()
+            emailTextField.font = UIFont.latoFont(ofSize: 18)
+        }
+    }
+
     static func instantiate() -> RegisterViewController {
         return UIStoryboard(name: "Register", bundle: nil).instantiateInitialViewController()
             as! RegisterViewController
