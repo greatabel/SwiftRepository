3aaf38892d1acbcb416a9a2dc40a783b4bb00873
greatabel <myreceiver2for2github@gmail.com>
2017-11-30 18:04:11 +0800

    make 2columns

--- 10Swift-By-Example/10ASAP_an_Ecommerce_App/10ASAP/10ASAP/Ecommerce/TwoColumnFlowLayout.swift
+++ 10Swift-By-Example/10ASAP_an_Ecommerce_App/10ASAP/10ASAP/Ecommerce/TwoColumnFlowLayout.swift
@@ -1,1 +1,27 @@
-import Foundation
+import UIKit
+
+class TwoColumnFlowLayout: UICollectionViewFlowLayout {
+    private struct Constants {
+        static let NumberColumns = CGFloat(2.0)
+        static let InteritemSpacing = CGFloat(1.0)
+    }
+
+    override func prepare() {
+        super.prepare()
+
+        configureItemSpacing()
+        configureItemSize()
+    }
+}
+
+private extension TwoColumnFlowLayout {
+    func configureItemSpacing() {
+        minimumInteritemSpacing = Constants.InteritemSpacing
+        minimumLineSpacing = Constants.InteritemSpacing
+    }
+
+    func configureItemSize() {
+        let itemSide = (collectionViewContentSize.width / Constants.NumberColumns) - (Constants.InteritemSpacing * 0.5)
+        itemSize = CGSize(width:itemSide, height:itemSide)
+    }
+}
