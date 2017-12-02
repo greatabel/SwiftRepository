540f211472e248fc553320770625c297f0366dd6
greatabel <myreceiver2for2github@gmail.com>
2017-12-02 23:38:49 +0800

    Mastering_swift4

---
+++ Mastering_Swift4/show_image.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+let absoluteImagePath = "/Users/wanchang/Downloads/AbelProject/SwiftRepository/Mastering_Swift4/image0.png"
+let image = NSImage(contentsOfFile: absoluteImagePath)
+
