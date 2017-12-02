782af958d4c10e8cd6a306386c07af7975c0d29d
greatabel <myreceiver2for2github@gmail.com>
2017-12-02 23:42:23 +0800

    Creating and displaying graphs

--- Mastering_Swift4/show_image.playground/Contents.swift
+++ Mastering_Swift4/show_image.playground/Contents.swift
@@ -6,3 +6,9 @@ var str = "Hello, playground"
 let absoluteImagePath = "/Users/wanchang/Downloads/AbelProject/SwiftRepository/Mastering_Swift4/image0.png"
 let image = NSImage(contentsOfFile: absoluteImagePath)
 
+// Creating and displaying graphs
+var j = 1
+for i in 1...6 {
+    j *= i
+}
+
