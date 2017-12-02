dc429970d4636926d57687f509b647857faa8353
greatabel <myreceiver2for2github@gmail.com>
2017-12-02 23:45:16 +0800

    hello.playground/ git push

---
+++ Mastering_Swift4/hello.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+print("Hello World")
+var name = "Jon"
+var language = "Swift"
+
+var message1 = " Welcome to the wonderful world of "
+var message2 = "\(name), Welcome to the wonderful world of \(language)!"
+
+print(message2)
+print(name, message1, language, "!")
