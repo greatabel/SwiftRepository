0738dcc199fa43ba809f311378fbfcb66d5820f9
greatabel <myreceiver2for2github@gmail.com>
2017-12-05 13:36:29 +0800

    11Working with Generics/

---
+++ Mastering_Swift4/11Working with Generics/generic.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+struct GenericSubscript {
+
+    let dictionary: [String: Any] = [:]
+
+    subscript<T: Hashable>(item: T) -> Int {
+        return item.hashValue
+    }
+
+    subscript<T>(key: String) -> T? {
+        return dictionary[key] as? T
+    }
+
+}
+
+var g = GenericSubscript()
+var x = g["test"]
