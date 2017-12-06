ad2077cb044490df2745107983c6a141fc6f253b
greatabel <myreceiver2for2github@gmail.com>
2017-12-06 11:41:12 +0800

    clousure_array.playground

---
+++ Mastering_Swift4/12Working with Closures/clousure_array.playground/Contents.swift
@@ -0,0 +1,49 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+let guests = ["Jon", "Kim", "Kailey", "Kara"]
+
+var greetGuest = { (name:String) -> Void in
+    print("Hello guest named \(name)")
+}
+
+var sayGoodbye = { (name:String) -> Void in
+    print("Goodbye \(name)")
+}
+
+guests.map(greetGuest)
+guests.map(sayGoodbye)
+
+var greetGuest2 = { (name:String) -> Void in
+    if (name.hasPrefix("K")) {
+        print("\(name) is on the guest list")
+    } else {
+        print("\(name) was not invited")
+    }
+}
+
+guests.map(greetGuest2)
+
+
+func temperatures(calculate:(Int)->Void) {
+    let numbers = [72,74,76,68,70,72,66]
+    numbers.map(calculate)
+}
+
+func testFunction() {
+    var total = 0
+    var count = 0
+    let addTemps = {
+        (num: Int) -> Void in
+        total += num
+        count += 1
+    }
+    temperatures(calculate: addTemps)
+    print("Total: \(total)")
+    print("Count: \(count)")
+    print("Average: \(total/count)")
+}
+
+testFunction()
+
