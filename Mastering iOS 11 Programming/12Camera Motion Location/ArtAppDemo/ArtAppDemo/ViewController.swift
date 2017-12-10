97493109aa63185e3e0f16e5ce46891f4c9a569c
greatabel <myreceiver2for2github@gmail.com>
2017-12-10 11:02:44 +0800

    constraint

--- Mastering iOS 11 Programming/12Camera Motion Location/ArtAppDemo/ArtAppDemo/ViewController.swift
+++ Mastering iOS 11 Programming/12Camera Motion Location/ArtAppDemo/ArtAppDemo/ViewController.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var videoView: UIView!
+
+
+    @IBOutlet var hConstraint: NSLayoutConstraint!
+    @IBOutlet var vConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
