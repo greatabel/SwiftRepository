232878a667c5bd19374a9d394ad6fe2206e68678
greatabel <myreceiver2for2github@gmail.com>
2017-12-11 16:16:12 +0800

    ThermostatSlider

--- Learning iOS UI Development/ch8Custom Controls/ch8Custom_Control/ch8Custom_Control/ThermostatSlider.swift
+++ Learning iOS UI Development/ch8Custom Controls/ch8Custom_Control/ch8Custom_Control/ThermostatSlider.swift
@@ -28,12 +28,150 @@ class ThermostatSlider: UIControl {
 
     private var _value:Float = 0.0
 
+    private var borderLayer = CALayer()
+    private var trackLayer = CALayer()
+    private var handleLayer = CALayer()
+
+    override init(frame:CGRect){
+
+        super.init(frame:frame)
+        designBorders()
+        designTrack()
+        designHandle()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder:aDecoder)
+    }
+
+    private func designBorders(){
+
+        borderLayer.backgroundColor = UIColor.white.cgColor
+        borderLayer.borderColor = UIColor(white: 0.9, alpha: 1.0).cgColor
+        borderLayer.borderWidth = 1
+        borderLayer.masksToBounds = true
+
+        layer.addSublayer(borderLayer)
+    }
+
+    private func designHandle(){
+
+        handleLayer.backgroundColor = UIColor.white.cgColor
+        handleLayer.shadowColor = UIColor.black.cgColor
+        handleLayer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        handleLayer.shadowRadius = 2
+        handleLayer.shadowOpacity = 0.3
+        handleLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        self.layer.addSublayer(handleLayer)
+    }
+
+    private func designTrack(){
+
+        trackLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
+        borderLayer.addSublayer(trackLayer)
+    }
+
+
+    override func layoutSubviews() {
+
+        super.layoutSubviews()
+        updateBorder()
+        updateHandle()
+        updateTrack()
+
+    }
+
+    private func updateBorder(){
+
+        borderLayer.frame = bounds
+        borderLayer.cornerRadius = bounds.size.width / 2.0
+    }
+
     private func updateHandle(){
 
+        // Update size and radius depending on control width
+        let controlWidth = bounds.size.width
+
+        handleLayer.bounds.size = CGSize(
+            width: controlWidth,
+            height: controlWidth)
+        handleLayer.cornerRadius = controlWidth / 2.0
+
+        // Update handle position depending on value
+        var position = borderLayer.position
+        if value != 0 {
+            position.y -= valueToY()
+        }
+        handleLayer.position = position
     }
 
     private func updateTrack(){
 
+        // Define track size
+        trackLayer.frame = CGRect(x:0, y:0, width:self.bounds.width, height:0)
+        trackLayer.position = CGPoint(
+            x: bounds.size.width/2,
+            y: bounds.size.height/2)
+
+        // Update layer height depending on value
+        trackLayer.bounds.size.height = abs(valueToY())
+
+        // Update anchor point and color depending on value
+        if value >= 0{
+            trackLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
+            trackLayer.backgroundColor = hotTrackColor.cgColor
+        } else {
+            trackLayer.anchorPoint = CGPoint(x: 0.5, y: 0)
+            trackLayer.backgroundColor = coldTrackColor.cgColor
+        }
+
+    }
+
+    private func valueToY()->CGFloat{
+        return CGFloat(value) / CGFloat(maxValue) * (bounds.size.height - handleLayer.bounds.size.height / 2.0) / 2
+    }
+
+    // Set the value for the given Y absolute coordinate
+    private func valueFromY(y:CGFloat)->Float{
+        let cleanY = min(max(0, y), bounds.size.height)
+        let trackHalfHeight = bounds.size.height / 2.0
+        let translatedY = trackHalfHeight - cleanY
+
+        return Float(translatedY) * maxValue / Float(trackHalfHeight)
+    }
+
+    // MARK: Touch Tracking ------------------------------------------------------------- **
+
+    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool{
+
+        self.handleLayer.transform = CATransform3DMakeScale(1.5, 1.5, 1)
+        let point = touch.location(in: self)
+        value = valueFromY(y: point.y)
+        self.isHighlighted = true
+
+        for t in allTargets{
+            print(actions(forTarget: t, forControlEvent: UIControlEvents.valueChanged)!)
+        }
+        return true
+    }
+
+    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        let point = touch.location(in: self)
+        value = valueFromY(y: point.y)
+
+        return true
+    }
+
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        self.handleLayer.transform = CATransform3DIdentity
+        self.isHighlighted = false
+
+    }
+
+    override func cancelTracking(with event: UIEvent?) {
+        self.handleLayer.transform = CATransform3DIdentity
+        self.isHighlighted = false
     }
 
 }
--- Learning iOS UI Development/ch8Custom Controls/ch8Custom_Control/ch8Custom_Control/ViewController.swift
+++ Learning iOS UI Development/ch8Custom Controls/ch8Custom_Control/ch8Custom_Control/ViewController.swift
@@ -2,9 +2,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+
+    var slider:ThermostatSlider?
+    var handleColor:UIColor?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        slider = ThermostatSlider(frame: CGRect(x: 20, y: 20, width: 100, height: 100))
+        slider?.translatesAutoresizingMaskIntoConstraints = false
+        slider?.autoresizingMask = []
+        view.addSubview(slider!)
+
+        // Autolayout
+
+        let cst_width = NSLayoutConstraint.constraints(withVisualFormat: "H:[slider(30)]",
+                                                                       options: NSLayoutFormatOptions(),
+                                                                       metrics: nil,
+                                                                       views: ["slider":slider!])
+
+        let cst_Y = NSLayoutConstraint.constraints(withVisualFormat: "V:|-150-[slider]-150-|",
+                                                                   options: NSLayoutFormatOptions(),
+                                                                   metrics: nil,
+                                                                   views: ["slider":slider!])
+
+        let cst_X = NSLayoutConstraint(
+            item: slider!,
+            attribute: NSLayoutAttribute.centerX,
+            relatedBy: NSLayoutRelation.equal,
+            toItem: view,
+            attribute: NSLayoutAttribute.centerX,
+            multiplier: 1,
+            constant: 0)
+
+        view.addConstraints(cst_Y)
+        view.addConstraint(cst_X)
+        slider!.addConstraints(cst_width)
+
+        slider!.addTarget(self, action: #selector(updateLabel), for: UIControlEvents.valueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -12,6 +48,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @objc func updateLabel(){
+        self.label?.text = "\(slider!.value)"
+    }
 
+    @IBAction func resetSlider(_ sender: Any) {
+        self.slider?.value = 0
+    }
+    
 }
 
