0b26daccf0e5c9b627fae233575330bb0b5b8ec6
greatabel <myreceiver2for2github@gmail.com>
2017-12-11 15:02:53 +0800

    redView

--- Learning iOS UI Development/ch7UI_Interactions/ch7UI_Interactions/ch7UI_Interactions/ViewController.swift
+++ Learning iOS UI Development/ch7UI_Interactions/ch7UI_Interactions/ch7UI_Interactions/ViewController.swift
@@ -2,9 +2,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        redView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        redView?.backgroundColor = UIColor.red
+        redView?.center = view.center
+        view.addSubview(redView!)
     }
 
     override func didReceiveMemoryWarning() {
