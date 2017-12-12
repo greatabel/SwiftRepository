59432aa7e552ba2f027352fe2f60390f34395cb1
greatabel <myreceiver2for2github@gmail.com>
2017-12-12 15:24:11 +0800

    setupCustomDashboardView

--- SnapKit/FunnySnap/FunnySnap/ViewController.swift
+++ SnapKit/FunnySnap/FunnySnap/ViewController.swift
@@ -7,9 +7,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(red:0.38, green:0.75, blue:0.88, alpha:1.0)
+
+        self.setupBottomRightButton()
+        self.setupCustomNavBar()
+        self.setupCustomDashboardView()
+
+    }
+
+    func setupBottomRightButton() {
         let superview = self.view
 
+        let button = UIButton()
+        button.layer.cornerRadius = 33
+        button.setTitle("+", for: .normal)
+        button.backgroundColor = UIColor.cyan
+        superview?.addSubview(button)
+        button.snp.makeConstraints {(make) -> Void in
+            make.width.equalTo(66)
+            make.height.equalTo(66)
+            make.bottom.equalTo((superview?.snp.bottom)!).offset(-20)
+            make.right.equalTo((superview?.snp.right)!).offset(-20)
+        }
+    }
+
+    func setupCustomNavBar() {
+        let superview = self.view
         let navbar = UIView()
         superview?.addSubview(navbar)
         navbar.backgroundColor = UIColor(red:0.48, green:0.45, blue:0.78, alpha:1)
@@ -28,22 +51,72 @@ class ViewController: UIViewController {
             make.center.equalTo(navbar)
         }
 
-        let button = UIButton()
-        button.layer.cornerRadius = 33
-        button.setTitle("+", for: .normal)
-        button.backgroundColor = UIColor.cyan
-        superview?.addSubview(button)
-        button.snp.makeConstraints {(make) -> Void in
-            make.width.equalTo(66)
-            make.height.equalTo(66)
-            make.bottom.equalTo((superview?.snp.bottom)!).offset(-20)
-            make.right.equalTo((superview?.snp.right)!).offset(-20)
-        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupCustomDashboardView() {
+        let superview = self.view
+        let dashboardView = UIView()
+        let miniView1 = UIView()
+        let miniView2 = UIView()
+        let bigView = UIView()
+        let miniLabel = UILabel()
+        let dashboardLabel = UILabel()
+
+        superview?.addSubview(dashboardView)
+        dashboardView.addSubview(miniView1)
+        dashboardView.addSubview(miniView2)
+        dashboardView.addSubview(bigView)
+        miniView1.addSubview(miniLabel)
+        bigView.addSubview(dashboardLabel)
+
+        dashboardView.backgroundColor = UIColor(red:0.38, green:0.85, blue:0.88, alpha:1)
+        miniView1.backgroundColor = UIColor.white
+        miniView2.backgroundColor = UIColor.lightGray
+        bigView.backgroundColor = UIColor.black
+
+        miniLabel.textColor = UIColor.white
+        miniLabel.text = "MiniBox"
+        miniLabel.backgroundColor = UIColor.black
+        miniLabel.textAlignment = .center
+
+        dashboardLabel.textColor = UIColor.white
+        dashboardLabel.text = "Dashboard"
+
+        dashboardView.snp.makeConstraints { (make) -> Void in
+            make.width.equalTo(superview!).multipliedBy(0.9)
+            make.height.equalTo(256)
+            make.top.equalTo(superview!).offset(84)
+            make.centerX.equalTo(superview!)
+        }
+
+        miniView1.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(dashboardView).offset(24)
+            make.top.equalTo(dashboardView).offset(20)
+            make.width.equalTo(dashboardView).dividedBy(2.5)
+            make.height.equalTo(dashboardView).multipliedBy(0.4)
+        }
+
+        miniView2.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(miniView1.snp.right).offset(20)
+            make.top.equalTo(dashboardView).offset(20)
+            make.size.equalTo(miniView1)
+        }
+
+        bigView.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(dashboardView).offset(24)
+            make.top.equalTo(miniView1.snp.bottom).offset(20)
+            make.right.equalTo(dashboardView).offset(-24)
+            make.bottom.equalTo(dashboardView).offset(-20)
+        }
+
+        miniLabel.snp.makeConstraints { (make) -> Void in
+            make.edges.equalTo(miniView1).inset(UIEdgeInsetsMake(5, 5, 5, 5))
+        }
+
+        dashboardLabel.snp.makeConstraints { (make) -> Void in
+            make.center.equalTo(bigView)
+        }
+
     }
 
 
