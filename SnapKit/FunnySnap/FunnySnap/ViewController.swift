2072c222ee1dffa3115595c4429f1bb56644abff
greatabel <myreceiver2for2github@gmail.com>
2017-12-12 14:41:42 +0800

    SnapKit

--- SnapKit/FunnySnap/FunnySnap/ViewController.swift
+++ SnapKit/FunnySnap/FunnySnap/ViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SnapKit
 
 // https://medium.com/cocoaacademymag/how-to-write-auto-layout-constraints-with-snapkit-in-ios-c5f95c7c695d
 
@@ -7,6 +8,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let superview = self.view
+        let button = UIButton()
+        button.layer.cornerRadius = 33
+        button.setTitle("+", for: .normal)
+        button.backgroundColor = UIColor.red
+        superview?.addSubview(button)
+        button.snp.makeConstraints {(make) -> Void in
+            make.width.equalTo(66)
+            make.height.equalTo(66)
+            make.bottom.equalTo((superview?.snp.bottom)!).offset(-20)
+            make.right.equalTo((superview?.snp.right)!).offset(-20)
+        }
     }
 
     override func didReceiveMemoryWarning() {
