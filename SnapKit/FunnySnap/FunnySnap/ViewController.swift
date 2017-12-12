5c219a3d34fe132e177430e879700fbbbb0bd1d2
greatabel <myreceiver2for2github@gmail.com>
2017-12-12 14:18:18 +0800

    podfile Podfile

--- SnapKit/FunnySnap/FunnySnap/ViewController.swift
+++ SnapKit/FunnySnap/FunnySnap/ViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+// https://medium.com/cocoaacademymag/how-to-write-auto-layout-constraints-with-snapkit-in-ios-c5f95c7c695d
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
