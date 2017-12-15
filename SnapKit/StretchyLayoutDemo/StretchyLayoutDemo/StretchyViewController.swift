174c6c40d0150e39a1f3a3436a31b1c086e5d9a1
greatabel <myreceiver2for2github@gmail.com>
2017-12-15 17:02:36 +0800

    add note

--- SnapKit/StretchyLayoutDemo/StretchyLayoutDemo/StretchyViewController.swift
+++ SnapKit/StretchyLayoutDemo/StretchyLayoutDemo/StretchyViewController.swift
@@ -1,10 +1,14 @@
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+// https://medium.freecodecamp.org/tutorial-creating-stretchy-layouts-on-ios-using-auto-layout-3fa974fa5e28
+
+class StretchyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.cyan
     }
 
     override func didReceiveMemoryWarning() {
