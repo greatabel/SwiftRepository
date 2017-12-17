4a4a489c95181362d89d1fae65b225d818950bc6
greatabel <myreceiver2for2github@gmail.com>
2017-12-17 22:54:11 +0800

    add image

--- SnapKit/StretchyLayoutDemo/StretchyLayoutDemo/StretchyViewController.swift
+++ SnapKit/StretchyLayoutDemo/StretchyLayoutDemo/StretchyViewController.swift
@@ -2,20 +2,69 @@ import UIKit
 import SnapKit
 
 // https://medium.freecodecamp.org/tutorial-creating-stretchy-layouts-on-ios-using-auto-layout-3fa974fa5e28
+class StretchyViewController: UIViewController, UIScrollViewDelegate {
 
-class StretchyViewController: UIViewController {
+    private let scrollView = UIScrollView()
+    private let infoText = UILabel()
+    private let imageView = UIImageView()
+    private let textContainer = UIView()
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        view.backgroundColor = UIColor.cyan
+
+        view.backgroundColor = .gray
+
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.delegate = self
+
+        imageView.image = UIImage(named: "Header")
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+
+        infoText.textColor = .white
+        infoText.numberOfLines = 0
+        let text =  """
+                    Lorem ipsum dolor sit amet, in alia adhuc aperiri nam. Movet scripta tractatos cu eum, sale commodo meliore ea eam, per commodo atomorum ea. Unum graeci iriure nec an, ea sit habeo movet electram. Id eius assum persius pro, id cum falli accusam. Has eu fierent partiendo, doming expetenda interesset cu mel, tempor possit vocent in nam. Iusto tollit ad duo, est at vidit vivendo liberavisse, vide munere nonumy sed ex.
+
+                    Quod possit expetendis id qui, consequat vituperata ad eam. Per cu elit latine vivendum. Ei sit nullam aliquam, an ferri epicuri quo. Ex vim tibique accumsan erroribus. In per libris verear adipiscing. Purto aliquid lobortis ea quo, ea utinam oportere qui.
+                    """
+        infoText.text = text + text + text
+
+        let imageContainer = UIView()
+        imageContainer.backgroundColor = .darkGray
+
+        textContainer.backgroundColor = .clear
+
+        let textBacking = UIView()
+        textBacking.backgroundColor = .red
+
+        view.addSubview(scrollView)
+
+        scrollView.addSubview(imageContainer)
+        scrollView.addSubview(textBacking)
+        scrollView.addSubview(textContainer)
+        scrollView.addSubview(imageView)
+
+        textContainer.addSubview(infoText)
+
+        scrollView.snp.makeConstraints {
+            make in
+
+            make.edges.equalTo(view)
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        scrollView.scrollIndicatorInsets = view.safeAreaInsets
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.safeAreaInsets.bottom, right: 0)
     }
 
 
 }
-
