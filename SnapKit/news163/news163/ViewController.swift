2bb7d35d52e00bfae1d3e8688cf154288b855a09
greatabel <myreceiver2for2github@gmail.com>
2017-12-19 21:11:51 +0800

    skeleton“

--- SnapKit/news163/news163/ViewController.swift
+++ SnapKit/news163/news163/ViewController.swift
@@ -2,14 +2,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var titleLabel: UILabel = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupNavgationBar()
+        addSubView()
+        setupSubview()
+        setupLayout()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func setupNavgationBar() {
+
+    }
+    
+    func addSubView() {
+
+    }
+
+    func setupSubview() {
+
+    }
+
+    func setupLayout() {
+
     }
 
 
