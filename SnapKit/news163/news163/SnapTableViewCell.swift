864cf00ae3363319f671f573a26943253a6920e8
greatabel <myreceiver2for2github@gmail.com>
2017-12-20 11:30:21 +0800

    SnapTableViewCell.swift

--- SnapKit/news163/news163/SnapTableViewCell.swift
+++ SnapKit/news163/news163/SnapTableViewCell.swift
@@ -1,6 +1,12 @@
 import UIKit
+import SnapKit
 
 class SnapTableViewCell: UITableViewCell {
+
+    var testImageView = UIImageView()
+    var titleLabel = UILabel()
+    var detailLabel = UILabel()
+    var typeLabel = UILabel()
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -13,6 +19,36 @@ class SnapTableViewCell: UITableViewCell {
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        self.contentView.addSubview(testImageView)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(detailLabel)
+        self.contentView.addSubview(typeLabel)
+        setLayout()
+    }
+
+    func setLayout() {
+        testImageView.snp.makeConstraints{ (make) in
+            make.top.bottom.left.equalTo(self.contentView)
+                .inset(UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 0))
+        }
+
+        titleLabel.snp.makeConstraints{ (make) in
+            make.left.equalTo(testImageView).offset(15)
+            make.top.equalTo(testImageView)
+            make.right.equalTo(self.contentView).offset(-15)
+        }
+
+        detailLabel.snp.makeConstraints{ (make) in
+            make.left.equalTo(testImageView).offset(15)
+            make.top.equalTo(titleLabel.snp.bottom).offset(15)
+            make.right.equalTo(self.contentView).offset(-15)
+        }
+        typeLabel.snp.makeConstraints{ (make) in
+            make.bottom.equalTo(self.contentView).offset(-15)
+            make.right.equalTo(self.contentView).offset(-15)
+            make.right.equalTo(self.contentView).offset(-15)
+        }
 
     }
     
