c94f5aaf132dc277c5d2e7c4d41e4bbb1b797830
greatabel <myreceiver2for2github@gmail.com>
2017-12-22 14:44:01 +0800

    ScrollImageView

---
+++ SnapKit/news163/news163/ScrollImageView.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+class ScrollImageView: UIView {
+
+    var imageURLArray: Array<URL>? {
+        didSet {
+            createImageViews()
+        }
+    }
+
+    var scrollView: UIScrollView?
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        scrollView = UIScrollView.init(frame:
+            CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height)
+            )
+        scrollView?.isPagingEnabled = true
+        self.addSubview(scrollView!)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("not implement")
+    }
+
+    func createImageViews() {
+        for subview in self.subviews {
+            subview.removeFromSuperview()
+        }
+        if let count = imageURLArray?.count {
+            for index in 0..<count {
+                let imageView = UIImageView()
+                imageView.frame = CGRect(x: CGFloat(index) * self.frame.size.width,
+                                         y: 0,
+                                         width: self.frame.size.width,
+                                         height: self.frame.height)
+                scrollView?.addSubview(imageView)
+
+                if let url = imageURLArray?[index] {
+                    imageView.kf.setImage(with: url)
+                }
+
+            }
+
+            scrollView?.contentSize = CGSize(width: CGFloat(count) * self.frame.size.width,
+                                             height:self.frame.size.height)
+        }
+    }
+
+}
