3b269ec8c621a1e8b0fa1a0a7ed2c216b0d6b6a6
greatabel <myreceiver2for2github@gmail.com>
2017-12-22 14:21:06 +0800

    add error handle

--- SnapKit/news163/news163/ViewController.swift
+++ SnapKit/news163/news163/ViewController.swift
@@ -58,13 +58,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     //    MARK: - Private
     func getDataFromServer() {
         Alamofire.request(firstURL, method: .get).responseJSON  { response in
-            let dic: NSDictionary = response.result.value as! NSDictionary
-            let tempArray = dic["T1348647853363"] as! NSArray
-            let dataArray = tempArray.subarray(with: NSRange(location: 1, length: tempArray.count - 1))
-                as NSArray
-            self.newsArray = dataArray
 
-            self.tableView.reloadData()
+            switch response.result {
+            case .success(let data):
+                if let dic: NSDictionary = data as? NSDictionary {
+                    if let tempArray = dic["T1348647853363"] as? NSArray {
+                        let dataArray = tempArray.subarray(with:
+                                NSRange(location: 1, length: tempArray.count - 1))
+                            as NSArray
+                        self.newsArray = dataArray
+                        self.tableView.reloadData()
+                    }
+                }
+            case .failure(let error):
+                print("Request failed with: \(error)")
+
+            }
+
+
         }
     }
     
