9215e3952a8490c8726c80e1b7e9eac626322077
greatabel <myreceiver2for2github@gmail.com>
2017-12-29 22:54:06 +0800

    @autoclosure

--- Swifter/autoclosure.playground/Contents.swift
+++ Swifter/autoclosure.playground/Contents.swift
@@ -8,3 +8,17 @@ func logIfTrue(predicate: () -> Bool) {
 
 
 logIfTrue(predicate: {return 2 > 1})
+
+print("simplify")
+logIfTrue(predicate: { 2 > 1})
+
+print("trailing closure")
+logIfTrue {return 2 > 1}
+
+print("@autoclosure ->")
+func logIfTrue_autoclosure(predicate: @autoclosure () -> Bool) {
+    if predicate() {
+        print("True")
+    }
+}
+logIfTrue_autoclosure(predicate: 2 > 1)
