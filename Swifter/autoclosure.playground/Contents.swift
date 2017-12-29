3784a008f0378b9147b636d0eaed2db1e8bd3538
greatabel <myreceiver2for2github@gmail.com>
2017-12-29 23:01:24 +0800

    change comment

--- Swifter/autoclosure.playground/Contents.swift
+++ Swifter/autoclosure.playground/Contents.swift
@@ -15,7 +15,7 @@ logIfTrue(predicate: { 2 > 1})
 print("trailing closure")
 logIfTrue {return 2 > 1}
 
-print("@autoclosure ->")
+print("with @autoclosure, now we can do it like:")
 func logIfTrue_autoclosure(predicate: @autoclosure () -> Bool) {
     if predicate() {
         print("True")
