c22e1a636ad3205b07c427dc3cdcb27e5e602ffe
greatabel <myreceiver2for2github@gmail.com>
2018-01-01 15:44:48 +0800

    anyObject

---
+++ Swifter/any_anyObject.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let swiftInt: Int = 1
+let swiftString: String = "test"
+
+var array: [Any] = []
+array.append(swiftInt)
+array.append(swiftString)
+
