ae19e18cb794315e1dcc0214166fb786a24c395a
greatabel <myreceiver2for2github@gmail.com>
2018-01-01 12:27:42 +0800

    subscript index

--- Swifter/subscript index.playground/Contents.swift
+++ Swifter/subscript index.playground/Contents.swift
@@ -9,3 +9,28 @@ var dic = ["cat":"meow", "goat":"mie"]
 print(dic["cat"] ?? "")
 dic["cat"] = "miao"
 print(dic["cat"] ?? "")
+
+extension Array {
+    subscript(input: [Int]) -> ArraySlice<Element> {
+        get {
+            var result = ArraySlice<Element>()
+            for i in input {
+                assert(i < self.count, "Index out of range")
+                result.append(self[i])
+            }
+            return result
+        }
+
+        set {
+            for (index,i) in input.enumerated() {
+                assert(i < self.count, "Index out of range")
+                self[i] = newValue[index]
+            }
+        }
+    }
+}
+print("-----")
+arr = [1,2,3,4,5]
+print(arr[[0,2,3]])
+arr[[0,2,3]] = [-1,-3,-4]
+print(arr[[0,2,3]])
