4fab4a87a94403dbfc26a6216601cd9e61407acd
greatabel <myreceiver2for2github@gmail.com>
2018-01-02 20:32:19 +0800

    init order

---
+++ Swifter/init order.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Cat {
+    var name: String
+    init() {
+        name = "cat"
+    }
+}
+
+class Tiger: Cat {
+    var power: Int
+    override init() {
+        power = 10
+        super.init()
+        print("power = \(power)")
+        print(name)
+        name = "tiger"
+        print(name)
+    }
+}
+var t = Tiger()
+
