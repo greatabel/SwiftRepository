13a1a35759d62b345454619b87e1422858490fc0
greatabel <myreceiver2for2github@gmail.com>
2018-01-03 15:34:10 +0800

    change

--- Swifter/designated_convenience_required.playground/Contents.swift
+++ Swifter/designated_convenience_required.playground/Contents.swift
@@ -30,3 +30,31 @@ var c = ClassA(bigNum: true)
 print(c.numA)
 var d = ClassB(bigNum: true)
 print(d.numA, d.numB)
+
+class ClassC: ClassA {
+
+}
+var e = ClassC(num: 3)
+print(e.numA)
+
+class ClassStrictA {
+    let numA: Int
+    required init(num: Int){
+        numA = num
+    }
+
+    convenience init(bigNum: Bool){
+        self.init(num: bigNum ? 10000: 1)
+    }
+}
+
+class ClassStrictB: ClassStrictA {
+     let numB: Int
+
+    required init(num: Int) {
+        numB = num + 1
+        super.init(num: num)
+    }
+}
+var sb = ClassStrictB(bigNum: true)
+print(sb.numA, sb.numB)
