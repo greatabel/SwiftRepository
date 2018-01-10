2cca229ed408904b27e29babd5197bb1b8ff2563
greatabel <myreceiver2for2github@gmail.com>
2018-01-10 17:06:30 +0800

    class static

--- Swifter/static_class.playground/Contents.swift
+++ Swifter/static_class.playground/Contents.swift
@@ -27,3 +27,36 @@ print(Point.ones)
 var one = Point(x: 1, y: 1)
 var two = Point(x: 2, y: 2)
 print(Point.add(p1: one, p2: two))
+
+
+class MyClassTest {
+    static var bar: MyClassTest?
+}
+
+protocol MyProtocol {
+    static func foo() -> String
+}
+
+struct MyStruct: MyProtocol {
+    static func foo() -> String {
+        return "MyStruct"
+    }
+}
+
+enum MyEnum: MyProtocol {
+    static func foo() -> String {
+        return "MyEnum"
+    }
+}
+
+class MyClass: MyProtocol {
+    // 在 class 中可以使用 class
+    class func foo() -> String {
+        return "MyClass.foo()"
+    }
+
+    // 也可以使用 static
+    static func bar() -> String {
+        return "MyClass.bar()"
+    }
+}
