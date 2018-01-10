39a7eef4d91d385660635dfa263a28901fef9e84
greatabel <myreceiver2for2github@gmail.com>
2018-01-10 16:37:02 +0800

    static vs class

---
+++ Swifter/static_class.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct Point {
+    let x: Double
+    let y: Double
+
+    // 存储属性
+    static let zero = Point(x: 0, y: 0)
+
+    // 计算属性
+    static var ones: [Point] {
+        return [Point(x: 1, y: 1),
+                Point(x: -1, y: 1),
+                Point(x: 1, y: -1),
+                Point(x: -1, y: -1)]
+    }
+
+    // 类型方法
+    static func add(p1: Point, p2: Point) -> Point {
+        return Point(x: p1.x + p2.x, y: p1.y + p2.y)
+    }
+}
+
+print(Point.ones)
+var one = Point(x: 1, y: 1)
+var two = Point(x: 2, y: 2)
+print(Point.add(p1: one, p2: two))
