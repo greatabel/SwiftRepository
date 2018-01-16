45424988995916450304b01d7497f05d45da4384
greatabel <myreceiver2for2github@gmail.com>
2018-01-16 23:45:56 +0800

    parameter default

--- Swifter/default_parameter.playground/Contents.swift
+++ Swifter/default_parameter.playground/Contents.swift
@@ -2,4 +2,9 @@
 
 import UIKit
 
+func sayHello1(str1: String = "Hello", str2: String, str3: String) {
+    print(st1 + str2 + str3)
+}
 
+sayHello1(str1: "Hi", str2:" # ", str3: " @ ")
+sayHello1(str2:" # ", str3: " @ ")
