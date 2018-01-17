296fdda1e4ff65fbd845ab75fae6f89116d31dae
greatabel <myreceiver2for2github@gmail.com>
2018-01-17 17:23:07 +0800

    parameter

--- Swifter/default_parameter.playground/Contents.swift
+++ Swifter/default_parameter.playground/Contents.swift
@@ -8,3 +8,9 @@ func sayHello1(str1: String = "Hello", str2: String, str3: String) {
 
 sayHello1(str1: "Hi", str2:" # ", str3: " @ ")
 sayHello1(str2:" # ", str3: " @ ")
+
+func sayHello2(str1: String, str2: String, str3: String = "World") {
+    print(str1 + str2 + str3)
+}
+sayHello2(str1: "Hello", str2: " Test ")
+sayHello2(str1: "Hello", str2: " # ", str3: "Here" )
