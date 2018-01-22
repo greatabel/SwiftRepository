f1475b3078b0205d9c48a397e5008d0090228166
greatabel <myreceiver2for2github@gmail.com>
2018-01-22 16:27:13 +0800

    self

---
+++ Swifter/self_in_interface_classmethod.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+protocol IntervalType {
+
+    func clamp(IntervalToClamp: Self) -> Self
+}
+
+protocol Copyable {
+    func copy() -> Self
+}
+
+class MyClass: Copyable {
+
+    var num = 1
+
+    func copy() -> Self {
+        let result = type(of: self).init()
+        result.num = num
+        return result
+    }
+
+    required init(){
+
+    }
+}
+
+let object = MyClass()
+object.num = 100
+
+let newObject = object.copy()
+object.num = 1
+
+print(object.num)     // 1
+print(newObject.num)  // 100
