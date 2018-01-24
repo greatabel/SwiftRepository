299324c76f68c2390d27a9877dd3a4d426947db4
greatabel <myreceiver2for2github@gmail.com>
2018-01-24 16:22:47 +0800

    print frame

--- Programming iOS 11/1Views/04CoordinateSpace/04CoordinateSpace/ViewController.swift
+++ Programming iOS 11/1Views/04CoordinateSpace/04CoordinateSpace/ViewController.swift
@@ -3,7 +3,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool { return true }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -15,11 +15,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func doButton1(_ sender: UIButton) {
-
+        common(sender)
     }
     
     @IBAction func doButton2(_ sender: UIButton) {
+        common(sender)
+    }
 
+    func common(_ sender: UIButton) {
+        let v = sender
+        let screen = UIScreen.main.fixedCoordinateSpace
+        let r = v.superview!.convert(v.frame, to: screen)
+        print(r)
+        print(v.frame)
+        do {
+            let r = v.superview!.convert(v.frame, to: UIScreen.main.coordinateSpace)
+            print(r)
+            print(v.frame)
+        }
     }
 }
 
