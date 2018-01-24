58e937924c9d4ab70db562f96bafdc565ed5cc16
greatabel <myreceiver2for2github@gmail.com>
2018-01-24 15:35:20 +0800

    show

--- Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/ViewController.swift
+++ Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/ViewController.swift
@@ -11,15 +11,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let v = UIView(frame: CGRect(0, 0, 150, 150))
+        let v = UIView(frame: CGRect(0, 0, 50, 50))
         v.backgroundColor = .blue
         self.view.addSubview(v)
 
-        let v1 = UIView(frame:CGRect(113, 111, 132, 194))
+        let v1 = UIView(frame:CGRect(113, 111, 132, 97))
         v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
-        let v2 = UIView(frame:CGRect(41, 56, 132, 194))
+        let v2 = UIView(frame:CGRect(41, 56, 132, 97))
         v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
-        let v3 = UIView(frame:CGRect(43, 197, 160, 230))
+        let v3 = UIView(frame:CGRect(43, 197, 160, 115))
         v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
 
         self.view.addSubview(v1)
@@ -27,12 +27,13 @@ class ViewController: UIViewController {
         self.view.addSubview(v3)
 
         // bounds
-        let v11 = UIView(frame:CGRect(213, 411, 132, 194))
+        let v11 = UIView(frame:CGRect(10, 411, 50, 50))
         v11.backgroundColor = .blue
-        let v22 = UIView(frame:v1.bounds.insetBy(dx: 10, dy: 10))
+        let v22 = UIView(frame:v11.bounds.insetBy(dx: 10, dy: 10))
         v22.backgroundColor = .red
         self.view.addSubview(v11)
         v11.addSubview(v22)
+
     }
 
 
