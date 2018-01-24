99c2ce9b649e4c1f7b7102bda85574f4a0a22f0e
greatabel <myreceiver2for2github@gmail.com>
2018-01-24 15:13:59 +0800

    frame

--- Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/AppDelegate.swift
+++ Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/AppDelegate.swift
@@ -8,6 +8,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = self.window ?? UIWindow()
+        self.window!.rootViewController = ViewController()
+
+        self.window!.backgroundColor = .white
+        self.window!.makeKeyAndVisible()
         return true
     }
 
--- Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/ViewController.swift
+++ Programming iOS 11/1Views/03experiment_CGRect/03experiment_CGRect/ViewController.swift
@@ -11,9 +11,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let v = UIView(frame: CGRect(100, 100, 50, 50))
+        let v = UIView(frame: CGRect(0, 0, 150, 150))
         v.backgroundColor = .blue
         self.view.addSubview(v)
+
+        let v1 = UIView(frame:CGRect(113, 111, 132, 194))
+        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        let v2 = UIView(frame:CGRect(41, 56, 132, 194))
+        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        let v3 = UIView(frame:CGRect(43, 197, 160, 230))
+        v3.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+
+        self.view.addSubview(v1)
+        v1.addSubview(v2)
+        self.view.addSubview(v3)
     }
 
 
