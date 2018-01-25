f9917f3c6b60a1b59dd45bf049a73c60b52150d8
greatabel <myreceiver2for2github@gmail.com>
2018-01-25 11:04:33 +0800

    rm not use file

--- Programming iOS 11/1Views/07constraints/07constraints/AppDelegate.swift
+++ Programming iOS 11/1Views/07constraints/07constraints/AppDelegate.swift
@@ -1,38 +1,71 @@
 import UIKit
 
+func delay(_ delay:Double, closure:@escaping ()->()) {
+    let when = DispatchTime.now() + delay
+    DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+}
+
+func dictionaryOfNames(_ arr:UIView...) -> [String:UIView] {
+    var d = [String:UIView]()
+    for (ix,v) in arr.enumerated() {
+        d["v\(ix+1)"] = v
+    }
+    return d
+}
+
+extension NSLayoutConstraint {
+    class func reportAmbiguity (_ v:UIView?) {
+        var v = v
+        if v == nil {
+            v = UIApplication.shared.keyWindow
+        }
+        for vv in v!.subviews {
+            print("\(vv) \(vv.hasAmbiguousLayout)")
+            if vv.subviews.count > 0 {
+                self.reportAmbiguity(vv)
+            }
+        }
+    }
+    class func listConstraints (_ v:UIView?) {
+        var v = v
+        if v == nil {
+            v = UIApplication.shared.keyWindow
+        }
+        for vv in v!.subviews {
+            let arr1 = vv.constraintsAffectingLayout(for:.horizontal)
+            let arr2 = vv.constraintsAffectingLayout(for:.vertical)
+            NSLog("\n\n%@\nH: %@\nV:%@", vv, arr1, arr2);
+            if vv.subviews.count > 0 {
+                self.listConstraints(vv)
+            }
+        }
+    }
+}
+
+extension CGRect {
+    init(_ x:CGFloat, _ y:CGFloat, _ w:CGFloat, _ h:CGFloat) {
+        self.init(x:x, y:y, width:w, height:h)
+    }
+}
+
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate : UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+    var window : UIWindow?
 
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]?) -> Bool {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = self.window ?? UIWindow()
+
+        self.window!.rootViewController = ViewController()
+        self.window!.backgroundColor = .white
+        self.window!.makeKeyAndVisible()
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("here") // for debugging
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
-
