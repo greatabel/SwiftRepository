8c896c442ef22cf168de5d6bf8c34460634cc66f
greatabel <myreceiver2for2github@gmail.com>
2018-01-25 11:54:56 +0800

    show

--- Programming iOS 11/2Drawing/01HiResTest/01HiResTest/ViewController.swift
+++ Programming iOS 11/2Drawing/01HiResTest/01HiResTest/ViewController.swift
@@ -1,17 +1,35 @@
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var iv3: UIImageView!
+    @IBOutlet weak var iv4: UIImageView!
+    @IBOutlet weak var iv5: UIImageView!
+    @IBOutlet weak var iv6: UIImageView!
+
+    @IBOutlet weak var iv: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(self.iv.image!.scale)
+
+        // let's try out the new image literal feature
+        // ooh, it works with code completion; you just type the name
+
+        self.iv3.image = #imageLiteral(resourceName: "one") // behind the scenes: #imageLiteral with resourceName: param
+        self.iv4.image = UIImage(named:"uno")
+
+        if let s = Bundle.main.path(forResource: "one", ofType: "png") {
+            self.iv5.image = UIImage(contentsOfFile: s)
+        }
+        if let s2 = Bundle.main.path(forResource: "uno", ofType: "png") {
+            self.iv6.image = UIImage(contentsOfFile: s2)
+        } else {
+            print("looking for smiley")
+            self.iv6.image = UIImage(named:"smiley")
+        }
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
-
