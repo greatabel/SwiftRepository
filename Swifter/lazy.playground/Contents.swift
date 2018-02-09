110c74875205a5368ef094c1d04c469dce36fdde
greatabel <myreceiver2for2github@gmail.com>
2018-02-09 11:47:59 +0800

    show lazy map

--- Swifter/lazy.playground/Contents.swift
+++ Swifter/lazy.playground/Contents.swift
@@ -14,3 +14,29 @@ for i in 0..<5 {
     a.str
 }
 
+print("------第2部分------")
+
+let data = 1...5
+let result = data.map {
+    (i: Int) -> Int in
+    print("processing \(i)")
+    return i * 2
+}
+print("Prepare to visit")
+for i in result {
+    print("processed :\(i)")
+}
+print("end visit")
+
+print("------第3部分------")
+let lazy_result = data.lazy.map {
+    (i: Int) -> Int in
+    print("processing \(i)")
+    return i * 2
+}
+print("准备访问结果")
+for i in lazy_result {
+    print("操作后结果为 \(i)")
+}
+
+print("操作完毕")
