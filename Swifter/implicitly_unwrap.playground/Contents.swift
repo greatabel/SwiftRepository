5b8576d0852df27656710d87fe595e134779375b
greatabel <myreceiver2for2github@gmail.com>
2018-05-10 17:10:52 +0800

    implicitly_unwrap.playground/

---
+++ Swifter/implicitly_unwrap.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class MyClass {
+    func foo() {
+        print(self)
+    }
+}
+
+var maybeObeject: MyClass!
+
+var maybeObeject0: MyClass! = MyClass()
+maybeObeject0!.foo()
+maybeObeject0.foo()
+
