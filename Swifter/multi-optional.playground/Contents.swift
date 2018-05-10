e46502edffc0b7d04eadc5cdddc808f6fcde7890
greatabel <myreceiver2for2github@gmail.com>
2018-05-10 17:15:15 +0800

    multi-optional.playground/

--- Swifter/multi-optional.playground/Contents.swift
+++ Swifter/multi-optional.playground/Contents.swift
@@ -1,4 +1,14 @@
 import UIKit
 
+var aNil: String? = nil
 
+var anotherNil: String?? = aNil
+var literalNil: String?? = nil
 
+if let a = anotherNil {
+    print("anotherNil")
+}
+
+if let b = literalNil {
+    print("literalNil")
+}
