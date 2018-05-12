9aac450bc0996ad191ba027871e6b3faa9058835
greatabel <myreceiver2for2github@gmail.com>
2018-05-12 22:50:15 +0800

    indirect_enum.playground

--- Swifter/indirect_enum.playground/Contents.swift
+++ Swifter/indirect_enum.playground/Contents.swift
@@ -8,4 +8,19 @@ class Node<T> {
         self.value = value
         self.next = next
     }
+
+    
 }
+
+let list = Node(value: 1,
+    next: Node(value: 2,
+    next: Node(value: 3,
+    next: Node(value: 4, next: nil))))
+
+indirect enum LinkedList<Element: Comparable> {
+    case Empty
+    case Node(Element, LinkedList<Element>)
+
+    
+}
+let linkedList = LinkedList.Node(1, .Node(2, .Node(3, .Node(4, .Empty))))
