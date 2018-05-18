0e91c9d750d1dce0a94db9b67add605b83f5aa05
greatabel <myreceiver2for2github@gmail.com>
2018-05-18 15:32:18 +0800

    UIPageViewControllerDataSource UIPageViewControllerDelegate

--- Practise/07U17/07U17/Procedure/Base/ViewController/UPageViewController.swift
+++ Practise/07U17/07U17/Procedure/Base/ViewController/UPageViewController.swift
@@ -40,10 +40,52 @@ class UPageViewController: UBaseViewController{
     }
 
     @objc func changeIndex(segment: UISegmentedControl) {
-
+        let index = segment.selectedSegmentIndex
+        if currentSelectIndex != index {
+            let target: [UIViewController] = [vcs[index]]
+            let direction: UIPageViewControllerNavigationDirection = currentSelectIndex > index ? .reverse: .forward
+            pageVC.setViewControllers(target, direction: direction, animated: false)
+            { [weak self] (finish) in
+                self?.currentSelectIndex = index
+            }
+        }
     }
 
     override func configUI() {
         
     }
 }
+
+
+extension UPageViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+
+        guard let index = vcs.index(of: viewController) else { return nil }
+        let beforeIndex = index - 1
+        guard beforeIndex >= 0 else {return nil}
+        return vcs[beforeIndex]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+
+        guard let index = vcs.index(of: viewController) else { return nil }
+        let afterIndex = index + 1
+        guard afterIndex <= vcs.count - 1 else { return nil }
+        return vcs[afterIndex]
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+
+        guard let viewController = pageViewController.viewControllers?.last,
+            let index = vcs.index(of: viewController) else {
+                return
+        }
+        currentSelectIndex = index
+        segment.setSelectedSegmentIndex(UInt(index), animated: true)
+        guard title != nil && pageStyle == .none else { return }
+        navigationItem.title = titles[index]
+    }
+
+
+}
+
